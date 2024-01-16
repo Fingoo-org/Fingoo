079b130d7578445a8d3f192fcfd6b20af83372a5
SeonghunYang <didtjdgns852@gmail.com>
2024-01-16 04:00:35 +0000

    [EI-98] [api] feat: Update TypeOrmConfigService

--- api/src/community/infrastructure/adapter/persistent/entity/post.entity.ts
+++ api/src/community/infrastructure/adapter/persistent/entity/post.entity.ts
@@ -1,5 +1,10 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
 export class PostEntity {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   content: string;
 }
--- api/src/config/typeorm.config.service.ts
+++ api/src/config/typeorm.config.service.ts
@@ -10,7 +10,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       type: 'mysql',
       host: this.configService.get<string>('DB_HOST'),
       port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_NAME'),
+      username: this.configService.get<string>('DB_USERNAME'),
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_DATABASE'),
       entities: ['dist/**/**/*.entity.{ts,js}'],
