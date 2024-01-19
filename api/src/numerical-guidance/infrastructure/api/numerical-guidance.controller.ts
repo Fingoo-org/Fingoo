07c2014a2d1eeb251f35dc8f78fc353654338de6
나경호 <gangho324@gmail.com>
2024-01-19 18:29:47 +0900

    [EI-91] [api] feat: Get 변동지표

---
+++ api/src/numerical-guidance/infrastructure/api/numerical-guidance.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { GetFluctuatingIndicatorsQuery } from '../../application/query/get-fluctuatingIndicators/get-fluctuatingIndicators.query';
+import { FluctuatingIndicatorsDto } from '../../application/query/get-fluctuatingIndicators/fluctuatingIndicators.dto';
+import { GetFluctuatingIndicatorsDto } from './dto/get-fluctuatingIndicators.dto';
+
+@Controller('/numerical-guidance')
+export class NumericalGuidanceController {
+  constructor(private queryBus: QueryBus) {}
+
+  @Get()
+  async getFluctuatingIndicators(
+    @Body() getFluctuatingIndicatorsDto: GetFluctuatingIndicatorsDto,
+  ): Promise<FluctuatingIndicatorsDto> {
+    const query = new GetFluctuatingIndicatorsQuery(
+      getFluctuatingIndicatorsDto.dataCount,
+      getFluctuatingIndicatorsDto.ticker,
+      getFluctuatingIndicatorsDto.type,
+    );
+    return this.queryBus.execute(query);
+  }
+}
