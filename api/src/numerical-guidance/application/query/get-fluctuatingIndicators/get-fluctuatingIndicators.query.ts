cca2a4728857f7ebfb90d998aa6ffb8f9cada9d0
나경호 <gangho324@gmail.com>
2024-01-19 18:30:19 +0900

    [EI-91] [api] feat: get-fluctuatingIndicators query

---
+++ api/src/numerical-guidance/application/query/get-fluctuatingIndicators/get-fluctuatingIndicators.query.ts
@@ -0,0 +1,9 @@
+import { IQuery } from '@nestjs/cqrs';
+
+export class GetFluctuatingIndicatorsQuery implements IQuery {
+  constructor(
+    readonly dataCount: number,
+    readonly tickers: string[],
+    readonly type: string,
+  ) {}
+}
