363f4a086c6573fb8e402283d30c949ed80319e3
나경호 <gangho324@gmail.com>
2024-01-20 17:58:03 +0900

    [EI-91] [api] feat: response Dto와 data 사이에 mapper 추가

--- api/src/numerical-guidance/application/query/get-fluctuatingIndicators/fluctuatingIndicators.dto.ts
+++ api/src/numerical-guidance/application/query/get-fluctuatingIndicators/fluctuatingIndicators.dto.ts
@@ -1,4 +1,4 @@
-type StockItem = {
+export type Item = {
   basDt: string;
   srtnCd: string;
   isinCd: string;
@@ -16,68 +16,35 @@ type StockItem = {
   mrktTotAmt: string;
 };
 
-type Item = {
-  item: StockItem[];
+export type Items = {
+  item: Item[];
 };
 
-type Body = {
+export type Body = {
   numOfRows: number;
   pageNo: number;
   totalCount: number;
-  items: Item[];
+  items: Items;
+};
+
+export type Header = {
+  resultCode: string;
+  resultMsg: string;
+};
+
+export type Response = {
+  header: Header;
+  body: Body;
 };
 
 export class FluctuatingIndicatorsDto {
-  header: {
-    resultCode: string;
-    resultMsg: string;
-  };
-  body: Body;
+  response: Response;
 
-  private constructor(header: { resultCode: string; resultMsg: string }, body: Body) {
-    this.header = header;
-    this.body = body;
+  private constructor(response: Response) {
+    this.response = response;
   }
 
-  static create(StringData: string): FluctuatingIndicatorsDto {
-    const data: {
-      resultCode: string;
-      resultMsg: string;
-      numOfRows: number;
-      pageNo: number;
-      totalCount: number;
-      items: Item[];
-    } = JSON.parse(StringData);
-
-    const header = {
-      resultCode: data.resultCode,
-      resultMsg: data.resultMsg,
-    };
-
-    const body = {
-      numOfRows: data.numOfRows,
-      pageNo: data.pageNo,
-      totalCount: data.totalCount,
-      items: data.items.map((item) => ({
-        item: item.item.map((subItem) => ({
-          basDt: subItem.basDt,
-          srtnCd: subItem.srtnCd,
-          isinCd: subItem.isinCd,
-          itmsNm: subItem.itmsNm,
-          mrktCtg: subItem.mrktCtg,
-          clpr: subItem.clpr,
-          vs: subItem.vs,
-          fltRt: subItem.fltRt,
-          mkp: subItem.mkp,
-          hipr: subItem.hipr,
-          lopr: subItem.lopr,
-          trqu: subItem.trqu,
-          trPrc: subItem.trPrc,
-          lstgStCnt: subItem.lstgStCnt,
-          mrktTotAmt: subItem.mrktTotAmt,
-        })),
-      })),
-    };
-    return new FluctuatingIndicatorsDto(header, body);
+  static create({ response }: { response: any }): FluctuatingIndicatorsDto {
+    return new FluctuatingIndicatorsDto(response);
   }
 }
