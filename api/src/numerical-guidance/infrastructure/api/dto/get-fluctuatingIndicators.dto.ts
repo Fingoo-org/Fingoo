2c04b5fe3733c29996c1c9107439352ca536e712
나경호 <gangho324@gmail.com>
2024-01-20 17:58:55 +0900

    [EI-91] [api] refactor: request dto 변경

--- api/src/numerical-guidance/infrastructure/api/dto/get-fluctuatingIndicators.dto.ts
+++ api/src/numerical-guidance/infrastructure/api/dto/get-fluctuatingIndicators.dto.ts
@@ -1,12 +1,25 @@
-import { IsInt, IsString } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsInt, IsString, ValidateNested } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+
+export class FluctuatingIndicatorInfo {
+  @IsString()
+  readonly ticker: string;
+
+  @IsString()
+  readonly market: string;
+}
 
 export class GetFluctuatingIndicatorsDto {
+  @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   readonly dataCount: number;
 
   @IsString()
-  readonly ticker: string[];
-
-  @IsString()
   readonly type: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => FluctuatingIndicatorInfo)
+  readonly fluctuatingIndicatorInfos: FluctuatingIndicatorInfo[];
 }
