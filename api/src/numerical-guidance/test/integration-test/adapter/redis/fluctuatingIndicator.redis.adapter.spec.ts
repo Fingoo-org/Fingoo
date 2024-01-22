a3c2070b5c3fa1f386c2a222b1e666ccfccf3444
나경호 <gangho324@gmail.com>
2024-01-22 17:09:22 +0900

    [EI-91] [api] test: testData 통합 관리

--- api/src/numerical-guidance/test/integration-test/adapter/redis/fluctuatingIndicator.redis.adapter.spec.ts
+++ api/src/numerical-guidance/test/integration-test/adapter/redis/fluctuatingIndicator.redis.adapter.spec.ts
@@ -2,106 +2,11 @@ import { Test } from '@nestjs/testing';
 import { FluctuatingIndicatorsDto } from '../../../../application/query/get-fluctuatingIndicators/fluctuatingIndicators.dto';
 import { FluctuatingIndicatorRedisAdapter } from '../../../../infrastructure/adapter/redis/fluctuatingIndicator.redis.adapter';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { fluctuatingIndicatorTestData } from '../../../data/fluctuatingIndicator.test.data';
+import { ConfigModule } from '@nestjs/config';
+import { RedisConfigService } from '../../../../../config/redis.config.service';
 
-const testData = {
-  response: {
-    header: { resultCode: '00', resultMsg: 'NORMAL SERVICE.' },
-    body: {
-      numOfRows: 5,
-      pageNo: 1,
-      totalCount: 1000,
-      items: {
-        item: [
-          {
-            basDt: '20240118',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '71700',
-            vs: '700',
-            fltRt: '.99',
-            mkp: '71600',
-            hipr: '72000',
-            lopr: '70700',
-            trqu: '17853397',
-            trPrc: '1274268001300',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '428033408835000',
-          },
-          {
-            basDt: '20240117',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '71000',
-            vs: '-1600',
-            fltRt: '-2.2',
-            mkp: '73100',
-            hipr: '73300',
-            lopr: '71000',
-            trqu: '22683660',
-            trPrc: '1627050948220',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '423854561050000',
-          },
-          {
-            basDt: '20240116',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '72600',
-            vs: '-1300',
-            fltRt: '-1.76',
-            mkp: '73500',
-            hipr: '73700',
-            lopr: '72500',
-            trqu: '14760415',
-            trPrc: '1075976773200',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '433406213130000',
-          },
-          {
-            basDt: '20240115',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '73900',
-            vs: '800',
-            fltRt: '1.09',
-            mkp: '73200',
-            hipr: '74000',
-            lopr: '73200',
-            trqu: '13212339',
-            trPrc: '973180055524',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '441166930445000',
-          },
-          {
-            basDt: '20240112',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '73100',
-            vs: '-100',
-            fltRt: '-.14',
-            mkp: '73000',
-            hipr: '74100',
-            lopr: '72800',
-            trqu: '13038939',
-            trPrc: '956811865637',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '436391104405000',
-          },
-        ],
-      },
-    },
-  },
-};
+const testData = fluctuatingIndicatorTestData;
 
 describe('FluctuatingIndicatorRedisAdapter', () => {
   let fluctuatingIndicatorRedisAdapter: FluctuatingIndicatorRedisAdapter;
@@ -130,7 +35,7 @@ describe('FluctuatingIndicatorRedisAdapter', () => {
     fluctuatingIndicatorRedisAdapter = module.get(FluctuatingIndicatorRedisAdapter);
   });
 
-  it('redis caching test.', async () => {
+  it('redis에서 캐시된 값을 불러오는 경우.', async () => {
     //given
     const testCachingData = FluctuatingIndicatorsDto.create(testData);
     await fluctuatingIndicatorRedisAdapter.cachingFluctuatingIndicator('testTicker', testCachingData);
@@ -144,7 +49,7 @@ describe('FluctuatingIndicatorRedisAdapter', () => {
     expect(result).toEqual(expected);
   });
 
-  it('redis caching fail test.', async () => {
+  it('redis에 캐시된 값이 없을 경우.', async () => {
     //given
     const testCachingData = FluctuatingIndicatorsDto.create(testData);
     await fluctuatingIndicatorRedisAdapter.cachingFluctuatingIndicator('testTicker', testCachingData);
--- api/src/numerical-guidance/test/unit-test/query/get-flucuatingIndicators.query.handler.spec.ts
+++ api/src/numerical-guidance/test/unit-test/query/get-flucuatingIndicators.query.handler.spec.ts
@@ -4,107 +4,10 @@ import { FluctuatingIndicatorsDto } from '../../../application/query/get-fluctua
 import { GetFluctuatingIndicatorsQuery } from '../../../application/query/get-fluctuatingIndicators/get-fluctuatingIndicators.query';
 import { CachingFluctuatingIndicatorPort } from '../../../application/port/cache/caching-fluctuatingIndicator.port';
 import { LoadCachedFluctuatingIndicatorPort } from '../../../application/port/cache/load-cached-fluctuatingIndicator.port';
+import { fluctuatingIndicatorTestData } from '../../data/fluctuatingIndicator.test.data';
 // import { LoadFluctuatingIndicatorPort } from '../../../application/port/external/load-fluctuatingIndicator.port';
 
-const testData = {
-  response: {
-    header: { resultCode: '00', resultMsg: 'NORMAL SERVICE.' },
-    body: {
-      numOfRows: 5,
-      pageNo: 1,
-      totalCount: 1000,
-      items: {
-        item: [
-          {
-            basDt: '20240118',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '71700',
-            vs: '700',
-            fltRt: '.99',
-            mkp: '71600',
-            hipr: '72000',
-            lopr: '70700',
-            trqu: '17853397',
-            trPrc: '1274268001300',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '428033408835000',
-          },
-          {
-            basDt: '20240117',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '71000',
-            vs: '-1600',
-            fltRt: '-2.2',
-            mkp: '73100',
-            hipr: '73300',
-            lopr: '71000',
-            trqu: '22683660',
-            trPrc: '1627050948220',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '423854561050000',
-          },
-          {
-            basDt: '20240116',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '72600',
-            vs: '-1300',
-            fltRt: '-1.76',
-            mkp: '73500',
-            hipr: '73700',
-            lopr: '72500',
-            trqu: '14760415',
-            trPrc: '1075976773200',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '433406213130000',
-          },
-          {
-            basDt: '20240115',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '73900',
-            vs: '800',
-            fltRt: '1.09',
-            mkp: '73200',
-            hipr: '74000',
-            lopr: '73200',
-            trqu: '13212339',
-            trPrc: '973180055524',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '441166930445000',
-          },
-          {
-            basDt: '20240112',
-            srtnCd: '005930',
-            isinCd: 'KR7005930003',
-            itmsNm: '삼성전자',
-            mrktCtg: 'KOSPI',
-            clpr: '73100',
-            vs: '-100',
-            fltRt: '-.14',
-            mkp: '73000',
-            hipr: '74100',
-            lopr: '72800',
-            trqu: '13038939',
-            trPrc: '956811865637',
-            lstgStCnt: '5969782550',
-            mrktTotAmt: '436391104405000',
-          },
-        ],
-      },
-    },
-  },
-};
+const testData = fluctuatingIndicatorTestData;
 
 describe('GetFluctuatingIndicatorsQueryHandler', () => {
   let getFluctuatingIndicatorsQueryHandler: GetFluctuatingIndicatorsQueryHandler;
