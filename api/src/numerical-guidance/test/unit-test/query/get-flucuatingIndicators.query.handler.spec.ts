5958dcbd1a91d8fced3991bbeb8c182967e676dc
나경호 <gangho324@gmail.com>
2024-01-22 12:53:23 +0900

    [EI-91] [api] test: redis, krx 연결 후 테스트 추가

--- api/src/numerical-guidance/test/unit-test/query/get-flucuatingIndicators.query.handler.spec.ts
+++ api/src/numerical-guidance/test/unit-test/query/get-flucuatingIndicators.query.handler.spec.ts
@@ -2,6 +2,9 @@ import { Test } from '@nestjs/testing';
 import { GetFluctuatingIndicatorsQueryHandler } from '../../../application/query/get-fluctuatingIndicators/get-fluctuatingIndicators.query.handler';
 import { FluctuatingIndicatorsDto } from '../../../application/query/get-fluctuatingIndicators/fluctuatingIndicators.dto';
 import { GetFluctuatingIndicatorsQuery } from '../../../application/query/get-fluctuatingIndicators/get-fluctuatingIndicators.query';
+import { CachingFluctuatingIndicatorPort } from '../../../application/port/cache/caching-fluctuatingIndicator.port';
+import { LoadCachedFluctuatingIndicatorPort } from '../../../application/port/cache/load-cached-fluctuatingIndicator.port';
+// import { LoadFluctuatingIndicatorPort } from '../../../application/port/external/load-fluctuatingIndicator.port';
 
 const testData = {
   response: {
@@ -105,6 +108,9 @@ const testData = {
 
 describe('GetFluctuatingIndicatorsQueryHandler', () => {
   let getFluctuatingIndicatorsQueryHandler: GetFluctuatingIndicatorsQueryHandler;
+  let loadCachedFluctuatingIndicatorPort: LoadCachedFluctuatingIndicatorPort;
+  // let loadFluctuatingIndicatorPort: LoadFluctuatingIndicatorPort;
+  let cachingFluctuatingIndicatorPort: CachingFluctuatingIndicatorPort;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -134,6 +140,9 @@ describe('GetFluctuatingIndicatorsQueryHandler', () => {
     }).compile();
 
     getFluctuatingIndicatorsQueryHandler = module.get(GetFluctuatingIndicatorsQueryHandler);
+    loadCachedFluctuatingIndicatorPort = module.get('LoadCachedFluctuatingIndicatorPort');
+    // loadFluctuatingIndicatorPort = module.get('LoadFluctuatingIndicatorPort');
+    cachingFluctuatingIndicatorPort = module.get('CachingFluctuatingIndicatorPort');
   });
 
   it('변동지표를 불러온다.', async () => {
@@ -148,5 +157,23 @@ describe('GetFluctuatingIndicatorsQueryHandler', () => {
     const expected = FluctuatingIndicatorsDto.create(testData);
 
     expect(result[0]).toEqual(expected);
+    expect(cachingFluctuatingIndicatorPort.cachingFluctuatingIndicator).toHaveBeenCalled();
+  });
+
+  it('변동지표가 redis에서 불러와진다.', async () => {
+    //given
+    const getFluctuatingIndicatorsQuery: GetFluctuatingIndicatorsQuery = new GetFluctuatingIndicatorsQuery(2, 'KOREA', [
+      { ticker: 'KR7005930009', market: 'KOSPI' },
+    ]);
+    //when
+    await getFluctuatingIndicatorsQueryHandler.execute(getFluctuatingIndicatorsQuery);
+    const result2 = await getFluctuatingIndicatorsQueryHandler.execute(getFluctuatingIndicatorsQuery);
+
+    //then
+    const expected = FluctuatingIndicatorsDto.create(testData);
+
+    expect(result2[0]).toEqual(expected);
+    expect(loadCachedFluctuatingIndicatorPort.loadCachedFluctuatingIndicator).toHaveBeenCalled();
+    // expect(loadFluctuatingIndicatorPort.loadFluctuatingIndicator).not.toHaveBeenCalled(); // TODO: 외부 api와 연동 후 테스트
   });
 });
