43a16d7f192f60acd89e4eb2c77d0f6dbfcb0ff7
SeonghunYang <didtjdgns852@gmail.com>
2024-01-22 11:21:16 +0000

    [EI-90] [web] feat: develope workspace layout

---
+++ web/app/workspace/layout.tsx
@@ -0,0 +1,11 @@
+import SideNav from '../ui/pages/workspace/sidenav';
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-96">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
