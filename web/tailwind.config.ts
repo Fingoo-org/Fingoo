586b2cf48555182ab15bf86d67922db175b88d22
SeonghunYang <didtjdgns852@gmail.com>
2024-01-25 01:50:14 +0000

    [EI-97] [web] chore: Add @headlessui/tailwindcss dependency and import it in tailwind.config.ts

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import headlessui from '@headlessui/tailwindcss';
 
 const config: Config = {
   content: [
@@ -10,11 +11,10 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [headlessui],
+};
+export default config;
