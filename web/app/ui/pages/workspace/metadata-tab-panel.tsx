dce3a5f2d0d7e6452bf143f373655c814eebfb89
SeonghunYang <didtjdgns852@gmail.com>
2024-01-25 01:18:31 +0000

    [EI-90] [web] feat: Add handleMetadataCreateAndSelect function to create and select metadata

--- web/app/ui/pages/workspace/metadata-tab-panel.tsx
+++ web/app/ui/pages/workspace/metadata-tab-panel.tsx
@@ -1,22 +1,26 @@
 import { useStore } from '@/app/store';
 import { MetadataList } from '../../components/numerical-guidance/metadata-list';
 import Button from '../../components/view/atom/button';
+import { IndicatorBoardMetadata } from '@/app/store/indicator-board-metadata.slice';
 
 export default function MetadataTabPanel() {
   const addMetadata = useStore((state) => state.addMetadata);
+  const selectMetadata = useStore((state) => state.selectMetaData);
 
-  const handleMetadataCreate = () => {
-    addMetadata({
+  const handleMetadataCreateAndSelect = () => {
+    const metadata: IndicatorBoardMetadata = {
       id: Math.random().toString(36),
       name: 'metadata1',
       indicators: [],
-    });
+    };
+    addMetadata(metadata);
+    selectMetadata(metadata.id);
   };
 
   return (
     <div>
       <MetadataList />
-      <Button onClick={handleMetadataCreate}>create</Button>
+      <Button onClick={handleMetadataCreateAndSelect}>create</Button>
     </div>
   );
 }
