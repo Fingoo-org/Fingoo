dc23634ca86fd0f5fa46b8c0b4267526134a874f
SeonghunYang <didtjdgns852@gmail.com>
2024-01-26 09:41:47 +0000

    [EI-111] [web] test: Add useStore hook and implement optimistic update

--- web/__tests__/hooks/use-indicator-metadata-list.hook.test.tsx
+++ web/__tests__/hooks/use-indicator-metadata-list.hook.test.tsx
@@ -5,6 +5,8 @@ import { HttpResponse, http } from 'msw';
 import { API_PATH } from '@/app/api/constants/api-path';
 import { SWRConfig } from 'swr';
 import { PropsWithChildren } from 'react';
+import { useStore } from '@/app/store';
+import { act } from 'react-dom/test-utils';
 
 const wrapper = ({ children }: PropsWithChildren) => {
   return <SWRConfig value={{ provider: () => new Map() }}>{children}</SWRConfig>;
@@ -75,6 +77,7 @@ describe('useIndicatorMetadataList', () => {
   it('메타 데이터 추가하기 실패', async () => {
     // given
     const { result } = renderHook(() => useIndicatorMetadataList(), { wrapper });
+    const { result: store } = renderHook(() => useStore());
     server.use(
       http.post(API_PATH.metadataList, () => {
         return new HttpResponse(null, { status: 400 });
@@ -84,7 +87,6 @@ describe('useIndicatorMetadataList', () => {
     // when
     await waitFor(() => expect(result.current.isLoading).toBe(false));
     await waitFor(() => result.current.createAndSelectMetadata({ id: '4', name: 'metadata4', indicators: [] }));
-    await waitFor(() => expect(result.current.mutationError).not.toBe(undefined));
 
     // then
     expect(result.current.metadataList).toEqual([
@@ -104,5 +106,68 @@ describe('useIndicatorMetadataList', () => {
         indicators: [],
       },
     ]);
+    expect(store.current.selectedMetadataId).toBe(null);
+  });
+
+  it('메타데이터 추가하기 실패: 낙관적 업데이트 테스트', async () => {
+    // given
+    const { result } = renderHook(() => useIndicatorMetadataList(), { wrapper });
+    const { result: store } = renderHook(() => useStore());
+    server.use(
+      http.post(API_PATH.metadataList, () => {
+        return new HttpResponse(null, { status: 400 });
+      }),
+    );
+
+    // when
+    await waitFor(() => expect(result.current.isLoading).toBe(false));
+    act(() => {
+      result.current.createAndSelectMetadata({ id: '4', name: 'metadata4', indicators: [] });
+    });
+    // 낙관적 업데이트 시 바로 업데이트 되기 때문에, 서버에서 실패하더라도 바로 업데이트 된다.
+    expect(result.current.metadataList).toEqual([
+      {
+        id: '1',
+        name: 'metadata1',
+        indicators: [],
+      },
+      {
+        id: '2',
+        name: 'metadata2',
+        indicators: [],
+      },
+      {
+        id: '3',
+        name: 'metadata3',
+        indicators: [],
+      },
+      {
+        id: '4',
+        name: 'metadata4',
+        indicators: [],
+      },
+    ]);
+    await waitFor(() => expect(result.current.mutationError).not.toBe(undefined));
+
+    // then
+    // 이후 rollback 되기 때문에, 업데이트가 되지 않는다.
+    expect(result.current.metadataList).toEqual([
+      {
+        id: '1',
+        name: 'metadata1',
+        indicators: [],
+      },
+      {
+        id: '2',
+        name: 'metadata2',
+        indicators: [],
+      },
+      {
+        id: '3',
+        name: 'metadata3',
+        indicators: [],
+      },
+    ]);
+    expect(store.current.selectedMetadataId).toBe(null);
   });
 });
