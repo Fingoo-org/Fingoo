069fc14e616fed439a5e4f2a6016750871a26821
SeonghunYang <didtjdgns852@gmail.com>
2024-01-28 09:54:54 +0000

    [EI-97] [web] test: Add unit test for useIndicatorList hook

---
+++ web/__tests__/hooks/use-indicator-list.hook.test.tsx
@@ -0,0 +1,21 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { SWRProviderWithoutCache } from '@/app/api/swr-provider';
+import { useIndicatorList } from '@/app/hooks/use-indicator-list.hook';
+
+const wrapper = SWRProviderWithoutCache;
+
+describe('useIndicatorList', () => {
+  it('지표 리스트 가져오기', async () => {
+    // given
+    const { result } = renderHook(() => useIndicatorList(), { wrapper });
+
+    // when
+    await waitFor(() => expect(result.current.indicatorList).toBe(undefined));
+
+    // then
+    expect(result.current.indicatorList).toHaveLength(3);
+    expect(result.current.indicatorList?.[0].ticker).toBe('AAPL');
+    expect(result.current.indicatorList?.[1].ticker).toBe('MSFT');
+    expect(result.current.indicatorList?.[2].ticker).toBe('GOOG');
+  });
+});
