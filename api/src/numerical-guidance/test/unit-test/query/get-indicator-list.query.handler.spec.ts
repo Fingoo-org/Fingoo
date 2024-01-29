7c1fd722b5027d37c4e2995f2953141eb43ce34d
yoounyoungheon <iddyoon@gmail.com>
2024-01-29 22:32:17 +0900

    [EI-112] [api] test: get-indicator-list-query.handler test 코드 작성

---
+++ api/src/numerical-guidance/test/unit-test/query/get-indicator-list.query.handler.spec.ts
@@ -0,0 +1,53 @@
+import { Test } from '@nestjs/testing';
+import { GetIndicatorListQueryHandler } from 'src/numerical-guidance/application/query/get-indicator-list/get-indicator-list.query.handler';
+import { IndicatorListDto } from 'src/numerical-guidance/application/query/get-indicator-list/indicator-list.dto';
+
+const testData = {
+  indicatorList: [
+    {
+      id: 1,
+      name: '삼성전자',
+      ticker: '005931',
+      type: 'stock',
+    },
+    {
+      id: 2,
+      name: '이스트아시아홀딩스',
+      ticker: '900110',
+      type: '',
+    },
+  ],
+};
+
+describe('GetIndicatorListQueryHandler', () => {
+  let getIndicatorListQueryHandler: GetIndicatorListQueryHandler;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        GetIndicatorListQueryHandler,
+        {
+          provide: 'GetIndicatorListPort',
+          useValue: {
+            getIndicatorList: jest.fn().mockImplementation(() => {
+              const indicatorList = IndicatorListDto.create(testData);
+              return indicatorList;
+            }),
+          },
+        },
+      ],
+    }).compile();
+    getIndicatorListQueryHandler = module.get(GetIndicatorListQueryHandler);
+  });
+
+  it('데이터 베이스에서 지표 리스트를 가져온다.', async () => {
+    // given
+
+    // when
+    const result = await getIndicatorListQueryHandler.execute();
+
+    // then
+    const expected = IndicatorListDto.create(testData);
+    expect(result).toEqual(expected);
+  });
+});
