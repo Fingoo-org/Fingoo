1712b454855e3a2949e9944c1ab9d2edc24dc47d
나경호 <gangho324@gmail.com>
2024-01-30 16:50:09 +0900

    [EI-114] [api] feat: 지표보드 메타데이터를 생성 controller

--- api/src/numerical-guidance/infrastructure/api/numerical-guidance.controller.ts
+++ api/src/numerical-guidance/infrastructure/api/numerical-guidance.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { Body, Controller, Get, HttpStatus, Post, Query, Res } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetFluctuatingIndicatorQuery } from '../../application/query/get-fluctuatingIndicator/get-fluctuatingIndicator.query';
 import { FluctuatingIndicatorDto } from '../../application/query/get-fluctuatingIndicator/fluctuatingIndicator.dto';
 import { GetFluctuatingIndicatorDto } from './dto/get-fluctuatingIndicator.dto';
 import { GetFluctuatingIndicatorWithoutCacheDto } from './dto/get-fluctuatingIndicator-without-cache.dto';
 import { GetFluctuatingIndicatorWithoutCacheQuery } from 'src/numerical-guidance/application/query/get-fluctuatingIndicator-without-cache/get-fluctuatingIndicator-without-cache.query';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateIndicatorBoardMetaDataDto } from './dto/create-indicator-board-meta-data.dto';
+import { CreateIndicatorBoardMetaDataCommand } from '../../application/command/create-indicator-board-meta-data/create-indicator-board-meta-data.command';
+import { Response } from 'express';
 
 @ApiTags('NumericalGuidanceController')
 @Controller('/numerical-guidance')
 export class NumericalGuidanceController {
-  constructor(private queryBus: QueryBus) {}
+  constructor(
+    private queryBus: QueryBus,
+    private commandBus: CommandBus,
+  ) {}
 
   @ApiOperation({ summary: '변동지표를 불러옵니다.' })
   @Get('/fluctuatingIndicator')
@@ -40,5 +46,20 @@ export class NumericalGuidanceController {
       getFluctuatingIndicatorWithoutCacheDto.endDate,
     );
     return this.queryBus.execute(query);
+  }
+
+  @ApiOperation({ summary: '지표보드 메타데이터를 생성합니다.' })
+  @Post('/indicatorBoardMetaData')
+  async createIndicatorBoardMetaData(
+    @Body() createIndicatorBoardMetaDataDto: CreateIndicatorBoardMetaDataDto,
+    @Res() res: Response,
+  ) {
+    const command = new CreateIndicatorBoardMetaDataCommand(
+      createIndicatorBoardMetaDataDto.indicatorBoardMetaDataName,
+      createIndicatorBoardMetaDataDto.indicatorIds,
+      createIndicatorBoardMetaDataDto.memberId,
+    );
+    await this.commandBus.execute(command);
+    res.status(HttpStatus.CREATED).send();
   }
 }
