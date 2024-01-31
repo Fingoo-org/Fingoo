5b5e40509927e8b650740dc5ea7bc3b4c2a2fb23
나경호 <gangho324@gmail.com>
2024-01-31 16:38:33 +0900

    [EI-114] [api] test: 지표모드 메타데이터 생성 handler 단위 테스트

---
+++ api/src/numerical-guidance/test/unit-test/query/create-indicator-board-meta-data.command.handler.spec.ts
@@ -0,0 +1,47 @@
+import { CreateIndicatorBoardMetaDataCommandHandler } from '../../../application/command/create-indicator-board-meta-data/create-indicator-board-meta-data.command.handler';
+import { CreateIndicatorBoardMetaDataPort } from '../../../application/port/persistent/create-indicator-board-meta-data.port';
+import { CqrsModule } from '@nestjs/cqrs';
+import { Test } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { CreateIndicatorBoardMetaDataCommand } from '../../../application/command/create-indicator-board-meta-data/create-indicator-board-meta-data.command';
+import { IndicatorBoardMetaData } from '../../../domain/indicator-board-meta-data';
+
+describe('CreateIndicatorBoardMetaDataCommandHandler', () => {
+  let createIndicatorBoardMetaDataCommandHandler: CreateIndicatorBoardMetaDataCommandHandler;
+  let createIndicatorBoardMetaDataPort: CreateIndicatorBoardMetaDataPort;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [CqrsModule, ConfigModule.forRoot()],
+      providers: [
+        CreateIndicatorBoardMetaDataCommandHandler,
+        {
+          provide: 'CreateIndicatorBoardMetaDataPort',
+          useValue: {
+            createIndicatorBoardMetaData: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    createIndicatorBoardMetaDataCommandHandler = module.get(CreateIndicatorBoardMetaDataCommandHandler);
+    createIndicatorBoardMetaDataPort = module.get('CreateIndicatorBoardMetaDataPort');
+  }, 10000);
+
+  it('변동지표를 불러온다.', async () => {
+    //given
+    const command: CreateIndicatorBoardMetaDataCommand = new CreateIndicatorBoardMetaDataCommand(
+      '메타데이터',
+      { key1: ['1', '2', '3'] },
+      1,
+    );
+
+    //when
+    const indicatorBoardMetaData: IndicatorBoardMetaData =
+      await createIndicatorBoardMetaDataCommandHandler.execute(command);
+
+    //then
+    expect(indicatorBoardMetaData.indicatorBoardMetaDataName).toEqual('메타데이터');
+    expect(createIndicatorBoardMetaDataPort.createIndicatorBoardMetaData).toHaveBeenCalledTimes(1);
+  });
+});
