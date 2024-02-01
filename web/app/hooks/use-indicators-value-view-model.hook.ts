16947dbf28ba70618730f7fae23f20e02d831cf0
SeonghunYang <didtjdgns852@gmail.com>
2024-02-01 07:13:01 +0000

    [EI-86] [web] feat: Refactor useIndicatorsValueViewModel hook and add IndicatorValueViewModel services

--- web/app/hooks/use-indicators-value-view-model.hook.ts
+++ web/app/hooks/use-indicators-value-view-model.hook.ts
@@ -1,12 +1,19 @@
+import { useMemo } from 'react';
 import { useFetchIndicatorsValue } from '../api/query/numerical-guidance.query';
+import { convertIndicatorsValueViewModel } from '../services/indicators-value-view-model.services';
 import { useSelectedIndicatorBoardMetadata } from './use-selected-indicator-board-metadata.hook';
 
 export const useIndicatorsValueViewModel = () => {
   const { selectedMetadata } = useSelectedIndicatorBoardMetadata();
-  const { data } = useFetchIndicatorsValue(selectedMetadata?.indicators ?? []);
+  const { data: indicatorsValueData } = useFetchIndicatorsValue(selectedMetadata?.indicators ?? []);
 
-  console.log(data);
+  const indciatorsValueViewModel = useMemo(() => {
+    if (!indicatorsValueData) return [];
+
+    return convertIndicatorsValueViewModel(indicatorsValueData);
+  }, [indicatorsValueData]);
+
   return {
-    indicatorsValue: data?.indicatorsValue,
+    indicatorsValue: indciatorsValueViewModel,
   };
 };
---
+++ web/app/services/indicators-value-view-model.services.ts
@@ -0,0 +1,35 @@
+import {
+  IndicatorValueItemResponse,
+  IndicatorValueResponse,
+  IndicatorsValueResponse,
+} from '../api/query/numerical-guidance.query';
+
+// Risk: item이 길어지면 오버헤드
+class IndicatorValueItem {
+  readonly date: string;
+  readonly value: number;
+  constructor({ date, value }: IndicatorValueItemResponse) {
+    this.date = date;
+    this.value = value;
+  }
+}
+
+class IndicatorValue {
+  readonly ticker: string;
+  readonly items: IndicatorValueItem[];
+  constructor({ ticker, items }: IndicatorValueResponse) {
+    this.ticker = ticker;
+    this.items = items.map((item) => new IndicatorValueItem(item));
+  }
+}
+
+export class IndicatorsValue {
+  readonly indicatorsValue: IndicatorValueResponse[];
+  constructor({ indicatorsValue }: IndicatorsValueResponse) {
+    this.indicatorsValue = indicatorsValue.map((indicatorValue) => new IndicatorValue(indicatorValue));
+  }
+}
+
+export const convertIndicatorsValueViewModel = (indicators: IndicatorsValueResponse) => {
+  return new IndicatorsValue(indicators);
+};
