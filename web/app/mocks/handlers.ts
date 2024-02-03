effeb5f8cef5dee62c7c3db8ebfa30e6bd489ed8
SeonghunYang <didtjdgns852@gmail.com>
2024-02-03 23:40:40 +0000

    [EI-118] [web] feat: Add delay for development in mock handlers

--- web/app/mocks/handlers.ts
+++ web/app/mocks/handlers.ts
@@ -4,27 +4,34 @@ import { mockDB } from './mock-db';
 import { AddIndicatorToMetadataRequestBody } from '../querys/numerical-guidance/indicator-board-metadata.query';
 import { CreateIndicatorMetadataRequestBody } from '../querys/numerical-guidance/indicator-board-metadata.query';
 
+const delayForDevelopment = async (ms = 1000) => {
+  if (process.env.NODE_ENV === 'development') {
+    await delay(ms);
+  }
+};
+
 export const handlers = [
   http.get(API_PATH.metadataList, async () => {
-    // await delay();
+    await delayForDevelopment();
     return HttpResponse.json(mockDB.getMetadataList());
   }),
   http.post<never, CreateIndicatorMetadataRequestBody, never>(API_PATH.metadataList, async ({ request }) => {
     const newMetadata = await request.json();
     mockDB.postMetadataList(newMetadata);
-    // await delay();
+    await delayForDevelopment();
     return HttpResponse.json({
       status: 200,
     });
   }),
-  http.get(API_PATH.indicatorList, () => {
+  http.get(API_PATH.indicatorList, async () => {
+    await delayForDevelopment();
     return HttpResponse.json(mockDB.getIndicatorList());
   }),
-  http.get<metadataParam>(`${API_PATH.metadata}/:metadataId`, ({ params }) => {
+  http.get<metadataParam>(`${API_PATH.metadata}/:metadataId`, async ({ params }) => {
     const { metadataId } = params;
 
     const metadata = mockDB.getMetadata(metadataId);
-
+    await delayForDevelopment();
     if (!metadata) {
       return HttpResponse.json(null, { status: 400 });
     }
@@ -37,17 +44,19 @@ export const handlers = [
       const { metadataId } = params;
       const indicator = await request.json();
       mockDB.postIndicatorToMetadata(metadataId, indicator);
+      await delayForDevelopment();
 
       return HttpResponse.json({ status: 200 });
     },
   ),
-  http.delete<metadataParam & indicatorParam>(`${API_PATH.metadata}/:metadataId/:indicatorKey`, ({ params }) => {
+  http.delete<metadataParam & indicatorParam>(`${API_PATH.metadata}/:metadataId/:indicatorKey`, async ({ params }) => {
     const { metadataId, indicatorKey } = params;
     mockDB.deleteIndicatorFromMetadata(metadataId, indicatorKey);
+    await delayForDevelopment();
 
     return HttpResponse.json({ status: 200 });
   }),
-  http.get(API_PATH.indicatorValue, ({ request }) => {
+  http.get(API_PATH.indicatorValue, async ({ request }) => {
     const url = new URL(request.url);
     const indicatorKey = url.searchParams.get('ticker');
     if (indicatorKey === null) {
@@ -55,6 +64,7 @@ export const handlers = [
     }
     const indicatorValue = mockDB.getIndicatorValue(indicatorKey);
     // Risk: 해당하는 값이 없을 때는? 에러 아니면 빈 객체?
+    await delayForDevelopment();
     if (!indicatorValue) {
       return HttpResponse.json(null, { status: 400 });
     }
