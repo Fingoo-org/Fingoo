68a899f63250d487eb565d8919c26e7239995d22
SeonghunYang <didtjdgns852@gmail.com>
2024-02-04 05:41:28 +0000

    [EI-118] [web] refactor: Refactor list item and list components

--- web/app/ui/components/view/atom/list-item.tsx
+++ web/app/ui/components/view/atom/list-item.tsx
@@ -6,13 +6,11 @@ type SelectableListItemProps = {
 
 export default function ListItem({ children, style }: PropsWithChildren<SelectableListItemProps>) {
   return (
-    <li>
-      <div
-        style={style}
-        className="bg-gray-100 hover:text-blue-700 hover:bg-blue-50 rounded flex p-4 h-full items-center font-medium"
-      >
-        {children}
-      </div>
-    </li>
+    <div
+      style={style}
+      className="bg-gray-100 hover:text-blue-700 hover:bg-blue-50 rounded flex p-4 h-full items-center font-medium"
+    >
+      {children}
+    </div>
   );
 }
