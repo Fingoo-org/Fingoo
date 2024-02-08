c82a3e8900c0c747b9c983beabb62cd8b467f82a
나경호 <gangho324@gmail.com>
2024-02-08 15:37:03 +0900

    [EI-131] [api] refactor: data mapper 패턴 적용

--- api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { CreateIndicatorBoardMetadataPort } from '../../../application/port/persistent/create-indicator-board-metadata.port';
+import { CreateIndicatorBoardMetadataPort } from '../../../application/port/persistence/create-indicator-board-metadata.port';
 import { IndicatorBoardMetadata } from '../../../domain/indicator-board-metadata';
 
 import { IndicatorBoardMetadataEntity } from './entity/indicator-board-metadata.entity';
 import { IndicatorBoardMetadataMapper } from './mapper/indicator-board-metadata.mapper';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthService } from '../../../../auth/auth.service';
 
 @Injectable()
 export class IndicatorBoardMetadataPersistentAdapter implements CreateIndicatorBoardMetadataPort {
-  async createIndicatorBoardMetaData(indicatorBoardMetaData: IndicatorBoardMetadata): Promise<number> {
-    const indicatorBoardMetaDataEntity: IndicatorBoardMetadataEntity =
-      IndicatorBoardMetadataMapper.mapDomainToEntity(indicatorBoardMetaData);
-    await indicatorBoardMetaDataEntity.save();
+  constructor(
+    @InjectRepository(IndicatorBoardMetadataEntity)
+    private readonly indicatorBoardMetadataRepository: Repository<IndicatorBoardMetadataEntity>,
+    private readonly authService: AuthService,
+  ) {}
+
+  async createIndicatorBoardMetaData(
+    indicatorBoardMetaData: IndicatorBoardMetadata,
+    memberId: number,
+  ): Promise<string> {
+    const member = await this.authService.findById(memberId);
+
+    const indicatorBoardMetaDataEntity: IndicatorBoardMetadataEntity = IndicatorBoardMetadataMapper.mapDomainToEntity(
+      indicatorBoardMetaData,
+      member,
+    );
+    await this.indicatorBoardMetadataRepository.save(indicatorBoardMetaDataEntity);
     return indicatorBoardMetaDataEntity.id;
+  }
+
+  async findOneBy(id: string) {
+    return this.indicatorBoardMetadataRepository.findOneBy({ id });
   }
 }
