55c18dcfbc7c1dfdcd8523672f52492f1139ee75
SeonghunYang <didtjdgns852@gmail.com>
2024-02-08 10:58:14 +0000

    [EI-127] [web] feat: Refactor DialogMenu component

--- web/app/ui/components/view/molocule/dialog-menu/dialog-menu.tsx
+++ web/app/ui/components/view/molocule/dialog-menu/dialog-menu.tsx
@@ -1,18 +1,47 @@
+'use client';
+
+import { useDialogMenuStore } from '@/app/store/stores/dialog-menu.store';
+import { Transition } from '@headlessui/react';
 import { DotsHorizontalIcon } from '@heroicons/react/solid';
+import React from 'react';
 
 export default function DialogMenu() {
+  const isOpen = useDialogMenuStore((state) => state.isOpen);
+  const action = useDialogMenuStore((state) => state.action);
+
   return (
-    <div className="fixed top-16 w-56">
-      <div
-        role="dialog"
-        className="relative bg-white rounded-lg shadow-lg overflow-hidden pointer-events-auto w-32 mt-2 origin-top-left ring-1 ring-black/5 focus:outline-none"
+    <>
+      <Transition
+        as={React.Fragment}
+        show={isOpen}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
       >
-        <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
-        <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
-        <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
-        <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
-      </div>
-    </div>
+        <div className="relative pointer-events-auto z-0">
+          <div
+            onClick={() => {
+              action.close();
+            }}
+            className="fixed top-0 left-0 w-screen h-screen"
+          />
+          <div className="fixed top-16 w-56">
+            <div
+              role="dialog"
+              className="relative bg-white rounded-lg shadow-lg overflow-hidden pointer-events-auto w-32 mt-2 origin-top-left ring-1 ring-black/5 focus:outline-none"
+            >
+              <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
+              <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
+              <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
+              <DialogMenuItem icon={DotsHorizontalIcon}>Edit</DialogMenuItem>
+            </div>
+          </div>
+        </div>
+      </Transition>
+    </>
   );
 }
 
