a817196f4fbb37506b34e4ed847f5e11d52109a6
SeonghunYang <didtjdgns852@gmail.com>
2024-02-10 05:33:45 +0000

    [EI-127] [web] feat: Add useDialogMenu hook for dialog menu functionality

---
+++ web/app/utils/hooks/use-dialog-menu.ts
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react';
+import { useDialogMenuStore } from '@/app/store/stores/dialog-menu.store';
+
+export default function useDialogMenu(key: string) {
+  const action = useDialogMenuStore((state) => state.action);
+  const isOpen = useDialogMenuStore((state) => state.isOpen[key]);
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    // 사실 사용 즉시 등록이기에 필요 없긴 하다
+    if (!isOpen) {
+      action.enroll(key);
+    }
+  }, []);
+
+  const openDialogMenu = (payload?: unknown) => {
+    const position = ref.current?.getBoundingClientRect();
+
+    if (!position) {
+      return;
+    }
+
+    const newPosition = {
+      x: position.left,
+      y: position.top + position.height / 2,
+    };
+
+    action.setPayload(payload);
+    action.setPosition(newPosition);
+    action.open(key);
+  };
+
+  return {
+    ref,
+    isOpen,
+    openDialogMenu,
+  };
+}
