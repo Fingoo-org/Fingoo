9eb247e68c743f340fc7808e9164af87fc12108e
SeonghunYang <didtjdgns852@gmail.com>
2024-02-11 06:48:10 +0000

    [EI-128] [web] refactor: Refactor dialog menu UX

--- web/app/ui/components/view/molocule/dialog-menu/dialog-menu-root.tsx
+++ web/app/ui/components/view/molocule/dialog-menu/dialog-menu-root.tsx
@@ -26,25 +26,26 @@ export function DialogMenuRoot({ children, dialogKey }: React.PropsWithChildren<
   return (
     <DialogMenuContext.Provider value={dialogKey}>
       <div className="fixed inset-0 pointer-events-none	overflow-hidden	z-50">
-        <Transition
-          as={React.Fragment}
-          show={isOpen || false}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
+        <Transition as={React.Fragment} show={isOpen || false}>
           <div className="relative pointer-events-auto z-0">
             <div onClick={handleOnClick} className="fixed top-0 left-0 w-screen h-screen" />
-            <div style={position && { left: position.x, top: position.y }} className="fixed w-56">
-              <div
-                role="dialog"
-                className="relative bg-white rounded-lg shadow-lg overflow-hidden pointer-events-auto w-32 mt-2 origin-top-left ring-1 ring-black/5 focus:outline-none"
+            <div style={position && { left: position.x, top: position.y }} className="fixed">
+              <Transition.Child
+                as={React.Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                {dialogMenuItems}
-              </div>
+                <div
+                  role="dialog"
+                  className="relative bg-white rounded-lg shadow-lg overflow-hidden pointer-events-auto w-32 mt-2 origin-top-left ring-1 ring-black/5 focus:outline-none"
+                >
+                  {dialogMenuItems}
+                </div>
+              </Transition.Child>
             </div>
           </div>
         </Transition>
