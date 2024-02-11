d69b76d506b8e9c5c00f92dd4e8e3f9d98034680
SeonghunYang <didtjdgns852@gmail.com>
2024-02-11 06:26:54 +0000

    [EI-128] [web] refactor: Refactor alert dialog buttons

--- web/app/ui/components/view/molocule/alert-dialog/alert-dialog-negative-button.tsx
+++ web/app/ui/components/view/molocule/alert-dialog/alert-dialog-negative-button.tsx
@@ -3,13 +3,16 @@ import { AlertDialogContext } from './alert-dialog-context';
 import { useAlertDialog } from './use-alert-dialog.hook';
 import { DialogMenuKey } from '@/app/utils/keys/dialog-menu-key';
 
+type NativeButtonType = Omit<React.ComponentPropsWithoutRef<'button'>, 'onClick'>;
+
 type AlertDialogNegativeButtonProps = {
   onClick?: () => void;
-};
+} & NativeButtonType;
 
 export function AlertDialogNegativeButton({
   children,
   onClick,
+  ...props
 }: React.PropsWithChildren<AlertDialogNegativeButtonProps>) {
   const dialogKey = useContext(AlertDialogContext);
   const { closeDialog } = useAlertDialog(dialogKey as DialogMenuKey);
@@ -24,8 +27,10 @@ export function AlertDialogNegativeButton({
   // Refactor: 버튼 디자인 시스템화
   return (
     <button
+      {...props}
       onClick={handleClick}
       type="button"
+      aria-label="Cancel"
       className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
     >
       {children}
--- web/app/ui/components/view/molocule/alert-dialog/alert-dialog-positive-button.tsx
+++ web/app/ui/components/view/molocule/alert-dialog/alert-dialog-positive-button.tsx
@@ -3,13 +3,16 @@ import { AlertDialogContext } from './alert-dialog-context';
 import { useAlertDialog } from './use-alert-dialog.hook';
 import { DialogMenuKey } from '@/app/utils/keys/dialog-menu-key';
 
+type NativeButtonType = Omit<React.ComponentPropsWithoutRef<'button'>, 'onClick'>;
+
 type AlertDialogPositiveButtonProps = {
   onClick?: () => void;
-};
+} & NativeButtonType;
 
 export function AlertDialogPositiveButton({
   children,
   onClick,
+  ...props
 }: React.PropsWithChildren<AlertDialogPositiveButtonProps>) {
   const dialogKey = useContext(AlertDialogContext);
   const { closeDialog } = useAlertDialog(dialogKey as DialogMenuKey);
@@ -24,7 +27,9 @@ export function AlertDialogPositiveButton({
   // Refactor: 버튼 디자인 시스템화
   return (
     <button
+      {...props}
       onClick={handleClick}
+      aria-label="Confirm"
       type="button"
       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
     >
