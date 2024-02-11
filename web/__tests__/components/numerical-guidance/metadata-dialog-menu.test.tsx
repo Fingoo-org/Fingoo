ffcabdb2dd7b932b70860840692f21daf2d2f6ab
SeonghunYang <didtjdgns852@gmail.com>
2024-02-11 06:32:21 +0000

    [EI-128] [web] test: Add tests for MetadataDeleteDialog

--- web/__tests__/components/numerical-guidance/metadata-dialog-menu.test.tsx
+++ web/__tests__/components/numerical-guidance/metadata-dialog-menu.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { SWRProviderWithoutCache } from '@/app/store/querys/swr-provider';
 import { resetMockDB } from '@/app/mocks/db.mock';
 import { resetAllStore } from '@/app/store/stores/reset-store';
