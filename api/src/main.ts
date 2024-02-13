d1593cda1508b30076c58aa6745ca0b1b1300e68
yoounyoungheon <iddyoon@gmail.com>
2024-02-13 14:32:18 +0900

    [EI-95] [api] rename: http-exception-filter 파일명 변경

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { initializeTransactionalContext } from 'typeorm-transactional';
-import { HttpExceptionFilter } from './utils/exception-filter/execptionFilter';
+import { HttpExceptionFilter } from './utils/exception-filter/http-execption-filter';
 
 async function bootstrap() {
   initializeTransactionalContext();
