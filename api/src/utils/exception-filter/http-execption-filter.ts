cc267d307cd8cdf760d9adf1494a91e1116057d0
yoounyoungheon <iddyoon@gmail.com>
2024-02-14 01:27:42 +0900

    [EI-95] [api] refactor: refactoring error filter

--- api/src/utils/exception-filter/http-execption-filter.ts
+++ api/src/utils/exception-filter/http-execption-filter.ts
@@ -7,33 +7,40 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const status = exception.getStatus();
-    const error: any = exception.getResponse() as
+    const errorResponse: any = exception.getResponse() as
       | string
       | { error: string; statusCode: number; message: string | string[] };
-
-    if (typeof error === 'string') {
-      response.status(status).json({
-        successs: false,
-        timestamp: new Date().toISOString(),
-        statusCode: status,
-        url: request.url,
-        error,
-      });
-    } else {
-      response.status(status).json({
-        successs: false,
-        message: error.message,
-        timestamp: new Date().toISOString(),
-        ...error,
-      });
-    }
-
     const url: string = request.url;
     const timestamp: string = new Date().toISOString();
 
+    if (typeof errorResponse === 'string') {
+      response.status(status).json({
+        successs: false,
+        timestamp: timestamp,
+        statusCode: status,
+        url: url,
+        errorResponse,
+      });
+    } else {
+      response.status(status).json({
+        success: false,
+        message: errorResponse.message,
+        timestamp: timestamp,
+        ...errorResponse,
+      });
+    }
+
+    this.errorLogging(url, errorResponse, timestamp);
+  }
+
+  private errorLogging(
+    url: string,
+    errorResponse: string | { error: string; statusCode: number; message: string | string[] },
+    timestamp: string,
+  ) {
     console.log('요청 url: ', url);
     console.log('-------------------------------------');
-    console.log('error 정보: ', error);
+    console.log('error 정보: ', errorResponse);
     console.log('-------------------------------------');
     console.log('발생 시간: ', timestamp);
   }
