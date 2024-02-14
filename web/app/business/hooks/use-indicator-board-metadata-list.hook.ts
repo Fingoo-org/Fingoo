a13235dbe0d25b23c9cfb23d734112645c65679a
SeonghunYang <didtjdgns852@gmail.com>
2024-02-14 07:36:50 +0000

    [EI-129] [web] feat: Add deleteMetadata to useIndicatoBoardrMetadataList hook

--- web/app/business/hooks/use-indicator-board-metadata-list.hook.ts
+++ web/app/business/hooks/use-indicator-board-metadata-list.hook.ts
@@ -1,13 +1,15 @@
 import {
   CreateIndicatorMetadataRequestBody,
   useCreateIndicatorMetadata,
+  useDeleteIndicatorBoardMetadata,
 } from '../../store/querys/numerical-guidance/indicator-board-metadata.query';
 import { useFetchIndicatorBoardMetadataList } from '../../store/querys/numerical-guidance/indicator-board-metadata.query';
 import { useRef } from 'react';
 
 export const useIndicatoBoardrMetadataList = () => {
   const { data, isValidating } = useFetchIndicatorBoardMetadataList();
-  const { trigger, isMutating } = useCreateIndicatorMetadata();
+  const { trigger: deleteMetadataTrigger } = useDeleteIndicatorBoardMetadata();
+  const { trigger: createMetadataTrigger, isMutating } = useCreateIndicatorMetadata();
   const isPending = useRef(false);
 
   if (isPending.current === false) {
@@ -22,15 +24,27 @@ export const useIndicatoBoardrMetadataList = () => {
 
   const createMetadata = async (metadata: CreateIndicatorMetadataRequestBody) => {
     try {
-      await trigger(metadata);
+      await createMetadataTrigger(metadata);
     } catch {
       // error: 전역 에러 처리 or 에러 바운더리에서 처리
     }
   };
 
+  const deleteMetadata = async (metadataId: string) => {
+    deleteMetadataTrigger(metadataId, {
+      optimisticData: () => {
+        return {
+          metadataList: data?.metadataList?.filter((metadata) => metadata.id !== metadataId),
+        };
+      },
+      revalidate: false,
+    });
+  };
+
   return {
     metadataList: data?.metadataList,
-    createMetadata,
     isPending: isPending.current,
+    createMetadata,
+    deleteMetadata,
   };
 };
--- web/app/ui/components/numerical-guidance/metadata-delete-dialog.tsx
+++ web/app/ui/components/numerical-guidance/metadata-delete-dialog.tsx
@@ -1,14 +1,23 @@
 'use client';
 import { DIALOG_KEY } from '@/app/utils/keys/dialog-key';
 import AlertDialog from '../view/molocule/alert-dialog';
+import { IndicatorBoardMetadataResponse } from '@/app/store/querys/numerical-guidance/indicator-board-metadata.query';
+import { useIndicatoBoardrMetadataList } from '@/app/business/hooks/use-indicator-board-metadata-list.hook';
 
 export default function MetadataDeleteDialog() {
+  const { deleteMetadata } = useIndicatoBoardrMetadataList();
+  const handleClick = (payload: unknown) => {
+    deleteMetadata((payload as IndicatorBoardMetadataResponse).id);
+  };
+
   return (
     <AlertDialog dialogKey={DIALOG_KEY.METADATA_DELETE}>
       <AlertDialog.Title>Delete Metadata</AlertDialog.Title>
       <AlertDialog.Body>Are you sure you want to delete this metadata?</AlertDialog.Body>
       <AlertDialog.NegativeButton>Cancel</AlertDialog.NegativeButton>
-      <AlertDialog.PositiveButton color={'red'}>Delete</AlertDialog.PositiveButton>
+      <AlertDialog.PositiveButton onClick={handleClick} color={'red'}>
+        Delete
+      </AlertDialog.PositiveButton>
     </AlertDialog>
   );
 }
