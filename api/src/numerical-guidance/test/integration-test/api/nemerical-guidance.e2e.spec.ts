8e437cb2a31a9bb3b5b7bcc0b15c04708c7f1476
yoounyoungheon <iddyoon@gmail.com>
2024-02-14 15:42:00 +0900

    [EI-95] [api] test: nemerical guidance e2e test 작성

---
+++ api/src/numerical-guidance/test/integration-test/api/nemerical-guidance.e2e.spec.ts
@@ -0,0 +1,102 @@
+import { Test } from '@nestjs/testing';
+import { CqrsModule } from '@nestjs/cqrs';
+import * as request from 'supertest';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { NumericalGuidanceController } from '../../../api/numerical-guidance.controller';
+import { GetIndicatorBoardMetaDataQueryHandler } from 'src/numerical-guidance/application/query/get-indicator-board-metadata/get-indicator-board-metadata.query.handler';
+import { IndicatorBoardMetadataPersistentAdapter } from 'src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter';
+import { HttpExceptionFilter } from 'src/utils/exception-filter/http-execption-filter';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemberEntity } from 'src/auth/member.entity';
+import { IndicatorBoardMetadataEntity } from 'src/numerical-guidance/infrastructure/adapter/persistence/entity/indicator-board-metadata.entity';
+import { PostgreSqlContainer } from '@testcontainers/postgresql';
+import { AuthService } from 'src/auth/auth.service';
+import { DataSource } from 'typeorm';
+
+describe('NumericalGuidance E2E Test', () => {
+  let app: INestApplication;
+  let dataSource: DataSource;
+  let environment;
+  const seeding = async () => {
+    const indicatorBoardMetaDataRepository = dataSource.getRepository(IndicatorBoardMetadataEntity);
+    await indicatorBoardMetaDataRepository.insert({
+      id: '0d73cea1-35a5-432f-bcd1-27ae3541ba73',
+      indicatorBoardMetaDataName: 'ds',
+      indicators: { key1: ['5', '2', '3'] },
+    });
+    indicatorBoardMetaDataRepository.save;
+  };
+
+  beforeAll(async () => {
+    environment = await new PostgreSqlContainer().start();
+    const [module] = await Promise.all([
+      Test.createTestingModule({
+        imports: [
+          CqrsModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+          }),
+          TypeOrmModule.forFeature([MemberEntity, IndicatorBoardMetadataEntity]),
+          TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: () => ({
+              type: 'postgres',
+              retryAttempts: 20,
+              retryDelay: 5000,
+              host: environment.getHost(),
+              port: environment.getPort(),
+              username: environment.getUsername(),
+              password: environment.getPassword(),
+              database: environment.getDatabase(),
+              entities: [IndicatorBoardMetadataEntity, MemberEntity],
+              synchronize: true,
+            }),
+          }),
+        ],
+        controllers: [NumericalGuidanceController],
+        providers: [
+          AuthService,
+          GetIndicatorBoardMetaDataQueryHandler,
+          {
+            provide: 'LoadIndicatorBoardMetadataPort',
+            useClass: IndicatorBoardMetadataPersistentAdapter,
+          },
+        ],
+      }).compile(),
+    ]);
+    dataSource = module.get<DataSource>(DataSource);
+    await seeding();
+    app = module.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        disableErrorMessages: false,
+      }),
+    );
+    app.useGlobalFilters(new HttpExceptionFilter());
+    await app.init();
+  }, 30000);
+
+  afterAll(async () => {
+    await environment.stop();
+    await app.close();
+  });
+
+  it('/get 메타보드 id를 전송해서 id에 해당하는 메타보를 가져온다.', async () => {
+    return request(app.getHttpServer())
+      .get(`/numerical-guidance/indicator-board-metadata/0d73cea1-35a5-432f-bcd1-27ae3541ba73`)
+      .set('Content-Type', 'application/json')
+      .expect(HttpStatus.OK);
+  });
+
+  it('/get 유효하지 않은 메타데이터 id를 전송한다.', () => {
+    return request(app.getHttpServer())
+      .get('/numerical-guidance/indicator-board-metadata/invlid-id')
+      .set('Content-Type', 'application/json')
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+});
