1e1f7f4de0ff5fecfbd0ed5929bf45e6ba3836d4
나경호 <gangho324@gmail.com>
2024-02-16 22:00:07 +0900

    [EI-132] [api] feat: 지표보드 메타데이터에 지표 ticker 추가 adapter

--- api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
@@ -8,10 +8,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthService } from '../../../../auth/auth.service';
 import { LoadIndicatorBoardMetadataPort } from 'src/numerical-guidance/application/port/persistence/load-indiactor-board-metadata.port';
+import { InsertIndicatorTickerPort } from '../../../application/port/persistence/insert-indicator-ticker.port';
 
 @Injectable()
 export class IndicatorBoardMetadataPersistentAdapter
-  implements CreateIndicatorBoardMetadataPort, LoadIndicatorBoardMetadataPort
+  implements CreateIndicatorBoardMetadataPort, LoadIndicatorBoardMetadataPort, InsertIndicatorTickerPort
 {
   constructor(
     @InjectRepository(IndicatorBoardMetadataEntity)
@@ -23,30 +24,50 @@ export class IndicatorBoardMetadataPersistentAdapter
     indicatorBoardMetaData: IndicatorBoardMetadata,
     memberId: number,
   ): Promise<string> {
-    const member = await this.authService.findById(memberId);
-
-    const indicatorBoardMetaDataEntity: IndicatorBoardMetadataEntity = IndicatorBoardMetadataMapper.mapDomainToEntity(
-      indicatorBoardMetaData,
-      member,
-    );
-    await this.indicatorBoardMetadataRepository.save(indicatorBoardMetaDataEntity);
-    return indicatorBoardMetaDataEntity.id;
-  }
-
-  async loadIndicatorBoardMetaData(id: string): Promise<IndicatorBoardMetadata> {
     try {
-      const indicatorMetaDataEintity = await this.findOneBy(id);
-      const indicatorBoardMetaData = await IndicatorBoardMetadataMapper.mapEntityToDomain(indicatorMetaDataEintity);
-      return indicatorBoardMetaData;
+      const member = await this.authService.findById(memberId);
+
+      const indicatorBoardMetaDataEntity: IndicatorBoardMetadataEntity = IndicatorBoardMetadataMapper.mapDomainToEntity(
+        indicatorBoardMetaData,
+        member,
+      );
+      await this.indicatorBoardMetadataRepository.save(indicatorBoardMetaDataEntity);
+      return indicatorBoardMetaDataEntity.id;
     } catch (error) {
       throw new BadRequestException({
-        message: 'invalid id',
+        message: '[ERROR] createIndicatorBoardMetaData 중에 문제가 발생했습니다.',
         error: error,
       });
     }
   }
 
-  async findOneBy(id: string) {
-    return this.indicatorBoardMetadataRepository.findOneBy({ id });
+  async loadIndicatorBoardMetaData(id: string): Promise<IndicatorBoardMetadata> {
+    try {
+      const indicatorMetaDataEntity = await this.indicatorBoardMetadataRepository.findOneBy({ id });
+      return await IndicatorBoardMetadataMapper.mapEntityToDomain(indicatorMetaDataEntity);
+    } catch (error) {
+      throw new BadRequestException({
+        message: '[ERROR] loadIndicatorBoardMetaData 중에 문제가 발생했습니다.',
+        error: error,
+      });
+    }
+  }
+
+  async addIndicatorTicker(indicatorBoardMetaData: IndicatorBoardMetadata): Promise<void> {
+    try {
+      const id = indicatorBoardMetaData.id;
+
+      const indicatorBoardMetaDataEntity: IndicatorBoardMetadataEntity =
+        await this.indicatorBoardMetadataRepository.findOneBy({ id });
+
+      indicatorBoardMetaDataEntity.tickers = indicatorBoardMetaData.tickers;
+
+      await this.indicatorBoardMetadataRepository.save(indicatorBoardMetaDataEntity);
+    } catch (error) {
+      throw new BadRequestException({
+        message: '[ERROR] insertIndicator 중에 문제가 발생했습니다.',
+        error: error,
+      });
+    }
   }
 }
