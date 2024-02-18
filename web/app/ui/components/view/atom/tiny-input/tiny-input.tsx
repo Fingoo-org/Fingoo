23be58af12c538355914e7ef47be433a301194c7
SeonghunYang <didtjdgns852@gmail.com>
2024-02-18 04:12:32 +0000

    [EI-133] [web] feat: Add TinyInput component and its stories

---
+++ web/app/ui/components/view/atom/tiny-input/tiny-input.tsx
@@ -0,0 +1,30 @@
+import IconButton from '../icon-button/icon-button';
+import { XCircleIcon } from '@heroicons/react/solid';
+
+type TinyInputProps = {
+  value: string;
+  withResetButton?: boolean;
+  onValueChange?: (value: string) => void;
+};
+
+export default function TinyInput({ value, withResetButton = false, onValueChange }: TinyInputProps) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    onValueChange?.(value);
+  };
+
+  const handleReset = () => {
+    onValueChange?.('');
+  };
+
+  return (
+    <div className="bg-white flex w-full rounded-lg text-sm has-[:focus]:ring-2 has=[:focus]:ring-blue-200 duration-100 ">
+      <input
+        value={value}
+        onChange={handleChange}
+        className="pl-2 py-0.5 focus:outline-none rounded-lg text-custom-inherit focus:ring-0 focus:ring-offset-0 border-none w-full p-0"
+      />
+      {withResetButton ? <IconButton color={'gray'} icon={XCircleIcon} size={'xs'} onClick={handleReset} /> : null}
+    </div>
+  );
+}
