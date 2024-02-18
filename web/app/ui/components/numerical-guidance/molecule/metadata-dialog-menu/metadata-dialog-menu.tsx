6532a2b116456020067d0c6dbd22921676b9efd0
SeonghunYang <didtjdgns852@gmail.com>
2024-02-18 05:41:38 +0000

    [EI-133] [web] feat: Add useIndicatorBoardMetadata hook and update metadata in MetadataDialogMenu

--- web/app/ui/components/numerical-guidance/molecule/metadata-dialog-menu/metadata-dialog-menu.tsx
+++ web/app/ui/components/numerical-guidance/molecule/metadata-dialog-menu/metadata-dialog-menu.tsx
@@ -5,16 +5,25 @@ import { DIALOG_KEY } from '@/app/utils/keys/dialog-key';
 import { useAlertDialog } from '../../../view/molocule/alert-dialog/use-alert-dialog.hook';
 import TinyInput from '../../../view/atom/tiny-input/tiny-input';
 import { IndicatorBoardMetadataResponse } from '@/app/store/querys/numerical-guidance/indicator-board-metadata.query';
+import { useIndicatorBoardMetadata } from '@/app/business/hooks/use-indicator-board-metadata.hook';
 
 export default function MetadataDialogMenu() {
   const { payload, openDialogWithPayload } = useAlertDialog(DIALOG_KEY.METADATA_DELETE);
+  const { metadata, updateMetadata } = useIndicatorBoardMetadata(
+    typeof payload !== 'undefined' ? (payload as IndicatorBoardMetadataResponse).id : undefined,
+  );
+
+  const handleMetadataUpdate = (name: string) => {
+    updateMetadata({ name });
+  };
 
   return (
     <DialogMenu size={'md'} dialogKey={DIALOG_KEY.METADATA_EDIT_MENU}>
       <DialogMenu.Header>
         <TinyInput
-          value={typeof payload !== 'undefined' ? (payload as IndicatorBoardMetadataResponse).name : ''}
+          value={metadata !== undefined ? metadata.name : ''}
           withResetButton={true}
+          onValueChange={handleMetadataUpdate}
         />
       </DialogMenu.Header>
       <DialogMenu.Item role={'menuitem'} aria-label="Delete" onClick={openDialogWithPayload} icon={TrashIcon}>
