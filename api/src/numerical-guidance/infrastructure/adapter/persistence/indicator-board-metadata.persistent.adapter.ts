f1e50fd2dce531faba81be5c6055e0e30175ce9f
나경호 <gangho324@gmail.com>
2024-02-19 17:02:55 +0900

    [EI-139] [api] refactor: 지표보드 메타데이터 삭제 adapter 예외처리

--- api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata.persistent.adapter.ts
@@ -1,16 +1,23 @@
-import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateIndicatorBoardMetadataPort } from '../../../application/port/persistence/create-indicator-board-metadata.port';
 import { IndicatorBoardMetadata } from '../../../domain/indicator-board-metadata';
 
 import { IndicatorBoardMetadataEntity } from './entity/indicator-board-metadata.entity';
 import { IndicatorBoardMetadataMapper } from './mapper/indicator-board-metadata.mapper';
 import { InjectRepository } from '@nestjs/typeorm';
-import { QueryFailedError, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { AuthService } from '../../../../auth/auth.service';
 import { LoadIndicatorBoardMetadataPort } from 'src/numerical-guidance/application/port/persistence/load-indiactor-board-metadata.port';
 import { InsertIndicatorTickerPort } from '../../../application/port/persistence/insert-indicator-ticker.port';
 import { TypeORMError } from 'typeorm/error/TypeORMError';
 import { DeleteIndicatorTickerPort } from '../../../application/port/persistence/delete-indicator-ticker.port';
+import { DeleteIndicatorBoardMetadataPort } from '../../../application/port/persistence/delete-indicator-board-metadata.port';
 
 @Injectable()
 export class IndicatorBoardMetadataPersistentAdapter
@@ -18,7 +25,8 @@ export class IndicatorBoardMetadataPersistentAdapter
     CreateIndicatorBoardMetadataPort,
     LoadIndicatorBoardMetadataPort,
     InsertIndicatorTickerPort,
-    DeleteIndicatorTickerPort
+    DeleteIndicatorTickerPort,
+    DeleteIndicatorBoardMetadataPort
 {
   constructor(
     @InjectRepository(IndicatorBoardMetadataEntity)
@@ -45,13 +53,14 @@ export class IndicatorBoardMetadataPersistentAdapter
         throw new NotFoundException({
           message: '[ERROR] 해당 회원을 찾을 수 없습니다.',
           error: error,
+          HttpStatus: HttpStatus.NOT_FOUND,
         });
       } else {
         throw new InternalServerErrorException({
           message: `[ERROR] 지표보드 메타데이터를 생성하는 도중에 오류가 발생했습니다. 다음과 같은 상황을 확인해보세요.
-          1. indicatorBoardMetaData 값 중 비어있는 값이 있는가
-          `,
+          1. indicatorBoardMetaData 값 중 비어있는 값이 있는지 확인해주세요.`,
           error: error,
+          HttpStatus: HttpStatus.INTERNAL_SERVER_ERROR,
         });
       }
     }
@@ -67,13 +76,14 @@ export class IndicatorBoardMetadataPersistentAdapter
         throw new NotFoundException({
           message: '[ERROR] 해당 지표보드 메타데이터를 찾을 수 없습니다.',
           error: error,
+          HttpStatus: HttpStatus.NOT_FOUND,
         });
-      } else if (error instanceof QueryFailedError) {
+      } else if (error instanceof TypeORMError) {
         throw new BadRequestException({
-          message: `[ERROR] 지표보드 메타데이터를 불러오는 도중에 오류가 발생했습니다. 다음을 확인해보세요.
-          1. id 값이 uuid 형식을 잘 따르고 있는가
-          `,
+          message: `[ERROR] 지표보드 메타데이터를 불러오는 도중에 오류가 발생했습니다.
+          1. id 값이 uuid 형식을 잘 따르고 있는지 확인해주세요.`,
           error: error,
+          HttpStatus: HttpStatus.BAD_REQUEST,
         });
       } else {
         throw new InternalServerErrorException({
@@ -102,8 +112,8 @@ export class IndicatorBoardMetadataPersistentAdapter
           error: error,
         });
       } else if (error instanceof TypeORMError) {
-        throw new InternalServerErrorException({
-          message: '[ERROR] 지표보드 메타데이터를 업데이트하는 도중에 오류가 발생했습니다.',
+        throw new BadRequestException({
+          message: '[ERROR] 지표보드 메타데이터를 업데이트하는 도중에 entity 오류가 발생했습니다.',
           error: error,
         });
       } else {
@@ -133,8 +143,9 @@ export class IndicatorBoardMetadataPersistentAdapter
           error: error,
         });
       } else if (error instanceof TypeORMError) {
-        throw new InternalServerErrorException({
-          message: '[ERROR] 지표보드 메타데이터를 업데이트하는 도중에 오류가 발생했습니다.',
+        throw new BadRequestException({
+          message: `[ERROR] 지표보드 메타데이터 지표 ticker를 삭제하는 도중에 entity 오류가 발생했습니다.
+          1. id 값이 uuid 형식을 잘 따르고 있는지 확인해주세요.`,
           error: error,
         });
       } else {
