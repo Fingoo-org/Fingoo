0b5b977f350c4ffd12101f6bca57c71281be0f41
yoounyoungheon <iddyoon@gmail.com>
2024-02-23 16:28:16 +0900

    [EI-138] [api] chore: seeding 수정

--- api/src/utils/database/seeds/data-source.ts
+++ api/src/utils/database/seeds/data-source.ts
@@ -1,19 +1,23 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 import { ConfigService } from '@nestjs/config';
+import { SeederOptions } from 'typeorm-extension';
 
 config();
 
 const configService = new ConfigService();
 
-export default new DataSource({
+const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: configService.get<string>('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
   username: configService.get<string>('POSTGRES_USER'),
   password: configService.get<string>('POSTGRES_PASSWORD'),
   database: configService.get<string>('POSTGRES_USER'),
-  entities: ['src/**/**/*.entity.{ts,js}'],
+  entities: ['src/**/*.entity.{ts,js}'],
   logging: false,
   synchronize: true,
-});
+  seeds: ['src/utils/database/seeds/**/*{.ts,.js}'],
+};
+
+export default new DataSource(options);
