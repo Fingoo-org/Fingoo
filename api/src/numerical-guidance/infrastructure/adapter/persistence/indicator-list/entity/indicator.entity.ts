addff33219c7057c2e2dd0351b8f6a6a96b0b8eb
나경호 <gangho324@gmail.com>
2024-02-23 14:15:28 +0900

    [EI-152] [api] refactor: table명 변경

--- api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-list/entity/indicator.entity.ts
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-list/entity/indicator.entity.ts
@@ -2,7 +2,7 @@ import { IndicatorType, Market } from 'src/utils/type/type-definition';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { BaseEntity } from '../../base.entity';
 
-@Entity()
+@Entity({ name: 'Indicator' })
 export class IndicatorEntity extends BaseEntity {
   @PrimaryColumn()
   id: number;
