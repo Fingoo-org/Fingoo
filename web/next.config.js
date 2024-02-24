e8ac6a0e1e07dc5e79afc719bcce96608de9791f
SeonghunYang <didtjdgns852@gmail.com>
2024-02-24 10:14:57 +0000

    [EI-153] [web] chore: Disable React strict mode for react-financial-chart

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: false, // This is to use react-finanical-chart
 };
 
 module.exports = nextConfig;
