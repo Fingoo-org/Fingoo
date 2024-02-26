a6fd4190acba096e36e0105c2fca83043a9a8ee8
SeonghunYang <didtjdgns852@gmail.com>
2024-02-26 06:49:58 +0000

    [EI-153] [web] feat: Add util component to advanced multi-line chart

--- web/app/ui/components/view/molocule/advanced-multi-line-chart/advanced-multi-line-chart.tsx
+++ web/app/ui/components/view/molocule/advanced-multi-line-chart/advanced-multi-line-chart.tsx
@@ -9,6 +9,9 @@ import {
   YAxis,
   MouseCoordinateY,
   MouseCoordinateX,
+  CrossHairCursor,
+  CurrentCoordinate,
+  SingleValueTooltip,
 } from 'react-financial-charts';
 import { format } from 'd3-format';
 import { timeFormat } from 'd3-time-format';
@@ -29,6 +32,14 @@ export default function AdvancedMultiLineChart<T extends Record<string, any>>({
 
   const { data: scaledData, xScale, xAccessor, displayXAccessor } = ScaleProvider(data);
 
+  const maxDate = scaledData[scaledData.length - 1];
+  const minDate = scaledData[Math.max(0, scaledData.length - 40)];
+  const xExtents = [xAccessor(minDate), xAccessor(maxDate)];
+
+  // const handleLoadBefore = (start: number, end: number) => {
+  //   const rowsToDownload = end - Math.ceil(start);
+  // };
+
   const yExtents = (d: T) => {
     return Object.keys(d).reduce(
       (acc, key) => {
@@ -42,33 +53,46 @@ export default function AdvancedMultiLineChart<T extends Record<string, any>>({
   const renderLienSeries = () => {
     return Object.keys(scaledData[0]).map((key, idx) => {
       if (typeof scaledData[0][key] === 'number') {
-        return <LineSeries key={key} strokeStyle={colors[idx]} yAccessor={(d) => d[key]} />;
+        return (
+          <>
+            <CurrentCoordinate fillStyle={colors[idx]} strokeStyle={colors[idx]} yAccessor={(d) => d[key]} />
+            <LineSeries key={key} strokeStyle={colors[idx]} yAccessor={(d) => d[key]} />
+            ;
+            <SingleValueTooltip
+              yAccessor={(d) => d[key]}
+              yLabel={key}
+              yDisplayFormat={format('.2f')}
+              valueFill={colors[idx]}
+              labelFill={colors[idx]}
+              origin={[8, 16 * (idx + 1)]}
+            />
+          </>
+        );
       }
     });
   };
 
-  const max = xAccessor(scaledData[scaledData.length - 1]);
-  const min = xAccessor(scaledData[Math.max(0, scaledData.length - 100)]);
-  const xExtents = [min, max + 5];
-
   return (
     <div ref={containerRef} className="h-full w-full">
       <ChartCanvas
-        // xExtents={xExtents}
+        xExtents={xExtents}
         xScale={xScale}
         data={scaledData}
         displayXAccessor={displayXAccessor}
+        disableZoom={true}
         xAccessor={xAccessor}
         width={sizes.containerWidth}
         margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
         height={sizes.containerHeight}
         seriesName="data"
         ratio={1}
+        onLoadBefore={handleLoadBefore}
       >
         <Chart id={1} height={sizes.containerHeight - 50} yExtents={yExtents}>
           <XAxis showTicks={false} showGridLines axisAt="bottom" orient="bottom" />
           <YAxis />
           {renderLienSeries()}
+          <CrossHairCursor />
           <MouseCoordinateY at="right" orient="right" displayFormat={format('.2f')} />
           <MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat('%Y-%m-%d')} />
         </Chart>
