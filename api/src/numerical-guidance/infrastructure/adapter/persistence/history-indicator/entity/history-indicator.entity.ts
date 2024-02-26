979d16d7fb728d5faa4378e1deff33c107caddeb
나경호 <gangho324@gmail.com>
2024-02-26 23:50:27 +0900

    [EI-151] [api] feat: history-indicator.entity 추가

---
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/history-indicator/entity/history-indicator.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { HistoryIndicatorValueEntity } from '../../history-indicator-value/entity/history-indicator-value.entity';
+import { IndicatorType, Market } from '../../../../../../utils/type/type-definition';
+
+@Entity({ name: 'historyIndicator' })
+export class HistoryIndicatorEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  type: IndicatorType;
+
+  @Column()
+  ticker: string;
+
+  @Column()
+  market: Market;
+
+  @OneToMany(
+    () => HistoryIndicatorValueEntity,
+    (historyIndicatorValueEntity) => historyIndicatorValueEntity.historyIndicator,
+  )
+  values: HistoryIndicatorValueEntity[];
+}
