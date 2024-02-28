29c136a4aa208b2648e626f0f1d3e1d9f8b9990d
SeonghunYang <didtjdgns852@gmail.com>
2024-02-28 02:46:51 +0000

    [EI-160] [web] feat: Add transitionToCustomForecastTab function to useIndicatorBoard hook

--- web/app/business/hooks/use-indicator-board.hook.ts
+++ web/app/business/hooks/use-indicator-board.hook.ts
@@ -3,9 +3,15 @@ import { useNumericalGuidanceStore } from '@/app/store/stores/numerical-guidance
 export const useIndicatorBoard = () => {
   const isAdvancedChart = useNumericalGuidanceStore((state) => state.isAdvancedChart);
   const { setIsAdvancedChart } = useNumericalGuidanceStore((state) => state.actions);
+  const { setTabIndex } = useNumericalGuidanceStore((state) => state.actions);
+
+  const transitionToCustomForecastTab = () => {
+    setTabIndex(1);
+  };
 
   return {
     isAdvancedChart,
     setIsAdvancedChart,
+    transitionToCustomForecastTab,
   };
 };
--- web/app/ui/components/numerical-guidance/molecule/indicator-dialog-menu.tsx
+++ web/app/ui/components/numerical-guidance/molecule/indicator-dialog-menu.tsx
@@ -3,9 +3,15 @@
 import { DIALOG_KEY } from '@/app/utils/keys/dialog-key';
 import DialogMenu from '../../view/molocule/dialog-menu';
 import { PlusCircleIcon } from '@heroicons/react/outline';
+import { useNumericalGuidanceStore } from '@/app/store/stores/numerical-guidance.store';
+import { useIndicatorBoard } from '@/app/business/hooks/use-indicator-board.hook';
 
 export default function IndicatorDialogMenu() {
-  const handleCustomForecastIndicatorCreate = () => {};
+  const { transitionToCustomForecastTab } = useIndicatorBoard();
+
+  const handleCustomForecastIndicatorCreate = () => {
+    transitionToCustomForecastTab();
+  };
 
   return (
     <DialogMenu size={'md'} dialogKey={DIALOG_KEY.INDICATOR_EDIT_MENU}>
