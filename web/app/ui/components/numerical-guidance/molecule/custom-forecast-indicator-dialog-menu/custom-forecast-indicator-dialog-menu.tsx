17f0549cae25fe061e8ebd58382f31ef0bf38e9e
SeonghunYang <didtjdgns852@gmail.com>
2024-03-01 10:25:24 +0000

    [EI-161] [web] feat: Add source indicator slider components

--- web/app/ui/components/numerical-guidance/molecule/custom-forecast-indicator-dialog-menu/custom-forecast-indicator-dialog-menu.tsx
+++ web/app/ui/components/numerical-guidance/molecule/custom-forecast-indicator-dialog-menu/custom-forecast-indicator-dialog-menu.tsx
@@ -6,6 +6,7 @@ import SourceIndicatorSearchList from '../source-indicator-search-list';
 import { Card } from '@tremor/react';
 import { useSelectedCustomForecastIndicatorViewModel } from '@/app/business/hooks/custom-forecast-indicator/use-selected-custom-forecast-indicator-view-model';
 import SourceIndicatorBadgeGroup from '../source-indicator-badge-group';
+import SourceIndicatorSliderGroup from '../source-indicator-slider-group';
 
 export default function CustomForecastIndicatorDialogMenu() {
   const { selectedCustomForecastIndicator } = useSelectedCustomForecastIndicatorViewModel();
@@ -34,6 +35,10 @@ export default function CustomForecastIndicatorDialogMenu() {
         <Card className="h-28 p-1.5">
           <SourceIndicatorSearchList />
         </Card>
+      </DialogMenu.Content>
+      <DialogMenu.Content>
+        <div className="text-xs font-bold	">가중치</div>
+        <SourceIndicatorSliderGroup />
       </DialogMenu.Content>
     </DialogMenu>
   );
