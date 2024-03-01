aa22a47c04e67210a590dff9a7fd0be39cc79289
SeonghunYang <didtjdgns852@gmail.com>
2024-03-01 10:40:20 +0000

    [EI-161] [web] feat: Add IconButton and XCircleIcon to SourceIndicatorBadgeGroup

--- web/app/ui/components/numerical-guidance/molecule/source-indicator-badge-group.tsx
+++ web/app/ui/components/numerical-guidance/molecule/source-indicator-badge-group.tsx
@@ -1,5 +1,7 @@
 import { useSelectedCustomForecastIndicatorViewModel } from '@/app/business/hooks/custom-forecast-indicator/use-selected-custom-forecast-indicator-view-model';
 import Badge from '../../view/atom/badge/badge';
+import IconButton from '../../view/atom/icons/icon-button';
+import { XCircleIcon } from '@heroicons/react/solid';
 
 export default function SourceIndicatorBadgeGroup() {
   const { sourceIndicatorList } = useSelectedCustomForecastIndicatorViewModel();
@@ -7,9 +9,14 @@ export default function SourceIndicatorBadgeGroup() {
   return (
     <div className="flex flex-wrap space-x-2">
       {sourceIndicatorList?.map((sourceIndicator) => {
+        const handleClick = () => {
+          // logic: 재료 지표 삭제
+          console.log('handleClick');
+        };
         return (
-          <Badge variant={'outline'} key={sourceIndicator.id}>
+          <Badge className="pr-0.5" variant={'outline'} key={sourceIndicator.id}>
             {sourceIndicator.ticker}
+            <IconButton color={'gray'} icon={XCircleIcon} size={'xs'} onClick={handleClick} />
           </Badge>
         );
       })}
