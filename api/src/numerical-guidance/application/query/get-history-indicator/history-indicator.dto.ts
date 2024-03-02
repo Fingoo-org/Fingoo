d9c3ba6838987433a26cc8b727ec175049ca40f7
나경호 <gangho324@gmail.com>
2024-03-02 09:43:58 +0900

    [EI-151] [api] feat: history 지표 응답 dto

---
+++ api/src/numerical-guidance/application/query/get-history-indicator/history-indicator.dto.ts
@@ -0,0 +1,30 @@
+import { Indicator } from '../get-indicator/indicator.dto';
+import { IndicatorValue } from '../get-fluctuatingIndicator/fluctuatingIndicator.dto';
+
+export type HistoryIndicatorValue = {
+  date: Date;
+  close: string;
+  compare: string;
+  fluctuation: string;
+  open: string;
+  high: string;
+  low: string;
+  volume: string;
+  tradingValue: string;
+  marketCapitalization: string;
+  outstandingShares: string;
+};
+
+export class HistoryIndicatorDto {
+  indicator: Indicator;
+  values: IndicatorValue[];
+
+  private constructor(indicator: Indicator, values: IndicatorValue[]) {
+    this.indicator = indicator;
+    this.values = values;
+  }
+
+  static create(indicator: Indicator, values: IndicatorValue[]): HistoryIndicatorDto {
+    return new HistoryIndicatorDto(indicator, values);
+  }
+}
