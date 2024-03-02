e5e1120ba33fc34c5401a3365272207905a032c6
yoounyoungheon <iddyoon@gmail.com>
2024-03-03 03:35:37 +0900

    [EI-47] [api] feat: custom-forecast-indicator: entity

---
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/custom-forecast-indicator/entity/custom-forecast-indicator.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../base.entity';
+import { IndicatorType } from 'src/utils/type/type-definition';
+
+@Entity({ name: 'CustomForecastIndicator' })
+export class CustomForecastIndicatorEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  customForecastIndicatorName: string;
+
+  @Column()
+  type: IndicatorType;
+
+  @Column()
+  targetIndicatorId: string;
+
+  @Column('jsonb', { nullable: true })
+  grangerVerification: string[];
+
+  @Column('jsonb', { nullable: true })
+  cointJohansenVerification: string[];
+
+  @Column('jsonb', { nullable: true })
+  sourceIndicatorIdsAndWeights: string[];
+}
