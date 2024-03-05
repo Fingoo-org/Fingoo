131d5a372f79552ebd056dac59ba2dc35c8de94f
yoounyoungheon <iddyoon@gmail.com>
2024-03-05 15:11:53 +0900

    [EI-47] [api] refactor: Domain test에서 객체 생성할 때 실제 uuid로 변경

--- api/src/numerical-guidance/test/unit-test/domain/custom-forecast-indicator.spec.ts
+++ api/src/numerical-guidance/test/unit-test/domain/custom-forecast-indicator.spec.ts
@@ -6,11 +6,22 @@ describe('Custom forecast indicator', () => {
     // given
 
     // when
-    const customForecastIndicator = CustomForecastIndicator.createNew('예측지표 이름', '목표지표 uuid');
+    const customForecastIndicator = CustomForecastIndicator.createNew(
+      '예측지표 이름',
+      'f5206520-da94-11ee-b91b-3551e6db3bbd',
+    );
 
     // then
     const type: IndicatorType = 'CustomForecastIndicator';
-    const expected = new CustomForecastIndicator('uuid', '예측지표 이름', type, '목표지표 uuid', [], [], []);
+    const expected = new CustomForecastIndicator(
+      'uuid',
+      '예측지표 이름',
+      type,
+      'f5206520-da94-11ee-b91b-3551e6db3bbd',
+      [],
+      [],
+      [],
+    );
     expect(expected.customForecastIndicatorName).toEqual(customForecastIndicator.customForecastIndicatorName);
     expect(expected.targetIndicatorId).toEqual(customForecastIndicator.targetIndicatorId);
   });
