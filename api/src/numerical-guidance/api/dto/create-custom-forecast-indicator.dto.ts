468dfc94bc520cc8bee74640ad1e2e10bdfdb1b4
yoounyoungheon <iddyoon@gmail.com>
2024-03-05 15:39:51 +0900

    [EI-47] [api] test: CreateCustomForecatIndicatorDto ApiProperty uuid 예시 실제 uui로 변경

--- api/src/numerical-guidance/api/dto/create-custom-forecast-indicator.dto.ts
+++ api/src/numerical-guidance/api/dto/create-custom-forecast-indicator.dto.ts
@@ -6,7 +6,7 @@ export class CreateCustomForecatIndicatorDto {
   @IsString()
   customForecastIndicatorName: string;
 
-  @ApiProperty({ example: 'uuid example', description: '목표 지표 id' })
+  @ApiProperty({ example: '0d73cea1-35a5-432f-bcd1-27ae3541ba60', description: '목표 지표 id' })
   @IsString()
   targetIndicatorId: string;
 }
