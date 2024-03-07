032c54fb6dd05374b7e84688c826123bed456d68
SeonghunYang <didtjdgns852@gmail.com>
2024-03-07 05:37:13 +0000

    [EI-161] [web] chore: Update font in layout and tailwind.config

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import MSWComponent from './mocks/msw-component.mock';
 import { SWRProvider } from './store/querys/swr-provider';
+import { cn } from './utils/style';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(fontSans.variable)}>
         <MSWComponent>
           <SWRProvider>{children}</SWRProvider>
         </MSWComponent>
