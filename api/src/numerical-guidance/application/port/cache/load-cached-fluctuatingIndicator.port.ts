63b9a00b155bcd9bcfde208861ac477951ab744d
나경호 <gangho324@gmail.com>
2024-03-10 20:01:42 +0900

    [EI-170] [api] refactor: 사용하지 않는 live indicator api 제거

--- api/src/numerical-guidance/application/port/cache/load-cached-fluctuatingIndicator.port.ts
+++
@@ -1,5 +0,0 @@
-import { FluctuatingIndicatorDto } from '../../query/get-fluctuatingIndicator/fluctuatingIndicator.dto';
-
-export interface LoadCachedFluctuatingIndicatorPort {
-  loadCachedFluctuatingIndicator(ticker: string): Promise<FluctuatingIndicatorDto | null>;
-}
