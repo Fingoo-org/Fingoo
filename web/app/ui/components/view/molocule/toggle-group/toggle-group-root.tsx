277930cd7434817ab3afda4dc1a3c20311050d59
SeonghunYang <didtjdgns852@gmail.com>
2024-03-11 05:40:58 +0000

    [EI-161] [web] feat: Add ToggleGroup component and its stories

--- web/app/ui/components/view/molocule/toggle-group/toggle-group-root.tsx
+++ web/app/ui/components/view/molocule/toggle-group/toggle-group-root.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/app/utils/style';
 import { ToggleGroupContext } from './toggle-group.context';
 
 export const toggleVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-slate-100 data-[state=on]:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-400 dark:focus-visible:ring-slate-300 dark:data-[state=on]:bg-slate-800 dark:data-[state=on]:text-slate-50',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-blue-100 data-[state=on]:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-400 dark:focus-visible:ring-slate-300 dark:data-[state=on]:bg-slate-800 dark:data-[state=on]:text-slate-50',
   {
     variants: {
       variant: {
