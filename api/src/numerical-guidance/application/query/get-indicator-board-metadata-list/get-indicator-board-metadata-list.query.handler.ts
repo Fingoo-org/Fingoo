ed2984b430130a67eb5ad0b64758499a778bb196
나경호 <gangho324@gmail.com>
2024-03-11 15:47:26 +0900

    [EI-171] [api] refactor: response 값 DTO로 변경

--- api/src/numerical-guidance/application/query/get-indicator-board-metadata-list/get-indicator-board-metadata-list.query.handler.ts
+++ api/src/numerical-guidance/application/query/get-indicator-board-metadata-list/get-indicator-board-metadata-list.query.handler.ts
@@ -1,8 +1,8 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { Inject, Injectable } from '@nestjs/common';
-import { IndicatorBoardMetadata } from 'src/numerical-guidance/domain/indicator-board-metadata';
 import { LoadIndicatorBoardMetadataListPort } from '../../port/persistence/indicator-board-metadata/load-indicator-board-metadata-list.port';
 import { GetIndicatorBoardMetadataListQuery } from './get-indicator-board-metadata-list.query';
+import { IndicatorBoardMetadataDto } from '../get-indicator-board-metadata/indicator-board-metadata.dto';
 
 @Injectable()
 @QueryHandler(GetIndicatorBoardMetadataListQuery)
@@ -11,10 +11,8 @@ export class GetIndicatorBoardMetadataListQueryHandler implements IQueryHandler 
     @Inject('LoadIndicatorBoardMetadataListPort')
     private readonly loadIndicatorBoardMetadataPort: LoadIndicatorBoardMetadataListPort,
   ) {}
-  async execute(query: GetIndicatorBoardMetadataListQuery): Promise<IndicatorBoardMetadata[]> {
+  async execute(query: GetIndicatorBoardMetadataListQuery): Promise<IndicatorBoardMetadataDto[]> {
     const memberId = query.memberId;
-    const indicatorBoardMetadataList: IndicatorBoardMetadata[] =
-      await this.loadIndicatorBoardMetadataPort.loadIndicatorBoardMetadataList(memberId);
-    return indicatorBoardMetadataList;
+    return await this.loadIndicatorBoardMetadataPort.loadIndicatorBoardMetadataList(memberId);
   }
 }
