6c3384ff0d63d54593fced8505eabb45ea2a3287
나경호 <gangho324@gmail.com>
2024-03-11 15:43:43 +0900

    [EI-171] [api] feat: domain <-> dto 변환 mapper 추가

--- api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata/mapper/indicator-board-metadata.mapper.ts
+++ api/src/numerical-guidance/infrastructure/adapter/persistence/indicator-board-metadata/mapper/indicator-board-metadata.mapper.ts
@@ -1,9 +1,13 @@
 import { IndicatorBoardMetadataEntity } from '../entity/indicator-board-metadata.entity';
 import { IndicatorBoardMetadata } from '../../../../../domain/indicator-board-metadata';
 import { MemberEntity } from '../../../../../../auth/member.entity';
+import { IndicatorBoardMetadataDto } from '../../../../../application/query/get-indicator-board-metadata/indicator-board-metadata.dto';
 
 export class IndicatorBoardMetadataMapper {
-  static mapDomainToEntity(indicatorBoardMetaData: IndicatorBoardMetadata, member: MemberEntity) {
+  static mapDomainToEntity(
+    indicatorBoardMetaData: IndicatorBoardMetadata,
+    member: MemberEntity,
+  ): IndicatorBoardMetadataEntity {
     const indicatorBoardMetadataEntity: IndicatorBoardMetadataEntity = new IndicatorBoardMetadataEntity();
     indicatorBoardMetadataEntity.indicatorBoardMetadataName = indicatorBoardMetaData.indicatorBoardMetadataName;
     indicatorBoardMetadataEntity.indicatorIds = { indicatorIds: indicatorBoardMetaData.indicatorIds };
@@ -13,12 +17,33 @@ export class IndicatorBoardMetadataMapper {
     return indicatorBoardMetadataEntity;
   }
 
-  static mapEntityToDomain(entity: IndicatorBoardMetadataEntity) {
-    const indicatorBoardMetadata = new IndicatorBoardMetadata(
+  static mapEntityToDomain(entity: IndicatorBoardMetadataEntity): IndicatorBoardMetadata {
+    return new IndicatorBoardMetadata(
       entity.id,
       entity.indicatorBoardMetadataName,
       entity.indicatorIds['indicatorIds'].toString().split(','),
+      entity.createdAt,
+      entity.updatedAt,
     );
-    return indicatorBoardMetadata;
+  }
+
+  static mapDtoToDomain(dto: IndicatorBoardMetadataDto): IndicatorBoardMetadata {
+    return new IndicatorBoardMetadata(
+      dto.id,
+      dto.indicatorBoardMetadataName,
+      dto.indicatorIds,
+      dto.createdAt,
+      dto.updatedAt,
+    );
+  }
+
+  static mapDomainToDto(domain: IndicatorBoardMetadata): IndicatorBoardMetadataDto {
+    return IndicatorBoardMetadataDto.create({
+      id: domain.id,
+      indicatorBoardMetadataName: domain.indicatorBoardMetadataName,
+      indicatorIds: domain.indicatorIds,
+      createdAt: domain.createdAt,
+      updatedAt: domain.updatedAt,
+    });
   }
 }
