3ae24ece7a45f30ed844d88f4771674230817fa4
SeonghunYang <didtjdgns852@gmail.com>
2024-03-18 06:27:21 +0000

    [EI-181] [web] chore: Remove unnecessary semicolon in CustomForecastIndicatorListContainer component

--- web/app/ui/pages/workspace/custom-forecast-indicator-list-container.tsx
+++ web/app/ui/pages/workspace/custom-forecast-indicator-list-container.tsx
@@ -10,7 +10,7 @@ export default function CustomForecastIndicatorListContainer() {
           suspense: true,
         }}
       >
-        <CustomForecastIndicatorList />;
+        <CustomForecastIndicatorList />
       </SWRConfig>
     </ClientDataSuspense>
   );
