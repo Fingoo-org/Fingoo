0eb3d1b36edca894998ea78117d58f04c3dc6ef9
SeonghunYang <didtjdgns852@gmail.com>
2024-03-18 01:45:06 +0000

    [EI-175] [web] feat: Fix formatting in MetadataListContainer component

--- web/app/ui/pages/workspace/metadata-list-container.tsx
+++ web/app/ui/pages/workspace/metadata-list-container.tsx
@@ -10,7 +10,7 @@ export default function MetadataListContainer() {
           suspense: true,
         }}
       >
-        <MetadataList />;
+        <MetadataList />
       </SWRConfig>
     </ClientDataSuspense>
   );
