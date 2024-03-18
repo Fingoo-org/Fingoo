39094f6e21f9f52faf2398cd2278c08bcc84e0fd
SeonghunYang <didtjdgns852@gmail.com>
2024-03-18 05:42:52 +0000

    [EI-175] [web] test: Add test for selecting custom forecast indicator

--- web/__tests__/components/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-list.test.tsx
+++ web/__tests__/components/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-list.test.tsx
@@ -4,6 +4,7 @@ import { resetMockDB } from '@/app/mocks/db';
 import { resetAllStore } from '@/app/store/stores/reset-store';
 import { useWorkspaceStore } from '@/app/store/stores/numerical-guidance/workspace.store';
 import CustomForecastIndicatorList from '@/app/ui/components/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-list';
+import userEvent from '@testing-library/user-event';
 
 describe('CustomForecastIndicatorList', () => {
   beforeEach(() => {
@@ -29,5 +30,26 @@ describe('CustomForecastIndicatorList', () => {
     expect(await screen.findByText(/customForecastIndicator1/i)).toBeVisible();
     expect(await screen.findByText(/customForecastIndicator2/i)).toBeVisible();
     expect(await screen.findByText(/customForecastIndicator3/i)).toBeVisible();
+  });
+
+  it('사용자가 커스텀 예측 지표를 클릭하면, 커스텀 예측 지표가 선택되어진다.', async () => {
+    // given
+    const user = userEvent.setup();
+    render(
+      <SWRProviderWithoutCache>
+        <CustomForecastIndicatorList />
+      </SWRProviderWithoutCache>,
+    );
+    const { result: store } = renderHook(() => useWorkspaceStore());
+    act(() => {
+      store.current.actions.selectMetadata('1');
+    });
+    await waitFor(() => expect(screen.getByRole('tablist')).toBeVisible());
+
+    // when
+    await user.click(await screen.findByText(/customForecastIndicator1/i));
+
+    // then
+    expect(await screen.findByRole('tab', { selected: true })).toBeInTheDocument();
   });
 });
