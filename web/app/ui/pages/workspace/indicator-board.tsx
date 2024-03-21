9b51da0c093ab5a733b031c3f2329a029e61e1d5
SeonghunYang <didtjdgns852@gmail.com>
2024-03-21 04:40:23 +0000

    [EI-184] [web] rename: rename Indicator Board component

--- web/app/ui/pages/workspace/indicator-board.tsx
+++ web/app/ui/pages/workspace/indicator-board.tsx
@@ -5,7 +5,7 @@ import IntervalToggleGroup from '../../components/numerical-guidance/indicator/i
 import { SWRConfig } from 'swr';
 import ClientDataSuspense from '../../components/view/atom/client-data-suspense';
 
-export default function ChartContainer() {
+export default function IndicatorBoard() {
   return (
     <Card className="h-[29.5rem] w-[47.5rem] rounded-lg bg-white shadow-lg" decoration="top">
       <ClientDataSuspense fallback={<div>loading...</div>}>
