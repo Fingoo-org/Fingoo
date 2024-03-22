a69f18db1d0ab7fb14404204ea91425c020a1bf8
나경호 <gangho324@gmail.com>
2024-03-23 00:46:44 +0900

    [EI-183] [api] test: 파일 업로드 e2e test

--- api/src/numerical-guidance/test/E2E test/indicator-board-metadata/indicator-board-metadata.e2e.spec.ts
+++ api/src/numerical-guidance/test/E2E test/indicator-board-metadata/indicator-board-metadata.e2e.spec.ts
@@ -24,6 +24,8 @@ import { AuthGuard } from '../../../../auth/auth.guard';
 import { of } from 'rxjs';
 import { HttpExceptionFilter } from '../../../../utils/exception-filter/http-exception-filter';
 import * as request from 'supertest';
+import { FileSupabaseAdapter } from '../../../infrastructure/adapter/storage/file.supabase.adapter';
+import { UploadFileCommandHandler } from '../../../application/command/upload-file/upload-file.command.handler';
 
 jest.mock('typeorm-transactional', () => ({
   Transactional: () => () => ({}),
@@ -33,6 +35,7 @@ describe('Indicator Board Metadata E2E Test', () => {
   let app: INestApplication;
   let dataSource: DataSource;
   let DBenvironment;
+  let fileSupabaseAdapter: FileSupabaseAdapter;
 
   const seeding = async () => {
     const memberEntity = dataSource.getRepository(MemberEntity);
@@ -100,6 +103,8 @@ describe('Indicator Board Metadata E2E Test', () => {
           DeleteIndicatorIdCommandHandler,
           DeleteIndicatorBoardMetadataCommandHandler,
           UpdateIndicatorBoardMetadataNameCommandHandler,
+          UploadFileCommandHandler,
+          FileSupabaseAdapter,
           {
             provide: 'CreateIndicatorBoardMetadataPort',
             useClass: IndicatorBoardMetadataPersistentAdapter,
@@ -133,6 +138,10 @@ describe('Indicator Board Metadata E2E Test', () => {
             useClass: IndicatorBoardMetadataPersistentAdapter,
           },
           {
+            provide: 'UploadFilePort',
+            useClass: FileSupabaseAdapter,
+          },
+          {
             provide: AuthGuard,
             useValue: {
               canActivate: jest.fn().mockImplementation((context) => {
@@ -146,6 +155,7 @@ describe('Indicator Board Metadata E2E Test', () => {
         ],
       }).compile(),
     ]);
+    fileSupabaseAdapter = module.get(FileSupabaseAdapter);
     dataSource = module.get<DataSource>(DataSource);
     await seeding();
     app = module.createNestApplication();
@@ -300,6 +310,26 @@ describe('Indicator Board Metadata E2E Test', () => {
         customForecastIndicatorId: "'customForecastIndicatorId1'",
       })
       .set('Content-Type', 'application/json')
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('/post 파일을 업로드한다.', async () => {
+    const imagePath = './src/numerical-guidance/test/data/test-file.png';
+
+    const response = request(app.getHttpServer())
+      .post('/api/numerical-guidance/indicator-board-metadata/file/upload')
+      .set('Content-Type', 'multipart/form-data')
+      .attach('fileName', imagePath);
+    response.expect(HttpStatus.CREATED);
+
+    await fileSupabaseAdapter.deleteFile('indicatorBoardMetadata/test-file.png');
+  });
+
+  it('/post 파일을 업로드한다. - 빈 파일을 보내는 경우', async () => {
+    return request(app.getHttpServer())
+      .post('/api/numerical-guidance/indicator-board-metadata/file/upload')
+      .set('Content-Type', 'multipart/form-data')
+      .attach('fileName', null)
       .expect(HttpStatus.BAD_REQUEST);
   });
 });
