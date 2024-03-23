6b872792da72214b9f5b76a534f1dd9ed3137957
SeonghunYang <didtjdgns852@gmail.com>
2024-03-23 03:30:04 +0000

    [EI-184] [web] style: Update custom forecast indicator UI

--- web/app/ui/components/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
+++ web/app/ui/components/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
@@ -2,6 +2,8 @@ import { IndicatorInfoResponse } from '@/app/store/querys/numerical-guidance/ind
 import { Card } from '@tremor/react';
 import Slider from '../../view/atom/slider/slider';
 import { useSelectedCustomForecastIndicatorViewModel } from '@/app/business/hooks/custom-forecast-indicator/use-selected-custom-forecast-indicator-view-model';
+import IconButton from '../../view/atom/icons/icon-button';
+import { XCircleIcon } from '@heroicons/react/solid';
 
 type SourceIndicatorSliderProps = {
   item: IndicatorInfoResponse & { weight: number };
@@ -14,10 +16,13 @@ export default function SourceIndicatorSlider({ item }: SourceIndicatorSliderPro
     updateSourceIndicatorWeight(item.id, value[0]);
   };
 
+  const handleSourceIndicatorDelete = () => {};
+
   return (
-    <Card className="flex space-x-6 rounded-lg px-1 py-2">
-      <div className="text-xs">{item.ticker}</div>
+    <Card className="flex items-center rounded-lg px-1 py-1">
+      <div className="mr-2 block w-16 truncate text-xs">{item.ticker}</div>
       <Slider onValueChange={handleSourceIndicatorWeightChange} value={[item.weight]} key={item.id} />
+      <IconButton color={'gray'} icon={XCircleIcon} size={'xs'} onClick={handleSourceIndicatorDelete} />
     </Card>
   );
 }
