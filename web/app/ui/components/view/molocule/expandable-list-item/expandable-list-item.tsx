eeba4b9915384739db739053e83225965d71d554
SeonghunYang <didtjdgns852@gmail.com>
2024-03-28 06:43:20 +0000

    [EI-192] [web] refactor: Refactor expandable list item component

--- web/app/ui/components/view/molocule/expandable-list-item/expandable-list-item.tsx
+++ web/app/ui/components/view/molocule/expandable-list-item/expandable-list-item.tsx
@@ -1,6 +1,5 @@
 import ListItem from '../../atom/list-item';
 import Accordion from '../accordion';
-import SelectableItem from '../../atom/selectable-item';
 import { useRef, useState } from 'react';
 import IconButton from '../../atom/icons/icon-button';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
@@ -42,6 +41,14 @@ export default function ExpandableListItem() {
     );
   };
 
+  const handleClick = () => {
+    if (selected) {
+      handleDeSelect();
+    } else {
+      handleSelect();
+    }
+  };
+
   return (
     <Accordion onValueChange={handleValueChange} type="single" collapsible>
       <Accordion.Item ref={ref} className="relative w-full" value="item1">
@@ -50,7 +57,7 @@ export default function ExpandableListItem() {
             isOpen
               ? ({ children }) => {
                   return (
-                    <div className="invisible absolute right-3 top-1 z-10  group-has-[:hover]:visible">
+                    <div className="invisible absolute right-3 top-0.5 z-10  group-has-[:hover]:visible">
                       <div className="flex items-center justify-center">{children}</div>
                     </div>
                   );
@@ -59,21 +66,26 @@ export default function ExpandableListItem() {
           }
           hoverRender={hoverRender}
         >
-          <SelectableItem
-            className="rounded-2xl"
-            onSelect={handleSelect}
-            onDeSelect={handleDeSelect}
-            selected={selected}
+          <div
+            onClick={handleClick}
+            className={cn('h-full w-full rounded-2xl bg-white pl-4 ring-1 ring-blue-200', {
+              'bg-blue-200 text-blue-900 opacity-80': selected,
+            })}
           >
-            <div className="flex items-center p-1 font-bold">선택해주세용</div>
+            <div className="flex items-center p-1 font-bold hover:text-blue-700 hover:opacity-20">선택해주세용</div>
             <Accordion.Content>
-              <div className="divide-y">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className="divide-y bg-white hover:bg-white hover:text-black"
+              >
                 <div>01</div>
                 <div>01</div>
                 <div>01</div>
               </div>
             </Accordion.Content>
-          </SelectableItem>
+          </div>
         </ListItem>
         <div className={cn('absolute right-3 top-4 z-20 -translate-y-2/4')}>
           <Accordion.Trigger />
