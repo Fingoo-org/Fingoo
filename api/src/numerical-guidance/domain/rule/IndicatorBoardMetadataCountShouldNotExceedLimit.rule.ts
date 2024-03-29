21ab148c0b56779546448c57a60c6d41d961e47d
나경호 <gangho324@gmail.com>
2024-03-29 23:11:18 +0900

    [EI-195] [api] refactor: 지표 개수 로직 수정
    모든 지표를 담는 sections를 통해 개수 rule 관리

--- api/src/numerical-guidance/domain/rule/IndicatorBoardMetadataCountShouldNotExceedLimit.rule.ts
+++ api/src/numerical-guidance/domain/rule/IndicatorBoardMetadataCountShouldNotExceedLimit.rule.ts
@@ -3,9 +3,9 @@ import { BusinessRule } from '../../../utils/domain/business.rule';
 const INDICATOR_MAXIMUM = 5;
 
 export class IndicatorBoardMetadataCountShouldNotExceedLimitRule implements BusinessRule {
-  constructor(private readonly indicatorIds: string[]) {}
+  constructor(private readonly sections: Record<string, string[]>) {}
 
-  isBroken = () => this.indicatorIds.length > INDICATOR_MAXIMUM;
+  isBroken = () => Object.values(this.sections).reduce((acc, values) => acc + values.length, 0) > INDICATOR_MAXIMUM;
 
   get Message() {
     return `지표보드 메타데이터의 개수는 최대 ${INDICATOR_MAXIMUM}개입니다.`;
