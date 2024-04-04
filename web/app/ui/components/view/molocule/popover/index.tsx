3a91e6bf9a9e3c5da99f63b5fca6321d38bb0e86
SeonghunYang <didtjdgns852@gmail.com>
2024-04-04 09:32:31 +0000

    [EI-214] [web] feat: Add Popover component and PopoverContent component

---
+++ web/app/ui/components/view/molocule/popover/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { PopoverContent } from '@radix-ui/react-popover';
+const PopoverRoot = PopoverPrimitive.Root;
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+const Popover = Object.assign(PopoverRoot, {
+  Content: PopoverContent,
+  Trigger: PopoverTrigger,
+});
+
+export default Popover;
