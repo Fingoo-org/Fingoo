451a6c6a4c4f73dcf3451db23d10f5e349201f87
Choi JunHo <duekzim@gmail.com>
2024-04-04 20:39:27 +0900

    [EI-199] [web] refactor: refactor storybook add decorators

--- web/app/ui/components/view/molocule/ConversationCard/Conversation-card.stories.tsx
+++ web/app/ui/components/view/molocule/ConversationCard/Conversation-card.stories.tsx
@@ -6,6 +6,13 @@ import { InfoCircledIcon, ChevronDownIcon } from '@radix-ui/react-icons';
 const meta: Meta<typeof ConversationCard> = {
   title: 'view/molecule/ConversationCard',
   component: ConversationCard,
+  decorators: [
+    (Story) => (
+      <div className="w-96">
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
@@ -14,34 +21,23 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: (args) => (
-    <div className="w-96">
-      <ConversationCard defaultOpen={args.defaultOpen}>
-        <ConversationCard.Header
-          title="defaultOpen: true"
-          infoIcon={InfoCircledIcon}
-          collapsibleIcon={ChevronDownIcon}
-        />
-        <ConversationCard.Content initContent={['item1', 'item2']} />
-      </ConversationCard>
-    </div>
+    <ConversationCard defaultOpen={args.defaultOpen}>
+      <ConversationCard.Header title="defaultOpen: true" infoIcon={InfoCircledIcon} collapsibleIcon={ChevronDownIcon} />
+      <ConversationCard.Content initContent={['item1', 'item2']} />
+    </ConversationCard>
   ),
-  args: {
-    defaultOpen: true,
-  },
 };
 
 export const CollapsibleFalse: Story = {
   render: (args) => (
-    <div className="w-96">
-      <ConversationCard defaultOpen={args.defaultOpen}>
-        <ConversationCard.Header
-          title="defaultOpen: false"
-          infoIcon={InfoCircledIcon}
-          collapsibleIcon={ChevronDownIcon}
-        />
-        <ConversationCard.Content initContent={['item1', 'item2']} />
-      </ConversationCard>
-    </div>
+    <ConversationCard defaultOpen={args.defaultOpen}>
+      <ConversationCard.Header
+        title="defaultOpen: false"
+        infoIcon={InfoCircledIcon}
+        collapsibleIcon={ChevronDownIcon}
+      />
+      <ConversationCard.Content initContent={['item1', 'item2']} />
+    </ConversationCard>
   ),
   args: {
     defaultOpen: false,
