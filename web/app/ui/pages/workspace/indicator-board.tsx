b8d71ed534d6f153d73ca4bfe9c43442690a875e
SeonghunYang <didtjdgns852@gmail.com>
2024-04-18 07:45:44 +0000

    [EI-225] [web] feat: Refactor workspace layout and indicator board component

--- web/app/ui/pages/workspace/indicator-board.tsx
+++ web/app/ui/pages/workspace/indicator-board.tsx
@@ -5,10 +5,11 @@ import IntervalToggleGroup from '../../components/numerical-guidance/indicator/i
 import { SWRConfig } from 'swr';
 import ClientDataSuspense from '../../components/util/client-data-suspense';
 import CustomForecastIndicatorStabilityCallout from '../../components/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-stability-callout';
+import React from 'react';
 
-export default function IndicatorBoard() {
+const IndicatorBoard = React.memo(function IndicatorBoard() {
   return (
-    <Card className="min-h-[32.5rem] w-[55rem] rounded-lg bg-white shadow-lg">
+    <Card className="min-h-[32.5rem] w-full rounded-lg bg-white shadow-lg">
       <ClientDataSuspense fallback={<div>loading...</div>}>
         <SWRConfig value={{ suspense: true, keepPreviousData: true }}>
           <IndicatorsChart />
@@ -20,4 +21,6 @@ export default function IndicatorBoard() {
       </ClientDataSuspense>
     </Card>
   );
-}
+});
+
+export default IndicatorBoard;
--- web/app/ui/pages/workspace/workspace.tsx
+++ web/app/ui/pages/workspace/workspace.tsx
@@ -1,5 +1,38 @@
+'use client';
 import IndicatorBoard from './indicator-board';
+import { useIndicatorBoardStore } from '@/app/store/stores/numerical-guidance/indicator-board.store';
+import ResizablePanelGroup from '../../components/view/molecule/resizable-panel-group';
 
 export default function Workspace() {
-  return <IndicatorBoard />;
+  const { splitScreen } = useIndicatorBoardStore();
+
+  if (splitScreen === 'vertical') {
+    return (
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanelGroup.Panel defaultSize={50}>
+          <div className="flex h-full items-center justify-center px-2">
+            <IndicatorBoard />
+          </div>
+        </ResizablePanelGroup.Panel>
+        <ResizablePanelGroup.Handle disabled={true} />
+        <ResizablePanelGroup.Panel defaultSize={50}>
+          <div className="flex h-full items-center justify-center px-2">
+            <IndicatorBoard />
+          </div>
+        </ResizablePanelGroup.Panel>
+      </ResizablePanelGroup>
+    );
+  }
+
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanelGroup.Panel defaultSize={100}>
+        <div className="flex h-full items-center justify-center">
+          <div className="w-[55rem]">
+            <IndicatorBoard />
+          </div>
+        </div>
+      </ResizablePanelGroup.Panel>
+    </ResizablePanelGroup>
+  );
 }
