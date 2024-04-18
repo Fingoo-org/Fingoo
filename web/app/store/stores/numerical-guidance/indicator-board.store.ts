e280ec01ab2c4e0ded9d6208a6bb9f8e32f3e230
SeonghunYang <didtjdgns852@gmail.com>
2024-04-18 06:50:08 +0000

    [EI-225] [web] feat: Add IndicatorBoardStore for numerical guidance

---
+++ web/app/store/stores/numerical-guidance/indicator-board.store.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { storeResetFns } from '../reset-store';
+
+export const splitScreens = ['full', 'vertical', 'square'] as const;
+export type SplitScreen = (typeof splitScreens)[number];
+
+type IndicatorBoardState = {
+  splitScreen: SplitScreen;
+};
+
+type IndicatorBoardAction = {
+  setSplitScreen: (splitScreen: SplitScreen) => void;
+};
+
+type IndicatorBoardStore = IndicatorBoardState & {
+  actions: IndicatorBoardAction;
+};
+
+const initialIndicatorBoardState: IndicatorBoardState = {
+  splitScreen: 'full',
+};
+
+export const useIndicatorBoardStore = create<IndicatorBoardStore>((set) => {
+  storeResetFns.add(() => set(initialIndicatorBoardState));
+  return {
+    ...initialIndicatorBoardState,
+    actions: {
+      setSplitScreen: (splitScreen) => set({ splitScreen }),
+    },
+  };
+});
