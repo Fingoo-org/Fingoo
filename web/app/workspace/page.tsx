1ec335b67fdb39cb4d88f790d9c01323e1ba4420
SeonghunYang <didtjdgns852@gmail.com>
2024-04-18 06:51:46 +0000

    [EI-225] [web] feat: Add SplitScreenToggleGroup component to Page

--- web/app/workspace/page.tsx
+++ web/app/workspace/page.tsx
@@ -1,9 +1,15 @@
 import IndicatorBoard from '../ui/pages/workspace/indicator-board';
+import SplitScreenToggleGroup from '../ui/components/numerical-guidance/indicator-board/split-screen-toggle-group';
 
 export default function Page() {
   return (
-    <div className="flex h-full items-center justify-center">
-      <IndicatorBoard />
-    </div>
+    <>
+      <div className="fixed left-1/2 -translate-x-1/2 rounded-lg bg-white">
+        <SplitScreenToggleGroup />
+      </div>
+      <div className="flex h-full items-center justify-center">
+        <IndicatorBoard />
+      </div>
+    </>
   );
 }
