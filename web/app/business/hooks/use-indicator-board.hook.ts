7b80164d9418dc3695e57f7a6f8afacecdd8c657
SeonghunYang <didtjdgns852@gmail.com>
2024-04-19 05:51:30 +0000

    [EI-227] [web] refactor: Refactor useIndicatorBoard hook

--- web/app/business/hooks/use-indicator-board.hook.ts
+++ web/app/business/hooks/use-indicator-board.hook.ts
@@ -2,19 +2,10 @@ import { useWorkspaceStore } from '@/app/store/stores/numerical-guidance/workspa
 
 export const useIndicatorBoard = () => {
   const isAdvancedChart = useWorkspaceStore((state) => state.isAdvancedChart);
-  const tabIndex = useWorkspaceStore((state) => state.tabIndex);
   const { setIsAdvancedChart } = useWorkspaceStore((state) => state.actions);
-  const { setTabIndex } = useWorkspaceStore((state) => state.actions);
-
-  const transitionToCustomForecastTab = () => {
-    setTabIndex('1');
-  };
 
   return {
-    tabIndex,
     isAdvancedChart,
     setIsAdvancedChart,
-    setTabIndex,
-    transitionToCustomForecastTab,
   };
 };
