1f776bfec57bfa9141ebdfb90c8d870823fdb33a
SeonghunYang <didtjdgns852@gmail.com>
2024-04-21 06:08:24 +0000

    [EI-227] [web] feat: Add FullScreen component to workspace

---
+++ web/app/ui/pages/workspace/split-screen/full-screen.tsx
@@ -0,0 +1,22 @@
+import { useIndicatorBoard } from '@/app/business/hooks/use-indicator-board.hook';
+import IndicatorBoard from '@/app/ui/components/numerical-guidance/indicator-board/indicator-board';
+import ResizablePanelGroup from '@/app/ui/components/view/molecule/resizable-panel-group';
+
+export default function FullScreen() {
+  const { indicatorBoardInfos } = useIndicatorBoard();
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanelGroup.Panel defaultSize={100}>
+        <div className="flex h-full items-center justify-center">
+          <div className="w-[55rem]">
+            {indicatorBoardInfos.length === 0 ? (
+              <IndicatorBoard />
+            ) : (
+              <IndicatorBoard indicatorBoardMetadataId={indicatorBoardInfos[0].metadataId} />
+            )}
+          </div>
+        </div>
+      </ResizablePanelGroup.Panel>
+    </ResizablePanelGroup>
+  );
+}
--- web/app/ui/pages/workspace/workspace.tsx
+++ web/app/ui/pages/workspace/workspace.tsx
@@ -1,30 +1,14 @@
 'use client';
-import IndicatorBoard from '../../components/numerical-guidance/indicator-board/indicator-board';
-import ResizablePanelGroup from '../../components/view/molecule/resizable-panel-group';
 import { useIndicatorBoard } from '@/app/business/hooks/use-indicator-board.hook';
-import { useState } from 'react';
 import VerticalSplitScreen from './split-screen/vertical-split-screen';
+import FullScreen from './split-screen/full-screen';
 
 export default function Workspace() {
-  const { splitScreen, indicatorBoardInfos } = useIndicatorBoard();
+  const { splitScreen } = useIndicatorBoard();
 
   if (splitScreen === 'vertical') {
     return <VerticalSplitScreen />;
   }
 
-  return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanelGroup.Panel defaultSize={100}>
-        <div className="flex h-full items-center justify-center">
-          <div className="w-[55rem]">
-            {indicatorBoardInfos.length === 0 ? (
-              <IndicatorBoard />
-            ) : (
-              <IndicatorBoard indicatorBoardMetadataId={indicatorBoardInfos[0].metadataId} />
-            )}
-          </div>
-        </div>
-      </ResizablePanelGroup.Panel>
-    </ResizablePanelGroup>
-  );
+  return <FullScreen />;
 }
