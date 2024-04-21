a4577cb2963b1f4aa95e2720287077cf9e5874d5
SeonghunYang <didtjdgns852@gmail.com>
2024-04-21 10:38:48 +0000

    [EI-226] [web] feat: Add toolCallHandler for weather API integration

--- web/app/(ai)/ai-test/page.tsx
+++ web/app/(ai)/ai-test/page.tsx
@@ -1,9 +1,48 @@
 'use client';
+import { generateId, type ChatRequest, type FunctionCallHandler, type ToolCallHandler } from 'ai';
+
+const toolCallHandler: ToolCallHandler = async (chatMessages, toolCalls) => {
+  console.log('client');
+  console.log('chatMessages:', chatMessages);
+  console.log('toolCalls:', toolCalls);
+  const functionCall = toolCalls[0].function;
+  if (functionCall.name === 'get_current_weather') {
+    if (functionCall.arguments) {
+      const parsedFunctionCallArguments = JSON.parse(functionCall.arguments);
+      console.log(parsedFunctionCallArguments);
+    }
+
+    // Generate a fake temperature
+    const temperature = Math.floor(Math.random() * (100 - 30 + 1) + 30);
+    const weather = ['sunny', 'cloudy', 'rainy', 'snowy'][Math.floor(Math.random() * 4)];
+
+    const functionResponse: ChatRequest = {
+      messages: [
+        ...chatMessages,
+        {
+          id: generateId(),
+          tool_call_id: toolCalls[0].id,
+          name: 'get_current_weather',
+          role: 'tool' as const,
+          content: JSON.stringify({
+            temperature,
+            weather,
+            info: 'This data is randomly generated and came from a fake weather API!',
+          }),
+        },
+      ],
+    };
+    return functionResponse;
+  }
+};
 
 import { useChat } from 'ai/react';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    experimental_onToolCall: toolCallHandler,
+  });
+
   return (
     <div className="stretch mx-auto flex w-full max-w-md flex-col py-24">
       {messages.map((m) => (
--- web/app/api/chat/route.ts
+++ web/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import type { ChatCompletionTool } from 'openai/resources/index.mjs';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -8,6 +9,32 @@ const openai = new OpenAI({
 
 // Set the runtime to edge for best performance
 export const runtime = 'edge';
+
+const functions: Array<ChatCompletionTool> = [
+  {
+    type: 'function',
+    function: {
+      name: 'get_current_weather',
+      description: 'Get the current weather',
+      parameters: {
+        type: 'object',
+        properties: {
+          location: {
+            type: 'string',
+            description: 'The city and state, e.g. San Francisco, CA',
+          },
+          format: {
+            type: 'string',
+            enum: ['celsius', 'fahrenheit'],
+            description: 'The temperature unit to use. Infer this from the users location.',
+          },
+        },
+        required: ['location', 'format'],
+      },
+    },
+  },
+];
+
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
@@ -16,10 +43,42 @@ export async function POST(req: Request) {
     model: 'gpt-3.5-turbo',
     stream: true,
     messages,
+    tools: functions,
   });
 
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response, {
+    experimental_onToolCall: async (toolCallPayload, appendToolCallMessage) => {
+      console.log('toolCallPayload', toolCallPayload);
+      const { name, arguments: args } = toolCallPayload.tools[0].func;
+
+      if (name === 'get_current_weather') {
+        // Call a weather API here
+        // const weatherData = {
+        //   temperature: 20,
+        //   unit: args.format === 'celsius' ? 'C' : 'F',
+        // };
+
+        // // `createFunctionCallMessages` constructs the relevant "assistant" and "function" messages for you
+        // const newMessages = appendToolCallMessage({
+        //   tool_call_id: toolCallPayload.tools[0].id,
+        //   function_name: name,
+        //   tool_call_result: weatherData,
+        // });
+
+        // return openai.chat.completions.create({
+        //   messages: [...messages, ...newMessages],
+        //   stream: true,
+        //   model: 'gpt-3.5-turbo-0613',
+        //   // see "Recursive Function Calls" below
+        //   tools: functions,
+        // });
+
+        // 이라면 client에서 처리
+        return undefined;
+      }
+    },
+  });
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
