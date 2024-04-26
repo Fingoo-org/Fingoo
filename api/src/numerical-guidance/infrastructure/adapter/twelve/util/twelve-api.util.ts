7942f05b4b827afa1d61d3785f7f6acc221c14ea
나경호 <gangho324@gmail.com>
2024-04-26 18:38:23 +0900

    [EI-231] [api] refactor: 지표 리스트 저장 시 나라 지정 및 Time Series 값 불러오기

--- api/src/numerical-guidance/infrastructure/adapter/twelve/util/twelve-api.util.ts
+++ api/src/numerical-guidance/infrastructure/adapter/twelve/util/twelve-api.util.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { IndicatorType } from '../../../../../utils/type/type-definition';
+import { IndicatorType, Interval } from '../../../../../utils/type/type-definition';
+import * as process from 'process';
 
 const BASE_URL = 'https://api.twelvedata.com';
 
@@ -8,8 +9,8 @@ const BASE_URL = 'https://api.twelvedata.com';
 export class TwelveApiUtil {
   constructor(private readonly api: HttpService) {}
 
-  async getReferenceData(type: IndicatorType) {
-    const requestUrl: string = `${BASE_URL}/${type}`;
+  async getReferenceData(type: IndicatorType, country: string) {
+    const requestUrl: string = `${BASE_URL}/${type}?country=${country}`;
     const response = await this.api.axiosRef.get(requestUrl);
     return response.data;
   }
@@ -18,5 +19,11 @@ export class TwelveApiUtil {
     const requestUrl: string = `${BASE_URL}/symbol_search/?symbol=${symbol}`;
     const response = await this.api.axiosRef.get(requestUrl);
     return response.data.data;
+  }
+
+  async getTimeSeries(symbol: string, interval: Interval, startDate: string, endDate: string) {
+    const requestUrl: string = `${BASE_URL}/time_series/?symbol=${symbol}&interval=1day&start_date=${startDate}&end_date=${endDate}&apikey=${process.env.TWELVE_KEY}`;
+    const response = await this.api.axiosRef.get(requestUrl);
+    return response.data;
   }
 }
