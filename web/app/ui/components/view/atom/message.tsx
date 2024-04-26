598d0bc2ca0b518d19cdc9e18f568550fb206b73
Choi JunHo <duekzim@gmail.com>
2024-04-26 11:25:28 +0900

    [EI-228] [web] refactor: message component focus lastest

--- web/app/ui/components/view/atom/message.tsx
+++ web/app/ui/components/view/atom/message.tsx
@@ -1,9 +1,12 @@
+import DotSpinner from './dot-spinner';
+
 export type MessageProps = {
   role: 'user' | 'assistant';
   content: string;
+  isLoading: boolean;
 };
 
-export const Message = ({ role, content }: MessageProps) => {
+export const Message = ({ role, content, isLoading }: MessageProps) => {
   return (
     <div
       className={` flex h-auto flex-col-reverse ${role === 'user' ? `items-end justify-end` : `items-start justify-start`}`}
@@ -13,7 +16,7 @@ export const Message = ({ role, content }: MessageProps) => {
           role === 'user' ? `justify-end bg-[#333333] text-right text-white` : `justify-start bg-[#f1f1f1] text-black`
         }`}
       >
-        <div className="flex font-pretendard text-sm font-light">{content}</div>
+        {isLoading ? <DotSpinner /> : <div className="flex font-pretendard text-sm font-light">{content}</div>}
       </div>
     </div>
   );
