dc11d41671ac0ecf0842e932e3787afbbcdcbb27
Choi JunHo <duekzim@gmail.com>
2024-04-27 16:15:38 +0900

    [EI-228] [web] refactor: dot-spinner refactor color

--- web/app/ui/components/view/atom/dot-spinner.tsx
+++ web/app/ui/components/view/atom/dot-spinner.tsx
@@ -1,8 +1,17 @@
 const DotSpinner = () => (
   <div className="flex items-center justify-center space-x-2">
-    <div className="h-1 w-1 animate-ping rounded-full bg-black"></div>
-    <div className="h-1 w-1 animate-ping rounded-full bg-black delay-150 "></div>
-    <div className="h-1 w-1 animate-ping rounded-full bg-black delay-300"></div>
+    <div className=" relative flex h-2 w-2 ">
+      <span className=" absolute inline-flex h-full w-full animate-ping rounded-full bg-black"></span>
+      <span className=" relative inline-flex h-2 w-2 rounded-full bg-gray-500"></span>
+    </div>
+    <div className=" relative flex h-2 w-2 ">
+      <span className=" absolute inline-flex h-full w-full animate-ping rounded-full bg-black delay-150"></span>
+      <span className=" relative inline-flex h-2 w-2 rounded-full bg-gray-500"></span>
+    </div>
+    <div className=" relative flex h-2 w-2 ">
+      <span className=" absolute inline-flex h-full w-full animate-ping rounded-full bg-black delay-150"></span>
+      <span className=" relative inline-flex h-2 w-2 rounded-full bg-gray-500"></span>
+    </div>
   </div>
 );
 
