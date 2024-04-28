1b2a4696eac90cdfb6eab00545a123c79f502c23
SeonghunYang <didtjdgns852@gmail.com>
2024-04-28 05:33:41 +0000

    [EI-236] [web] refactor: Refactor useSWR import in indicator.query.ts and remove unused code in prefetching.ts

--- web/app/store/querys/numerical-guidance/indicator.query.ts
+++ web/app/store/querys/numerical-guidance/indicator.query.ts
@@ -1,4 +1,5 @@
-import useSWR from 'swr';
+import useSWRImmutable from 'swr/immutable';
+
 import { API_PATH } from '../api-path';
 import { fetchLiveIndicatorsValue } from '../fetcher';
 import { Interval } from '../../stores/numerical-guidance/indicator-board.store';
@@ -32,5 +33,5 @@ export const useFetchLiveIndicatorsValueByType = (params: LiveIndicatorRequestPa
   // fix: id마다 indicator Type 찾을 수 있도록 변경할 필요 있음 indicatorType은 다행이 key에 들어갈 필요는 없음
   const key = ids ? [`${API_PATH.liveIndicatorValue}`, interval, indicatorType, startDate, ...ids] : null;
 
-  return useSWR<IndicatorsValueResponse, any, string[] | null>(key, fetchLiveIndicatorsValue);
+  return useSWRImmutable<IndicatorsValueResponse, any, string[] | null>(key, fetchLiveIndicatorsValue);
 };
