5ba8aa2d8745407d0bef50c559ea207af1432c04
SeonghunYang <didtjdgns852@gmail.com>
2024-05-01 07:38:54 +0000

    [EI-238] [web] refactor: Refactor collapsible styles in chat-card.tsx

--- web/app/ui/components/view/molecule/chat-card/chat-card-root.tsx
+++ web/app/ui/components/view/molecule/chat-card/chat-card-root.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react';
 import * as Collapsible from '@radix-ui/react-collapsible';
+import { cn } from '@/app/utils/style';
 
 type ChatCardProps = {
   defaultOpen: boolean;
   children: ReactNode;
+  className?: string;
 };
 
-export const ChatCardRoot = ({ defaultOpen, children }: ChatCardProps) => {
+export const ChatCardRoot = ({ defaultOpen, className, children }: ChatCardProps) => {
   return (
-    <Collapsible.Root className="flex h-full flex-col" defaultOpen={defaultOpen}>
+    <Collapsible.Root className={cn('flex h-full flex-col', className)} defaultOpen={defaultOpen}>
       {children}
     </Collapsible.Root>
   );
