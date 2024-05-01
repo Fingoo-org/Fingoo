e75716661735ab018c9a2d2606f13903ac1f05bc
yoounyoungheon <iddyoon@gmail.com>
2024-05-01 22:14:42 +0900

    [EI-237] [api] refactor: business rule 수정

--- api/src/numerical-guidance/domain/rule/TargetIndicatorIdShouldBeUUID.rule.ts
+++ api/src/numerical-guidance/domain/rule/TargetIndicatorIdShouldBeUUID.rule.ts
@@ -3,8 +3,9 @@ import { BusinessRule } from 'src/utils/domain/business.rule';
 export class TargetIndicatorIdShouldBeUUIDRule implements BusinessRule {
   constructor(private readonly TargetIndicatorId: string) {}
 
-  isBroken = () =>
-    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(this.TargetIndicatorId);
+  private readonly UUID_Pattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+
+  isBroken = () => this.UUID_Pattern.test(this.TargetIndicatorId);
 
   get Message() {
     return '타겟지표 id형식은 UUID를 따라야 합니다.';
