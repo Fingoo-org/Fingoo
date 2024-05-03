f2dc58689c13db33e5a70a554b9a0fef8b14c67d
SeonghunYang <didtjdgns852@gmail.com>
2024-05-03 02:02:04 +0000

    [EI-242] [web] refactor: Refactor UI components in web app

--- web/app/ui/components/view/hooks/use-submit.hooks.ts
+++ web/app/ui/components/view/hooks/use-submit.hooks.ts
@@ -2,13 +2,13 @@ import React, { useRef, type RefObject } from 'react';
 
 type SubmitProps = {
   formRef: RefObject<HTMLFormElement>;
-  onKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 };
 
 export function useSubmit(): SubmitProps {
   const formRef = useRef<HTMLFormElement>(null);
 
-  const handleEnterKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>): void => {
+  const handleEnterKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === 'Enter' && !event.shiftKey) {
       formRef.current?.requestSubmit();
       event.preventDefault();
