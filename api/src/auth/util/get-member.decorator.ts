4e10d3d0f1df3bb2a8b686b31c3eb65127672d0f
나경호 <gangho324@gmail.com>
2024-05-05 23:06:45 +0900

    [EI-241] [api] refactor: 로그인한 유저 정보 데코레이터

--- api/src/auth/util/get-member.decorator.ts
+++ api/src/auth/util/get-member.decorator.ts
@@ -1,7 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { MemberEntity } from '../entity/member.entity';
+import { SupabaseClient, User, UserResponse } from '@supabase/supabase-js';
+import * as process from 'process';
 
-export const Member = createParamDecorator((data, ctx: ExecutionContext): MemberEntity => {
-  const req = ctx.switchToHttp().getRequest();
-  return req.member;
+export const LoginUser = createParamDecorator(async (_, ctx: ExecutionContext): Promise<User> => {
+  const request = ctx.switchToHttp().getRequest();
+
+  const client: SupabaseClient = new SupabaseClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  const token = request.headers.authorization?.split(' ')[1] ?? [];
+  const userResponse: UserResponse = await client.auth.getUser(token);
+  return userResponse.data.user;
 });
