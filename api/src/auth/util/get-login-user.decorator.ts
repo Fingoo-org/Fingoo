07d5f3ee7a21c3b9d9f3b684053f26867f811db9
나경호 <gangho324@gmail.com>
2024-05-07 01:21:24 +0900

    [EI-241] [api] feat: auth guard mocking

--- api/src/auth/util/get-login-user.decorator.ts
+++ api/src/auth/util/get-login-user.decorator.ts
@@ -1,12 +1,16 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { SupabaseClient, User, UserResponse } from '@supabase/supabase-js';
 import * as process from 'process';
+import { mockUser } from '../test/data/mock-auth.guard';
 
 export const LoginUser = createParamDecorator(async (_, ctx: ExecutionContext): Promise<User> => {
   const request = ctx.switchToHttp().getRequest();
 
   const client: SupabaseClient = new SupabaseClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-  const token = request.headers.authorization?.split(' ')[1] ?? [];
+  const [type, token] = request.headers.authorization?.split(' ') ?? [];
+  if (type === 'MockBearer' && token === 'mockingAccessToken') {
+    return mockUser;
+  }
   const userResponse: UserResponse = await client.auth.getUser(token);
   return userResponse.data.user;
 });
