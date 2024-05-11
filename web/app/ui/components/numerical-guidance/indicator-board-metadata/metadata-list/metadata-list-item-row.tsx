f4c86d3dc9309b31cc6ae0ad538be07a53c43bc7
SeonghunYang <didtjdgns852@gmail.com>
2024-05-11 06:27:15 +0000

    [EI-300] [web] refactor: Add IconButton to MetadataListItemRow component for additional functionality

--- web/app/ui/components/numerical-guidance/indicator-board-metadata/metadata-list/metadata-list-item-row.tsx
+++ web/app/ui/components/numerical-guidance/indicator-board-metadata/metadata-list/metadata-list-item-row.tsx
@@ -1,7 +1,10 @@
-import { ListItem } from '@tremor/react';
+import ListItem from '../../../view/atom/list-item';
 import DraggableItem from '../../../view/atom/draggable/draggable-item';
 import IndicatorUnitSelector from './indicator-unit-selector';
 import { useIndicatorBoardMetadataViewModel } from '@/app/business/hooks/numerical-guidance/indicator-board-metedata/use-indicator-board-metadata-view-model.hook';
+import IconButton from '../../../view/atom/icons/icon-button';
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import { MouseEventHandler } from 'react';
 
 type MetadataListItemRowProps = {
   indicatorId: string;
@@ -18,6 +21,9 @@ export default function MetadataListItemRow({
   const indicatorInfo = indicatorBoardMetadata?.getIndicatorInfo(indicatorId);
   const indicatorText = `${indicatorInfo?.symbol}(${indicatorInfo?.name})`;
 
+  const handleIconButton: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+  };
   return (
     indicatorInfo && (
       <div className="relative">
@@ -26,14 +32,25 @@ export default function MetadataListItemRow({
           active={activeDragItemId === indicatorInfo.id}
           id={indicatorInfo.id}
         >
-          <ListItem>
-            {/* <ListItem hoverRender={hoverRender}> */}
-            <div className="w-40 truncate">{indicatorText}</div>
-          </ListItem>
+          <div className="w-40 truncate">{indicatorText}</div>
         </DraggableItem>
         {activeDragItemId !== indicatorInfo.id ? (
-          <div className="absolute right-3 top-1/2 -translate-y-1/2">
-            <IndicatorUnitSelector indicatorBoardMetadataId={indicatorBoardMetadataId} indicatorId={indicatorInfo.id} />
+          <div className="absolute right-2 top-1/2 flex -translate-y-1/2 items-center">
+            <div className="mr-1">
+              <IndicatorUnitSelector
+                indicatorBoardMetadataId={indicatorBoardMetadataId}
+                indicatorId={indicatorInfo.id}
+              />
+            </div>
+            <IconButton
+              aria-label="metadata-item-row-edit-button"
+              // ref={iconButtonRef}
+              onClick={handleIconButton}
+              icon={DotsHorizontalIcon}
+              color={'gray'}
+              className=""
+              size={'xs'}
+            />
           </div>
         ) : null}
       </div>
