667671f7abacc40871e00482e39a100af2f979fd
SeonghunYang <didtjdgns852@gmail.com>
2024-05-29 03:55:27 +0000

    [EI-254] [web] refactor: Refactor source indicator slider group component

--- apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
+++ apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
@@ -1,32 +1,33 @@
 import { Card } from '@tremor/react';
 import Slider from '../../../view/atom/slider/slider';
-import { useSelectedCustomForecastIndicatorViewModel } from '@/app/business/hooks/numerical-guidance/custom-forecast-indicator/use-selected-custom-forecast-indicator-view-model';
 import IconButton from '../../../view/atom/icons/icon-button';
 import { XCircleIcon } from '@heroicons/react/solid';
 import { IndicatorType } from '@/app/store/stores/numerical-guidance/indicator-list.store';
 
-type SourceIndicatorInfo = {
+export type SourceIndicatorInfo = {
   weight: number;
   disabled: boolean;
   id: string;
   indicatorType: IndicatorType;
-  // ticker: string;
-  // name: string;
 };
 
 type SourceIndicatorSliderProps = {
   item: SourceIndicatorInfo;
+  onWeightChange: (item: SourceIndicatorInfo, value: number) => void;
+  onSourceIndicatorDelete: (item: SourceIndicatorInfo) => void;
 };
 
-export default function SourceIndicatorSlider({ item }: SourceIndicatorSliderProps) {
-  const { updateSourceIndicatorWeight, deleteSourceIndicator } = useSelectedCustomForecastIndicatorViewModel();
-
+export default function SourceIndicatorSlider({
+  item,
+  onWeightChange,
+  onSourceIndicatorDelete,
+}: SourceIndicatorSliderProps) {
   const handleSourceIndicatorWeightChange = (value: number[]) => {
-    updateSourceIndicatorWeight(item.id, value[0]);
+    onWeightChange(item, value[0]);
   };
 
   const handleSourceIndicatorDelete = () => {
-    deleteSourceIndicator(item.id);
+    onSourceIndicatorDelete(item);
   };
 
   return (
