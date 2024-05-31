d5f326bda26079fc468a1d3651fd18dd4f523313
나경호 <hoyana1225@gmail.com>
2024-05-31 15:22:18 +0900

    [EI-253] [api] feat: live economy 지표값 fred api 연결

--- apps/api/src/numerical-guidance/infrastructure/adapter/fred/util/fred-api.util.ts
+++ apps/api/src/numerical-guidance/infrastructure/adapter/fred/util/fred-api.util.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { Interval } from '../../../../../utils/type/type-definition';
 
 const BASE_URL = 'https://api.stlouisfed.org/fred';
 
@@ -12,7 +13,23 @@ export class FredApiUtil {
     const response = await this.api.axiosRef.get(requestUrl);
     return response.data.seriess;
   }
-  //
-  // async getSeries(symbol: string) {
-  // }
+
+  async getSeries(symbol: string, interval: Interval, startDate: string, endDate: string) {
+    const requestUrl: string = `${BASE_URL}/series/observations?series_id=${symbol.toUpperCase()}&api_key=${process.env.FRED_KEY}&file_type=json&observation_start=${startDate}&observation_end=${endDate}&frequency${this.convertInterval(interval)}`;
+    const response = await this.api.axiosRef.get(requestUrl);
+    return response.data.observations;
+  }
+
+  private convertInterval(interval: Interval): string {
+    switch (interval) {
+      case 'day':
+        return 'd';
+      case 'week':
+        return 'w';
+      case 'month':
+        return 'm';
+      case 'year':
+        return 'a';
+    }
+  }
 }
