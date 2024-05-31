9e8fe42bc6085a89b9f9ff304af67ae660405fef
나경호 <hoyana1225@gmail.com>
2024-05-31 15:19:51 +0900

    [EI-253] [api] feat: live economy 지표 handler

--- apps/api/src/numerical-guidance/application/query/live-indicator/get-live-indicator/get-live-indicator.query.handler.ts
+++ apps/api/src/numerical-guidance/application/query/live-indicator/get-live-indicator/get-live-indicator.query.handler.ts
@@ -2,10 +2,17 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { GetLiveIndicatorQuery } from './get-live-indicator.query';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { LoadLiveIndicatorPort } from '../../../port/external/twelve/load-live-indicator.port';
-import { Interval, LiveIndicatorDtoType } from '../../../../../utils/type/type-definition';
+import {
+  FredFrequency,
+  IndicatorDtoType,
+  Interval,
+  LiveIndicatorDtoType,
+} from '../../../../../utils/type/type-definition';
 import { CachingLiveIndicatorPort } from '../../../port/cache/caching-live-indicator.port';
 import { LoadCachedLiveIndicatorPort } from '../../../port/cache/load-cached-live-indicator.port';
 import { LoadIndicatorPort } from '../../../port/persistence/indicator/load-indicator.port';
+import { LoadLiveEconomyIndicatorPort } from '../../../port/external/fred/load-live-economy-indicator.port';
+import { EconomyDto } from '../../indicator/get-indicator-list/dto/economy.dto';
 
 @Injectable()
 @QueryHandler(GetLiveIndicatorQuery)
@@ -14,6 +21,8 @@ export class GetLiveIndicatorQueryHandler implements IQueryHandler {
   constructor(
     @Inject('LoadLiveIndicatorPort')
     private readonly loadLiveIndicatorPort: LoadLiveIndicatorPort,
+    @Inject('LoadLiveEconomyIndicatorPort')
+    private readonly loadLiveEconomyIndicatorPort: LoadLiveEconomyIndicatorPort,
     @Inject('LoadCachedLiveIndicatorPort')
     private readonly loadCachedLiveIndicatorPort: LoadCachedLiveIndicatorPort,
     @Inject('CachingLiveIndicatorPort')
@@ -23,19 +32,65 @@ export class GetLiveIndicatorQueryHandler implements IQueryHandler {
   ) {}
 
   async execute(query: GetLiveIndicatorQuery): Promise<LiveIndicatorDtoType> {
-    const { indicatorId, interval, startDate, indicatorType } = query;
+    const { indicatorId, startDate, interval, indicatorType } = query;
 
-    const indicatorDto = await this.loadIndicatorPort.loadIndicator(indicatorId, indicatorType);
+    const indicatorDto: IndicatorDtoType = await this.loadIndicatorPort.loadIndicator(indicatorId, indicatorType);
 
     const { key, endDate } = this.createLiveIndicatorKey(indicatorDto, interval, startDate);
 
     let liveIndicatorDto: LiveIndicatorDtoType = await this.loadCachedLiveIndicatorPort.loadCachedLiveIndicator(key);
 
     if (this.isNotCached(liveIndicatorDto)) {
-      liveIndicatorDto = await this.loadLiveIndicatorPort.loadLiveIndicator(indicatorDto, interval, startDate, endDate);
-      await this.cachingLiveIndicatorPort.cachingLiveIndicator(key, liveIndicatorDto);
-      this.logger.log('Live indicator(TWELVE) 호출');
+      if (indicatorType === 'economy' && interval === 'none') {
+        const economicInterval: FredFrequency = (indicatorDto as EconomyDto).frequency;
+        liveIndicatorDto = await this.loadAndCacheLiveEconomyIndicator(
+          indicatorDto,
+          economicInterval,
+          startDate,
+          endDate,
+          key,
+        );
+        this.logger.log('Live indicator(FRED) 호출');
+      } else {
+        liveIndicatorDto = await this.loadAndCacheLiveIndicator(indicatorDto, interval, startDate, endDate, key);
+        this.logger.log('Live indicator(TWELVE) 호출');
+      }
     }
+
+    return liveIndicatorDto;
+  }
+
+  private async loadAndCacheLiveIndicator(
+    indicatorDto: IndicatorDtoType,
+    interval: Interval,
+    startDate: string,
+    endDate: string,
+    key: string,
+  ): Promise<LiveIndicatorDtoType> {
+    const liveIndicatorDto = await this.loadLiveIndicatorPort.loadLiveIndicator(
+      indicatorDto,
+      interval,
+      startDate,
+      endDate,
+    );
+    await this.cachingLiveIndicatorPort.cachingLiveIndicator(key, liveIndicatorDto);
+    return liveIndicatorDto;
+  }
+
+  private async loadAndCacheLiveEconomyIndicator(
+    indicatorDto: IndicatorDtoType,
+    interval: FredFrequency,
+    startDate: string,
+    endDate: string,
+    key: string,
+  ): Promise<LiveIndicatorDtoType> {
+    const liveIndicatorDto = await this.loadLiveEconomyIndicatorPort.loadLiveIndicator(
+      indicatorDto,
+      interval,
+      startDate,
+      endDate,
+    );
+    await this.cachingLiveIndicatorPort.cachingLiveIndicator(key, liveIndicatorDto);
     return liveIndicatorDto;
   }
 
@@ -43,8 +98,15 @@ export class GetLiveIndicatorQueryHandler implements IQueryHandler {
     return indicatorDto == null;
   }
 
-  private createLiveIndicatorKey(indicatorDto, interval: Interval, formattedStartDate: string) {
+  private createLiveIndicatorKey(indicatorDto: IndicatorDtoType, interval: string, formattedStartDate: string) {
     const endDate = this.getEndDate();
+    if (interval === 'none') {
+      const economicInterval: string = (indicatorDto as EconomyDto).frequency;
+      return {
+        key: `${indicatorDto.indicatorType}/live${indicatorDto.symbol}${economicInterval}${formattedStartDate}${endDate}`,
+        endDate: endDate,
+      };
+    }
     return {
       key: `${indicatorDto.indicatorType}/live${indicatorDto.symbol}${interval}${formattedStartDate}${endDate}`,
       endDate: endDate,
