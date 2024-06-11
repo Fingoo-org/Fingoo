d7cead8df1fd54e4fc80bc72391e56ccdf091f08
SeonghunYang <didtjdgns852@gmail.com>
2024-06-11 11:17:30 +0000

    [EI-254] [web] refactor: Refactor useSWRCache hook to improve caching and data retrieval

--- apps/web/app/store/querys/use-swr-cache.hook.ts
+++ apps/web/app/store/querys/use-swr-cache.hook.ts
@@ -3,17 +3,55 @@ import { unstable_serialize, useSWRConfig } from 'swr';
 export const useSWRCache = () => {
   const { cache } = useSWRConfig();
 
-  const getSynchronizedData = (key: string | string[]) => {
-    return cache.get(unstable_serialize(key))?.data;
+  const getSynchronizedData = <T>(key: string | string[]) => {
+    return cache.get(unstable_serialize(key))?.data as T | undefined;
   };
 
-  const getPreviousCachedData = (key: string | string[]) => {
+  const getPreviousCachedData = <T>(key: string | string[]) => {
     const previousKey = key.slice(0, -1);
-    return cache.get(unstable_serialize(previousKey))?.data;
+    return cache.get(unstable_serialize(previousKey))?.data as T | undefined;
+  };
+
+  const getPreviousCachedIncluded = <T>(partialKey: string) => {
+    console.log(cache);
+    for (const key of cache.keys()) {
+      console.log(key, partialKey);
+      console.log(key.includes(partialKey));
+      if (key.includes(partialKey)) {
+        console.log(cache.get(key));
+        return cache.get(key)?.data as T | undefined;
+      }
+    }
+  };
+
+  const matchKeyList = (matchStrs: string[]) => {
+    const result = [];
+
+    for (const cacheKey of cache.keys()) {
+      if (matchStrs.every((str, i) => cacheKey.includes(str))) {
+        result.push(cacheKey);
+      }
+    }
+
+    return result;
+  };
+
+  const getMatchedAndExcludedKeyList = (matchStrs: string[], excludeKey: string | string[]) => {
+    const matchedKeys = matchKeyList(matchStrs);
+
+    return matchedKeys.filter((key) => unstable_serialize(excludeKey) !== key);
+  };
+
+  const hasKey = (key: string | string[]) => {
+    return cache.get(unstable_serialize(key)) !== undefined;
   };
 
   return {
     getSynchronizedData,
     getPreviousCachedData,
+    matchKeyList,
+    getPreviousCachedIncluded,
+    hasKey,
+    getMatchedAndExcludedKeyList,
   };
 };
