fd63a49798307ed6e50ae81e8be64ee4417b3a51
SeonghunYang <didtjdgns852@gmail.com>
2024-06-12 10:58:10 +0000

    [EI-257] [web] feat: Refactor SideNavigationBar component and improve menu functionality

--- apps/web/app/ui/components/view/molecule/side-navigation-bar/side-navigation-bar.tsx
+++ apps/web/app/ui/components/view/molecule/side-navigation-bar/side-navigation-bar.tsx
@@ -36,31 +36,33 @@ export default function SideNavigationBar() {
           <MenuIcon onClick={handleMenuSelect} value="name" selected={selected === 'name'} />
         </div>
       </div>
-      <Sidebar collapsedWidth="10px" width="350px" collapsed={collapsed} backgroundColor={'#fff'} className="h-screen ">
-        <div className="relative">
-          <div className="absolute right-0">
-            <IconButton
-              data-collapsed={collapsed}
-              className="transition-transform duration-200 data-[collapsed=true]:rotate-180"
-              color={'gray'}
-              icon={ChevronDoubleLeftIcon}
-              onClick={handleCollapse}
-            />
-          </div>
-        </div>
-        <div className="flex h-screen flex-col">
-          <SideNavHeader />
-        </div>
+      <Sidebar
+        transitionDuration={500}
+        collapsedWidth="0"
+        width="350px"
+        collapsed={collapsed}
+        backgroundColor={'#fff'}
+        className="h-screen "
+      >
+        <CloseButton collapsed={collapsed} onCollapse={handleCollapse} />
+        <div className="flex h-screen flex-col"></div>
       </Sidebar>
     </div>
   );
 }
 
-function SideNavHeader() {
+function CloseButton({ collapsed, onCollapse }: { collapsed: boolean; onCollapse: () => void }) {
   return (
-    <div className="ml-8 flex h-[10%] items-center">
-      <div className="mr-8 h-14 w-14 min-w-14 rounded-lg bg-gray-300"></div>
-      <p className="truncate text-xl">Fingoo</p>
+    <div className="relative">
+      <div className="absolute right-0">
+        <IconButton
+          data-collapsed={collapsed}
+          className="transition-transform duration-200 data-[collapsed=true]:rotate-180"
+          color={'gray'}
+          icon={ChevronDoubleLeftIcon}
+          onClick={onCollapse}
+        />
+      </div>
     </div>
   );
 }
@@ -80,7 +82,7 @@ function MenuIcon({ value, selected, onClick }: MenuIcon) {
     <button
       onClick={handleClick}
       className={cn(
-        'h-14 w-14   transition-all duration-200 ease-in-out	',
+        'h-14 w-14  transition-all duration-200 ease-in-out	',
         selected ? 'w-full rounded-l-lg rounded-r-none bg-white ' : '',
       )}
     >
