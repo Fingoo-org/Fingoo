b287f72fc331e534f116af3227855ebf347cd883
SeonghunYang <didtjdgns852@gmail.com>
2024-06-14 09:29:58 +0000

    [EI-257] [web] feat: Add Chat AI navigator component to workspace layout

--- apps/web/app/(sub-page)/workspace/layout.tsx
+++ apps/web/app/(sub-page)/workspace/layout.tsx
@@ -1,11 +1,13 @@
 import SideNav from '../../ui/pages/workspace/side-bar/sidenav';
 import FloatingComponentContainer from '../../ui/pages/workspace/floating-component-container';
+import ChatAiNavigator from '@/app/ui/pages/workspace/chat-ai-navigator';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen md:flex-row md:overflow-hidden">
       <SideNav />
-      <div className="grow">{children}</div>
+      <div className="grow bg-fingoo-gray-1.5">{children}</div>
+      <ChatAiNavigator />
       <FloatingComponentContainer />
     </div>
   );
---
+++ apps/web/app/ui/pages/workspace/chat-ai-navigator.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Sidebar } from 'react-pro-sidebar';
+
+export default function ChatAiNavigator() {
+  return (
+    <Sidebar width="400px" backgroundColor={'#fff'} className="shadow-sm">
+      <div>hi</div>
+    </Sidebar>
+  );
+}
