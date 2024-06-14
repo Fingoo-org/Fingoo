c7ad5728ebf1735174026b7d17629640f9d4bbee
SeonghunYang <didtjdgns852@gmail.com>
2024-06-14 09:40:51 +0000

    [EI-257] [web] feat: Add Chat AI navigator component to workspace layout

--- apps/web/app/ui/pages/workspace/chat-ai-navigator.tsx
+++ apps/web/app/ui/pages/workspace/chat-ai-navigator.tsx
@@ -1,10 +1,15 @@
 'use client';
 import { Sidebar } from 'react-pro-sidebar';
+import Chat from '../../components/domain/linguistic-guidance/chat';
 
 export default function ChatAiNavigator() {
   return (
     <Sidebar width="400px" backgroundColor={'#fff'} className="shadow-sm">
-      <div>hi</div>
+      <div className="h-[150px] w-full bg-gray-300"></div>
+      <div className="h-8 w-full border"></div>
+      <div className="px-6">
+        <Chat />
+      </div>
     </Sidebar>
   );
 }
