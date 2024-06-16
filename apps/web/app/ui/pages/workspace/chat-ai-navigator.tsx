ce8d60d3819a0861145f10f50f80064008e33a27
SeonghunYang <didtjdgns852@gmail.com>
2024-06-16 03:55:31 +0000

    [EI-999] [web] refactor: Add collapsible functionality to ChatAiNavigator component

--- apps/web/app/ui/pages/workspace/chat-ai-navigator.tsx
+++ apps/web/app/ui/pages/workspace/chat-ai-navigator.tsx
@@ -1,17 +1,47 @@
 'use client';
 import { Sidebar } from 'react-pro-sidebar';
 import Chat from '../../components/domain/linguistic-guidance/chat';
+import { useState } from 'react';
+import { cn } from '@/app/utils/style';
+import IconButton from '../../components/view/atom/icons/icon-button';
+import { ChevronRightIcon } from '@heroicons/react/solid';
 
 export default function ChatAiNavigator() {
+  const [collpase, setCollpase] = useState(false);
+
+  const handleCollpase = () => {
+    setCollpase(!collpase);
+  };
+
   return (
-    <Sidebar width="400px" backgroundColor={'#fff'} className="shadow-sm">
-      <div className="">
-        <div className="h-[13vh] w-full bg-gray-300"></div>
-        <div className="h-[3vh] w-full border"></div>
-        <div className="h-[84vh]">
-          <Chat />
+    <div className="relatvie">
+      <IconButton
+        size={'xs'}
+        data-collapsed={collpase}
+        className={cn(
+          'absolute top-[3vh] z-10 rounded-full bg-fingoo-gray-1.5 shadow-md transition-transform duration-200 data-[collapsed=true]:rotate-180',
+          collpase ? 'right-[8px]' : 'right-[388px]',
+        )}
+        color={'gray'} 
+        onClick={handleCollpase}
+        icon={ChevronRightIcon}
+      />
+      <Sidebar
+        collapsed={collpase}
+        transitionDuration={0}
+        collapsedWidth="20px"
+        width="400px"
+        backgroundColor={'#fff'}
+        className="shadow-sm"
+      >
+        <div>
+          <div className="h-[13vh] w-full bg-gray-300"></div>
+          <div className="h-[3vh] w-full border"></div>
+          <div className="h-[84vh]">
+            <Chat />
+          </div>
         </div>
-      </div>
-    </Sidebar>
+      </Sidebar>
+    </div>
   );
 }
