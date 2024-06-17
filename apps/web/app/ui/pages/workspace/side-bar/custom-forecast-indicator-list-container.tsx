1c8d9052a210f72ce18cc2a5578f512c15563ca8
SeonghunYang <didtjdgns852@gmail.com>
2024-06-17 09:54:43 +0000

    [EI-999] [web] feat: Add ListSkeleton component for UI loading state

--- apps/web/app/ui/pages/workspace/side-bar/custom-forecast-indicator-list-container.tsx
+++ apps/web/app/ui/pages/workspace/side-bar/custom-forecast-indicator-list-container.tsx
@@ -1,10 +1,17 @@
 import { SWRConfig } from 'swr';
 import ClientDataSuspense from '../../../components/util/client-data-suspense';
 import CustomForecastIndicatorList from '../../../components/domain/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-list/custom-forecast-indicator-list';
+import ListSkeleton from '@/app/ui/components/view/skeletons';
 
 export default function CustomForecastIndicatorListContainer() {
   return (
-    <ClientDataSuspense fallback={<div>loading...</div>}>
+    <ClientDataSuspense
+      fallback={
+        <div className="pt-2">
+          <ListSkeleton />
+        </div>
+      }
+    >
       <SWRConfig
         value={{
           suspense: true,
