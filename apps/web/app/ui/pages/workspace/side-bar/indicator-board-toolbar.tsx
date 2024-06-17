f2b01aa9b1a5bc01243523756cbec72919c21e68
SeonghunYang <didtjdgns852@gmail.com>
2024-06-17 10:42:32 +0000

    [EI-999] [web] refactor: Update IndicatorBoardToolbar component with blank state for metadata selection

--- apps/web/app/ui/pages/workspace/side-bar/indicator-board-toolbar.tsx
+++ apps/web/app/ui/pages/workspace/side-bar/indicator-board-toolbar.tsx
@@ -4,6 +4,7 @@ import IndicatorListContainer from './indicator-list-container';
 import CustomForecastIndicatorListContainer from './custom-forecast-indicator-list-container';
 import { useWorkspace } from '@/app/business/hooks/use-workspace.hook';
 import { useSelectedIndicatorBoardMetadata } from '@/app/business/hooks/numerical-guidance/indicator-board-metedata/use-selected-indicator-board-metadata-view-model.hook';
+import Blank from '@/app/ui/components/view/molecule/blank';
 
 export default function IndicatorBoardToolbar() {
   const { selectedMetadata } = useSelectedIndicatorBoardMetadata();
@@ -17,11 +18,7 @@ export default function IndicatorBoardToolbar() {
 
   return (
     <div>
-      <Tabs
-        className="mt-10 w-full"
-        value={isMetadataSelected ? tabIndex : undefined}
-        onValueChange={handleTabValueChange}
-      >
+      <Tabs className="mt-10 w-full" value={isMetadataSelected ? tabIndex : '2'} onValueChange={handleTabValueChange}>
         <Tabs.List className="grid h-10 w-full grid-cols-2">
           <Tabs.Trigger disabled={!isMetadataSelected} value="0">
             지표 추가
@@ -35,6 +32,11 @@ export default function IndicatorBoardToolbar() {
         </Tabs.Content>
         <Tabs.Content value="1">
           <CustomForecastIndicatorListContainer />
+        </Tabs.Content>
+        <Tabs.Content value="2">
+          <div className="flex h-[20vh] items-center justify-center text-sm opacity-60">
+            <div>메타데이터를 선택해주세요</div>
+          </div>
         </Tabs.Content>
       </Tabs>
     </div>
