a6ac91d772e370a91c2b6cf66bd1afffe2c0b07b
나경호 <hoyana1225@gmail.com>
2024-06-19 19:15:47 +0900

    [EI-239] [api] refactor: redis expire 값 최적화

--- apps/api/src/numerical-guidance/infrastructure/adapter/redis/live-indicator.redis.adapter.ts
+++ apps/api/src/numerical-guidance/infrastructure/adapter/redis/live-indicator.redis.adapter.ts
@@ -6,7 +6,10 @@ import { Redis } from 'ioredis';
 import { LiveIndicatorMapper } from '../twelve/mapper/live-indicator.mapper';
 import { LiveIndicatorDtoType } from '../../../../utils/type/type-definition';
 
-const REDIS_EXPIRE_TIME = 93600;
+const REDIS_EXPIRE_DAY = 86400; // 하루
+const REDIS_EXPIRE_WEEK = 604800; // 1주일
+const REDIS_EXPIRE_MONTH = 2592000; // 1달
+const REDIS_EXPIRE_YEAR = 31536000; // 1년
 
 @Injectable()
 export class LiveIndicatorRedisAdapter implements LoadCachedLiveIndicatorPort, CachingLiveIndicatorPort {
@@ -20,8 +23,20 @@ export class LiveIndicatorRedisAdapter implements LoadCachedLiveIndicatorPort, C
 
   async cachingLiveIndicator(key: string, indicatorDto: LiveIndicatorDtoType): Promise<void> {
     const value: string = JSON.stringify(indicatorDto);
+    let expireTime = REDIS_EXPIRE_DAY;
+
+    if (key.includes('-interval:day-')) {
+      expireTime = REDIS_EXPIRE_DAY;
+    } else if (key.includes('-interval:week-')) {
+      expireTime = REDIS_EXPIRE_WEEK;
+    } else if (key.includes('-interval:month-')) {
+      expireTime = REDIS_EXPIRE_MONTH;
+    } else if (key.includes('-interval:year-')) {
+      expireTime = REDIS_EXPIRE_YEAR;
+    }
+
     this.redis.set(key, value);
-    this.redis.expire(key, REDIS_EXPIRE_TIME);
+    this.redis.expire(key, expireTime);
   }
 
   async disconnectRedis() {
