3dce0575bd9be75cc8a280554db0fb03439aa3c4
Choi JunHo <duekzim@gmail.com>
2024-06-20 01:37:12 +0900

    [EI-999] [web] refactor: Update predict, explain instruction and explain-indicator output instruction add

--- apps/web/app/business/hooks/linguistic-guidance/use-explain-indicator.hook.ts
+++ apps/web/app/business/hooks/linguistic-guidance/use-explain-indicator.hook.ts
@@ -12,10 +12,11 @@ export default function useExplainIndicator() {
     return JSON.stringify(`
       지표 정보: ${JSON.stringify(indicator.formattedIndicator)}
       
-      아래와 같은 지시사항을 따라 사용자에게 지표에 대한 설명을 제공합니다.
+      speak_to_user에 명시된 출력필드를 따르며 아래와 같은 지시사항을 따라 사용자에게 지표에 대한 설명을 제공합니다.
         - 지표의 의미와 중요성을 설명에 포함하여 제공합니다.
         - 지표의 특징과 활용 방법을 포함하여 제공합니다.
         - 분석을 하지 말고 단순히 지표에 대한 정보를 제공합니다.
+        - speak_to_user를 호출하여 명시된 출력필드에 따라 사용자에게 설명을 제공합니다.
       `);
   };
 
