a8149f3f1fec0905427e679479dd2c98b97dc7ab
SeonghunYang <didtjdgns852@gmail.com>
2024-06-23 07:10:24 +0000

    [EI-260] [web] refactor: Update IndicatorBoard component to support square split screen layout

--- apps/web/app/ui/pages/workspace/split-screen/square-split-screen.tsx
+++ apps/web/app/ui/pages/workspace/split-screen/square-split-screen.tsx
@@ -1,5 +1,40 @@
-
+import IndicatorBoard from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board';
+import IndicatorBoardDraggableContext from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board-draggable-context';
+import ResizablePanelGroup from '@/app/ui/components/view/molecule/resizable-panel-group';
 
 export default function SquareSplitScreen() {
-  return <div></div>
+  return (
+    <IndicatorBoardDraggableContext>
+      <ResizablePanelGroup direction={'vertical'}>
+        <ResizablePanelGroup.Panel defaultSize={50}>
+          <ResizablePanelGroup direction="horizontal">
+            <ResizablePanelGroup.Panel defaultSize={50}>
+              <div className="flex h-full items-center justify-center px-2">
+                <IndicatorBoard />
+              </div>
+            </ResizablePanelGroup.Panel>
+            <ResizablePanelGroup.Panel defaultSize={50}>
+              <div className="flex h-full items-center justify-center px-2">
+                <IndicatorBoard />
+              </div>
+            </ResizablePanelGroup.Panel>
+          </ResizablePanelGroup>
+        </ResizablePanelGroup.Panel>
+        <ResizablePanelGroup.Panel defaultSize={50}>
+          <ResizablePanelGroup direction="horizontal">
+            <ResizablePanelGroup.Panel defaultSize={50}>
+              <div className="flex h-full items-center justify-center px-2">
+                <IndicatorBoard />
+              </div>
+            </ResizablePanelGroup.Panel>
+            <ResizablePanelGroup.Panel defaultSize={50}>
+              <div className="flex h-full items-center justify-center px-2">
+                <IndicatorBoard />
+              </div>
+            </ResizablePanelGroup.Panel>
+          </ResizablePanelGroup>
+        </ResizablePanelGroup.Panel>
+      </ResizablePanelGroup>
+    </IndicatorBoardDraggableContext>
+  );
 }
