8154ee1ab009d59dad046f72fc2511c7c7b0251f
SeonghunYang <didtjdgns852@gmail.com>
2024-06-23 07:49:07 +0000

    [EI-260] [web] refactor: Update split screen layout components and hooks

--- apps/web/app/ui/pages/workspace/split-screen/square-split-screen.tsx
+++ apps/web/app/ui/pages/workspace/split-screen/square-split-screen.tsx
@@ -1,38 +1,16 @@
-import IndicatorBoard from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board';
 import IndicatorBoardDraggableContext from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board-draggable-context';
 import ResizablePanelGroup from '@/app/ui/components/view/molecule/resizable-panel-group';
+import { HorizontalIndicatorBoard } from './horizontal-indicator-board';
 
 export default function SquareSplitScreen() {
   return (
     <IndicatorBoardDraggableContext>
       <ResizablePanelGroup direction={'vertical'}>
         <ResizablePanelGroup.Panel defaultSize={50}>
-          <ResizablePanelGroup direction="horizontal">
-            <ResizablePanelGroup.Panel defaultSize={50}>
-              <div className="flex h-full items-center justify-center px-2">
-                <IndicatorBoard />
-              </div>
-            </ResizablePanelGroup.Panel>
-            <ResizablePanelGroup.Panel defaultSize={50}>
-              <div className="flex h-full items-center justify-center px-2">
-                <IndicatorBoard />
-              </div>
-            </ResizablePanelGroup.Panel>
-          </ResizablePanelGroup>
+          <HorizontalIndicatorBoard indexOffset={0} />
         </ResizablePanelGroup.Panel>
         <ResizablePanelGroup.Panel defaultSize={50}>
-          <ResizablePanelGroup direction="horizontal">
-            <ResizablePanelGroup.Panel defaultSize={50}>
-              <div className="flex h-full items-center justify-center px-2">
-                <IndicatorBoard />
-              </div>
-            </ResizablePanelGroup.Panel>
-            <ResizablePanelGroup.Panel defaultSize={50}>
-              <div className="flex h-full items-center justify-center px-2">
-                <IndicatorBoard />
-              </div>
-            </ResizablePanelGroup.Panel>
-          </ResizablePanelGroup>
+          <HorizontalIndicatorBoard indexOffset={2} />
         </ResizablePanelGroup.Panel>
       </ResizablePanelGroup>
     </IndicatorBoardDraggableContext>
--- apps/web/app/ui/pages/workspace/split-screen/vertical-split-screen.tsx
+++ apps/web/app/ui/pages/workspace/split-screen/vertical-split-screen.tsx
@@ -1,48 +1,10 @@
-import IndicatorBoard from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board';
-import Draggable from '@/app/ui/components/view/atom/draggable/draggable';
-import ResizablePanelGroup from '@/app/ui/components/view/molecule/resizable-panel-group';
-import { cn } from '@/app/utils/style';
-import { SortableContext } from '@dnd-kit/sortable';
-import { useSplitIndicatorBoard } from '@/app/business/hooks/numerical-guidance/indicator-board/use-split-indicator-board.hook';
 import IndicatorBoardDraggableContext from '@/app/ui/components/domain/numerical-guidance/indicator-board/indicator-board-draggable-context';
+import { HorizontalIndicatorBoard } from './horizontal-indicator-board';
 
 export default function VerticalSplitScreen() {
-  const { activeDragMetadataId, draggableIndicatorBoardContextValue } = useSplitIndicatorBoard();
-
   return (
     <IndicatorBoardDraggableContext>
-      <ResizablePanelGroup direction="horizontal">
-        {Array.from({ length: 2 }, () => 0).map((_, index) => {
-          const item = draggableIndicatorBoardContextValue[index];
-          const metadataId = item?.[0];
-          return (
-            <>
-              {item ? (
-                <ResizablePanelGroup.Panel key={index} defaultSize={50}>
-                  <SortableContext id={`${index}`} items={item}>
-                    <div
-                      className={cn('flex h-full items-center justify-center px-2', {
-                        'border-2 border-lime-300': activeDragMetadataId ? metadataId !== activeDragMetadataId : false,
-                      })}
-                    >
-                      <Draggable active={metadataId === activeDragMetadataId} handle="top" id={metadataId}>
-                        <IndicatorBoard indicatorBoardMetadataId={metadataId} />
-                      </Draggable>
-                    </div>
-                  </SortableContext>
-                </ResizablePanelGroup.Panel>
-              ) : (
-                <ResizablePanelGroup.Panel key={index} defaultSize={50}>
-                  <div className="flex h-full items-center justify-center px-2">
-                    <IndicatorBoard />
-                  </div>
-                </ResizablePanelGroup.Panel>
-              )}
-              {index === 0 ? <ResizablePanelGroup.Handle disabled={true} /> : null}
-            </>
-          );
-        })}
-      </ResizablePanelGroup>
+      <HorizontalIndicatorBoard indexOffset={0} />
     </IndicatorBoardDraggableContext>
   );
 }
