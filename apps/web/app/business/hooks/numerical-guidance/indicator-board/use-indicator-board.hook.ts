4f379d2244ab49fe3a67d8ab0cc02bdb8addd59f
SeonghunYang <didtjdgns852@gmail.com>
2024-06-23 05:57:36 +0000

    [EI-260] [web] refactor: Update useIndicatorBoard hook in indicator board page

--- apps/web/app/business/hooks/numerical-guidance/indicator-board/use-indicator-board.hook.ts
+++ apps/web/app/business/hooks/numerical-guidance/indicator-board/use-indicator-board.hook.ts
@@ -3,38 +3,14 @@ import {
   Interval,
   useIndicatorBoardStore,
 } from '@/app/store/stores/numerical-guidance/indicator-board.store';
-import { type SplitScreen } from '@/app/store/stores/numerical-guidance/indicator-board.store';
-import { useWorkspaceStore } from '@/app/store/stores/numerical-guidance/workspace.store';
-
-function maxIndicatorBoard(splitScreen: SplitScreen) {
-  switch (splitScreen) {
-    case 'full':
-      return 1;
-    case 'vertical':
-      return 2;
-    case 'square':
-      return 4;
-  }
-}
 
 export const useIndicatorBoard = (indicatorBoardMetadataId?: string) => {
   const indicatorBoardInfos = useIndicatorBoardStore((state) => state.indicatorBoardInfos);
   const indicatorBoardInfo = indicatorBoardInfos.find((info) => info.metadataId === indicatorBoardMetadataId);
 
-  const numberOfMetadataInIndicatorBoard = useIndicatorBoardStore((state) => state.indicatorBoardInfos.length);
-  const splitScreen = useIndicatorBoardStore((state) => state.splitScreen);
-  const {
-    addIndicatorBoardInfo,
-    clearIndicatorBoardInfos,
-    updateIndicatorBoardInfo,
-    deleteIndicatorBoardInfo,
-    checkMetadataInIndicatorBoard,
-    setSplitScreen,
-    sliceIndicatorBoardInfos,
-    setIndicatorBoardInfos,
-  } = useIndicatorBoardStore((state) => state.actions);
-  const selectedMetadataId = useWorkspaceStore((state) => state.selectedMetadataId);
-  const { selectMetadata } = useWorkspaceStore((state) => state.actions);
+  const { updateIndicatorBoardInfo, deleteIndicatorBoardInfo, checkMetadataInIndicatorBoard } = useIndicatorBoardStore(
+    (state) => state.actions,
+  );
 
   const isAdvancedChart = indicatorBoardInfo?.isAdvancedChart ?? false;
   const interval = indicatorBoardInfo?.interval ?? 'day';
@@ -59,48 +35,12 @@ export const useIndicatorBoard = (indicatorBoardMetadataId?: string) => {
     updateIndicatorBoardInfo(indicatorBoardMetadataId, { dateRange });
   }
 
-  function addMetadataToIndicatorBoard(metadataId: string) {
-    if (splitScreen === 'full') {
-      clearIndicatorBoardInfos();
-      addIndicatorBoardInfo(metadataId);
-      return true;
-    }
-
-    if (numberOfMetadataInIndicatorBoard < maxIndicatorBoard(splitScreen)) {
-      addIndicatorBoardInfo(metadataId);
-      return true;
-    }
-    return false;
-  }
-
   function deleteMetadataFromIndicatorBoard(metadataId: string) {
     deleteIndicatorBoardInfo(metadataId);
   }
 
-  function transitionSplitScreen(screenType: SplitScreen) {
-    const slicedIndicatorBoardInfos = sliceIndicatorBoardInfos(maxIndicatorBoard(screenType));
-
-    const selectedMetadata = slicedIndicatorBoardInfos.findIndex((info) => info.metadataId === selectedMetadataId);
-
-    if (selectedMetadata === -1) {
-      selectMetadata(undefined);
-    }
-
-    setIndicatorBoardInfos(slicedIndicatorBoardInfos);
-    setSplitScreen(screenType);
-  }
-
-  function reorderIndicatorBoardInfos(metadataIds: string[]) {
-    const newIndicatorBoardInfos = metadataIds.map((metadataId) => {
-      return indicatorBoardInfos.find((info) => info.metadataId === metadataId)!;
-    });
-
-    setIndicatorBoardInfos(newIndicatorBoardInfos);
-  }
-
   return {
     indicatorBoardInfos,
-    splitScreen,
     indicatorBoardInfo,
     interval,
     isAdvancedChart,
@@ -108,10 +48,7 @@ export const useIndicatorBoard = (indicatorBoardMetadataId?: string) => {
     setIsAdvancedChart,
     setInterval,
     checkMetadataInIndicatorBoard,
-    addMetadataToIndicatorBoard,
     deleteMetadataFromIndicatorBoard,
-    transitionSplitScreen,
-    reorderIndicatorBoardInfos,
     updateDateRange,
   };
 };
