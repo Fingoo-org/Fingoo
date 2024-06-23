384148d95aa66593a674f7ac891f89bf8d12119c
SeonghunYang <didtjdgns852@gmail.com>
2024-06-23 05:58:53 +0000

    [EI-260] [web] refactor: Add useSplitIndicatorBoard hook for managing split screen functionality

--- apps/web/__tests__/business/hooks/indicator-board/use-indicator-board.hook.test.tsx
+++ apps/web/__tests__/business/hooks/indicator-board/use-indicator-board.hook.test.tsx
@@ -3,6 +3,7 @@ import { SWRProviderWithoutCache } from '@/app/ui/components/util/swr-provider';
 import { resetMockDB } from '@/app/mocks/db';
 import { resetAllStore } from '@/app/store/stores/reset-store';
 import { useIndicatorBoard } from '@/app/business/hooks/numerical-guidance/indicator-board/use-indicator-board.hook';
+import { useSplitIndicatorBoard } from '@/app/business/hooks/numerical-guidance/indicator-board/use-split-indicator-board.hook';
 
 const wrapper = SWRProviderWithoutCache;
 
@@ -19,89 +20,17 @@ describe('useIndicatorBoard', () => {
     // when
     await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(0));
   });
-  describe('addMetadataToIndicatorBoard', () => {
-    it('지표를 지표 보드에 추가하면, 지표보드 정보를 가져온다', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(1));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('1'));
-    });
-
-    it('screen이 full일 때, 지표를 두번 추가하면, 마지막에 추가한 지표 정보만 가져온다', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-        result.current.addMetadataToIndicatorBoard('2');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(1));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('2'));
-    });
-
-    it('screen이 vertical일 때, 지표를 세번 추가하면, 세번 째 지표는 추가되지 않는다.', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.transitionSplitScreen('vertical');
-      });
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-        result.current.addMetadataToIndicatorBoard('2');
-      });
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('3');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(2));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('1'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[1].metadataId).toBe('2'));
-    });
-
-    it('screen이 square일 때, 지표를 다섯번 추가하면, 다섯번째 지표는 추가되지 않는다.', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.transitionSplitScreen('square');
-      });
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-        result.current.addMetadataToIndicatorBoard('2');
-        result.current.addMetadataToIndicatorBoard('3');
-        result.current.addMetadataToIndicatorBoard('4');
-      });
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('5');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(4));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('1'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[1].metadataId).toBe('2'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[2].metadataId).toBe('3'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[3].metadataId).toBe('4'));
-    });
-  });
 
   describe('deleteMetadataFromIndicatorBoard', () => {
     it('지표를 지표 보드에서 삭제하면, 지표보드 정보를 가져온다', async () => {
       // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
+      const { result } = renderHook(
+        () => ({
+          ...useIndicatorBoard(),
+          ...useSplitIndicatorBoard(),
+        }),
+        { wrapper },
+      );
 
       // when
       act(() => {
@@ -119,7 +48,13 @@ describe('useIndicatorBoard', () => {
   describe('updateIndicatorBoardState', () => {
     it('지표 보드의 isAdvancedChart를 변경하면, 지표보드 정보를 가져온다', async () => {
       // given
-      const { result } = renderHook(() => useIndicatorBoard('1'), { wrapper });
+      const { result } = renderHook(
+        () => ({
+          ...useIndicatorBoard('1'),
+          ...useSplitIndicatorBoard(),
+        }),
+        { wrapper },
+      );
 
       // when
       act(() => {
@@ -134,7 +69,13 @@ describe('useIndicatorBoard', () => {
     });
     it('지표 보드의 interval을 변경하면, 지표보드 정보를 가져온다', async () => {
       // given
-      const { result } = renderHook(() => useIndicatorBoard('1'), { wrapper });
+      const { result } = renderHook(
+        () => ({
+          ...useIndicatorBoard('1'),
+          ...useSplitIndicatorBoard(),
+        }),
+        { wrapper },
+      );
 
       // when
       act(() => {
@@ -146,70 +87,6 @@ describe('useIndicatorBoard', () => {
 
       // then
       await waitFor(() => expect(result.current.indicatorBoardInfos[0].interval).toBe('week'));
-    });
-  });
-
-  describe('transitionSplitScreen', () => {
-    it('splitScreen을 변경하면, splitScreen 정보를 가져온다', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.transitionSplitScreen('vertical');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.splitScreen).toBe('vertical'));
-    });
-
-    it('지표가 2개 추가되었을 때, splitScreen을 vertical에서 full로 변경하면, 지표 보드 정보를 1개만 남겨 가져온다', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.transitionSplitScreen('vertical');
-      });
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-        result.current.addMetadataToIndicatorBoard('2');
-      });
-      act(() => {
-        result.current.transitionSplitScreen('full');
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos).toHaveLength(1));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('1'));
-    });
-  });
-
-  describe('reorderIndicatorBoardInfos', () => {
-    it('지표 보드의 순서를 변경하면, 지표보드 정보를 가져온다', async () => {
-      // given
-      const { result } = renderHook(() => useIndicatorBoard(), { wrapper });
-
-      // when
-      act(() => {
-        result.current.transitionSplitScreen('square');
-      });
-
-      act(() => {
-        result.current.addMetadataToIndicatorBoard('1');
-        result.current.addMetadataToIndicatorBoard('2');
-        result.current.addMetadataToIndicatorBoard('3');
-        result.current.addMetadataToIndicatorBoard('4');
-      });
-      act(() => {
-        result.current.reorderIndicatorBoardInfos(['2', '1', '3', '4']);
-      });
-
-      // then
-      await waitFor(() => expect(result.current.indicatorBoardInfos[0].metadataId).toBe('2'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[1].metadataId).toBe('1'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[2].metadataId).toBe('3'));
-      await waitFor(() => expect(result.current.indicatorBoardInfos[3].metadataId).toBe('4'));
     });
   });
 });
