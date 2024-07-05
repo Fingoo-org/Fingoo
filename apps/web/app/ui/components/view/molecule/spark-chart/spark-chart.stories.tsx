463ff3bfd906be442f3675ebb299b70e3893e350
SeonghunYang <didtjdgns852@gmail.com>
2024-07-05 06:12:49 +0000

    [EI-16] [web] feat: Add SparkChart component for displaying spark charts

---
+++ apps/web/app/ui/components/view/molecule/spark-chart/spark-chart.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SparkChart from './spark-chart';
+
+const meta = {
+  title: 'view/molecule/SparkChart',
+  component: SparkChart,
+} satisfies Meta<typeof SparkChart>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const mockData = [
+  {
+    month: 'Jan 21',
+    Performance: 4000,
+    Benchmark: 3000,
+  },
+  {
+    month: 'Feb 21',
+    Performance: 3000,
+    Benchmark: 2000,
+  },
+  {
+    month: 'Mar 21',
+    Performance: 2000,
+    Benchmark: 1700,
+  },
+  {
+    month: 'Apr 21',
+    Performance: 2780,
+    Benchmark: 2500,
+  },
+  {
+    month: 'May 21',
+    Performance: 1890,
+    Benchmark: 1890,
+  },
+  {
+    month: 'Jun 21',
+    Performance: 2390,
+    Benchmark: 2000,
+  },
+  {
+    month: 'Jul 21',
+    Performance: 3490,
+    Benchmark: 3000,
+  },
+];
+
+export const Default: Story = {
+  args: {
+    data: mockData,
+    lineChartCategories: ['Performance'],
+    areaChartCategories: ['Benchmark'],
+    index: 'month',
+  },
+};
