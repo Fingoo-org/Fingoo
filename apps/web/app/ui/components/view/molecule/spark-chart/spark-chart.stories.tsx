12f6266039debc1651d1507cfff549445c4d9cf5
SeonghunYang <didtjdgns852@gmail.com>
2024-07-06 08:00:51 +0000

    [EI-16] [web] feat: Refactor SparkChart component to include line chart functionality and format chart data

--- apps/web/app/ui/components/view/molecule/spark-chart/spark-chart.stories.tsx
+++ apps/web/app/ui/components/view/molecule/spark-chart/spark-chart.stories.tsx
@@ -13,38 +13,113 @@ type Story = StoryObj<typeof meta>;
 const mockData = [
   {
     month: 'Jan 21',
-    Performance: 4000,
-    Benchmark: 3000,
+    Performance: {
+      value: 3000,
+      displayValue: 3000,
+    },
+    Benchmark: {
+      value: 3000,
+      displayValue: 3000,
+    },
   },
   {
     month: 'Feb 21',
-    Performance: 3000,
-    Benchmark: 2000,
+    Performance: {
+      value: 2000,
+      displayValue: 2000,
+    },
+    Benchmark: {
+      value: 2000,
+      displayValue: 2000,
+    },
   },
   {
     month: 'Mar 21',
-    Performance: 2000,
-    Benchmark: 1700,
+    Performance: {
+      value: 4000,
+      displayValue: 4000,
+    },
+    Benchmark: {
+      value: 4000,
+      displayValue: 4000,
+    },
   },
   {
     month: 'Apr 21',
-    Performance: 2780,
-    Benchmark: 2500,
+    Performance: {
+      value: 5000,
+      displayValue: 5000,
+    },
+    Benchmark: {
+      value: 5000,
+      displayValue: 5000,
+    },
   },
   {
     month: 'May 21',
-    Performance: 1890,
-    Benchmark: 1890,
+    Performance: {
+      value: 6000,
+      displayValue: 6000,
+    },
+    Benchmark: {
+      value: 6000,
+      displayValue: 6000,
+    },
   },
   {
     month: 'Jun 21',
-    Performance: 2390,
-    Benchmark: 2000,
+    Performance: {
+      value: 7000,
+      displayValue: 7000,
+    },
+    Benchmark: {
+      value: 7000,
+      displayValue: 7000,
+    },
   },
   {
     month: 'Jul 21',
-    Performance: 3490,
-    Benchmark: 3000,
+    Performance: {
+      value: 8000,
+      displayValue: 8000,
+    },
+    Benchmark: {
+      value: 8000,
+      displayValue: 8000,
+    },
+  },
+  {
+    month: 'Aug 21',
+    Performance: {
+      value: 9000,
+      displayValue: 9000,
+    },
+    Benchmark: {
+      value: 9000,
+      displayValue: 9000,
+    },
+  },
+  {
+    month: 'Sep 21',
+    Performance: {
+      value: 10000,
+      displayValue: 10000,
+    },
+    Benchmark: {
+      value: 10000,
+      displayValue: 10000,
+    },
+  },
+  {
+    month: 'Oct 21',
+    Performance: {
+      value: 11000,
+      displayValue: 11000,
+    },
+    Benchmark: {
+      value: 11000,
+      displayValue: 11000,
+    },
   },
 ];
 
