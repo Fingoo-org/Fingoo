78c1fb4c5b9db790620276cabaa799a59605c7fc
SeonghunYang <didtjdgns852@gmail.com>
2024-07-06 07:19:13 +0000

    [EI-16] [web] feat: Refactor multi-line chart component and add chart data formatter

--- apps/web/app/ui/components/view/molecule/multi-line-chart/multi-line-chart.tsx
+++ apps/web/app/ui/components/view/molecule/multi-line-chart/multi-line-chart.tsx
@@ -3,11 +3,9 @@ import { EventProps } from '@tremor/react';
 import LineChart from './line-chart';
 import { useState } from 'react';
 import { ChartTooltip } from './chart-tooltip';
-import {
-  FormattedRowType,
-  chartValueFormatterFactory,
-} from '@/app/business/services/numerical-guidance/chart/indicator-formatter.service';
+import { FormattedRowType } from '@/app/business/services/numerical-guidance/chart/indicator-formatter.service';
 import { cn } from '@/app/utils/style';
+import { formatChartData } from '@/app/utils/tremor/chart-data-formatter';
 
 type MultiLineChartProps = {
   data: FormattedRowType[];
@@ -30,7 +28,7 @@ export default function MultiLineChart({
   const [value, setValue] = useState<EventProps>(null);
   const index = 'date';
 
-  const formatteedData = formmatData(data, categories);
+  const formatteedData = formatChartData(data, categories);
   return (
     <>
       <LineChart
@@ -56,41 +54,4 @@ export default function MultiLineChart({
       />
     </>
   );
-}
-
-function formmatData(data: FormattedRowType[], categories: string[]) {
-  const caculateChartValue = chartValueFormatterFactory(categories);
-
-  return data.map((d) => {
-    return {
-      ...Object.keys(d).reduce((acc, key) => {
-        if (key === 'date') {
-          return { ...acc, [key]: d[key] };
-        }
-        return {
-          ...acc,
-          [key]: caculateChartValue(
-            d[key] as {
-              value: number;
-              displayValue: number;
-            },
-          ),
-        };
-      }, {}),
-      displayValue: Object.keys(d).reduce((acc, key) => {
-        if (key === 'date') {
-          return { ...acc, [key]: d[key] };
-        }
-        return {
-          ...acc,
-          [key]: (
-            d[key] as {
-              value: number;
-              displayValue: number;
-            }
-          ).displayValue,
-        };
-      }, {}),
-    };
-  });
 }
---
+++ apps/web/app/utils/tremor/chart-data-formatter.ts
@@ -0,0 +1,40 @@
+import {
+  chartValueFormatterFactory,
+  FormattedRowType,
+} from '@/app/business/services/numerical-guidance/chart/indicator-formatter.service';
+
+export function formatChartData(data: FormattedRowType[], categories: string[]): any {
+  const calculateChartValue = chartValueFormatterFactory(categories);
+  return data.map((d) => {
+    return {
+      ...Object.keys(d).reduce((acc, key) => {
+        if (key === 'date') {
+          return { ...acc, [key]: d[key] };
+        }
+        return {
+          ...acc,
+          [key]: calculateChartValue(
+            d[key] as {
+              value: number;
+              displayValue: number;
+            },
+          ),
+        };
+      }, {}),
+      displayValue: Object.keys(d).reduce((acc, key) => {
+        if (key === 'date') {
+          return { ...acc, [key]: d[key] };
+        }
+        return {
+          ...acc,
+          [key]: (
+            d[key] as {
+              value: number;
+              displayValue: number;
+            }
+          ).displayValue,
+        };
+      }, {}),
+    };
+  });
+}
