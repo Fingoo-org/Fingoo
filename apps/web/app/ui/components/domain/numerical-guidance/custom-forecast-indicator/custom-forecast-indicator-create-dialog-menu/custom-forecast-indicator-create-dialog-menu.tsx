dbd517b0f60a144c6f3a46e016436adf83c2a3be
SeonghunYang <didtjdgns852@gmail.com>
2024-07-17 04:44:03 +0000

    [EI-16] [web] feat: Add currentStep property to CustomForecastIndicatorCreateDialogMenu

--- apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-create-dialog-menu/custom-forecast-indicator-create-dialog-menu.tsx
+++ apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/custom-forecast-indicator-create-dialog-menu/custom-forecast-indicator-create-dialog-menu.tsx
@@ -6,10 +6,16 @@ import { useStepper } from '@/app/utils/hooks/use-stepper.hook';
 import SelectSourceIndicatorStepDialogMenu from './steps/select-source-indicator-step-dialog-menu';
 
 export default function CustomForecastIndicatorCreateDialogMenu() {
-  const { Stepper, nextStep, prevStep } = useStepper();
+  const { currentStep, Stepper, nextStep, prevStep } = useStepper();
 
   return (
-    <DialogMenu color={'gray'} size={'3xl'} side={'right'} dialogKey={DIALOG_KEY.CUSTOM_FORECAST_INDICATOR_CREATE_MENU}>
+    <DialogMenu
+      color={'gray'}
+      size={'3xl'}
+      side={'right'}
+      yOffset={currentStep === 1 ? -100 : 0}
+      dialogKey={DIALOG_KEY.CUSTOM_FORECAST_INDICATOR_CREATE_MENU}
+    >
       <Stepper>
         <Stepper.Step step={0}>
           <SelectTargetIndicatorStepDialogMenu nextStep={nextStep} />
