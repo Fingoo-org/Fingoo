3e7263740bf7e7fee0bacc3aed962e321aac3bb8
SeonghunYang <didtjdgns852@gmail.com>
2024-07-17 04:55:36 +0000

    [EI-16] [web] refactor: Update SourceIndicatorSlider component to display indicator weight

--- apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
+++ apps/web/app/ui/components/domain/numerical-guidance/custom-forecast-indicator/source-indicator-slider.tsx
@@ -37,13 +37,16 @@ export default function SourceIndicatorSlider({
       {item.disabled ? (
         <div className="grow">Disabled</div>
       ) : (
-        <Slider
-          min={-50}
-          max={50}
-          onValueChange={handleSourceIndicatorWeightChange}
-          value={[item.weight]}
-          key={item.id}
-        />
+        <>
+          <Slider
+            min={-50}
+            max={50}
+            onValueChange={handleSourceIndicatorWeightChange}
+            value={[item.weight]}
+            key={item.id}
+          />
+          <div className="px-1 text-xs">{item.weight}</div>
+        </>
       )}
       <IconButton color={'gray'} icon={XCircleIcon} size={'xs'} onClick={handleSourceIndicatorDelete} />
     </Card>
