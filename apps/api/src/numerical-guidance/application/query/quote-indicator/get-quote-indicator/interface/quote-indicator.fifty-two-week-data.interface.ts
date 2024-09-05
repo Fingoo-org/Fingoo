08ed131f2079a4872111133ed69432e8ad22c610
reindeer002 <lsj000424@gmail.com>
2024-09-05 12:32:03 +0000

    [MC-26] [api] feat: ADD query DTO interface enforce the default screen data

---
+++ apps/api/src/numerical-guidance/application/query/quote-indicator/get-quote-indicator/interface/quote-indicator.fifty-two-week-data.interface.ts
@@ -0,0 +1,8 @@
+export interface QuoteIndicatorFiftyTwoWeekData {
+  low: string;
+  high: string;
+  low_change: string;
+  high_change: string;
+  low_change_percent: string;
+  high_change_percent: string;
+}
