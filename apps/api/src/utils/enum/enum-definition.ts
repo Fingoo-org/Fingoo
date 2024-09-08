8dce62d9c7e30e90c04bd2515219fa0462d23685
reindeer002 <lsj000424@gmail.com>
2024-09-08 12:20:55 +0000

    [MC-26] [api] feat: add entire interval value

--- apps/api/src/utils/enum/enum-definition.ts
+++ apps/api/src/utils/enum/enum-definition.ts
@@ -3,5 +3,11 @@ export enum QuoteIndicatorIntervalEnum {
   MIN5 = '5min',
   MIN15 = '15min',
   MIN30 = '30min',
+  MIN45 = '45min',
+  HOUR1 = '1h',
+  HOUR2 = '2h',
+  HOUR4 = '4h',
   DAY1 = '1day',
+  WEEK1 = '1week',
+  MONTH1 = '1month',
 }
