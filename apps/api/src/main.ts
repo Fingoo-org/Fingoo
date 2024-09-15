f9ea921f4e5a44fbb722cc1e0afecb0124f81fed
SeonghunYang <didtjdgns852@gmail.com>
2024-09-15 22:52:50 +0900

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
         : 'http://localhost';
 
   app.enableCors({
-    origin: ['https://fingoo.app', 'https://fingoo-web-beta.vercel.app', applicationHost, 'http://localhost:3000'],
+    origin: ['https://www.fingoo.app', 'https://fingoo.app', 'https://fingoo-web-beta.vercel.app', applicationHost, 'http://localhost:3000'],
     credentials: true,
   });
 
