42b47376b283ad3e21bc19ba2607080ddfbbcef6
SeonghunYang <didtjdgns852@gmail.com>
2024-09-21 19:49:54 +0900

    [web] fix: 잘못 들어간 text 제거

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   <FloatingComponentContainer />
                 </div>
               </SWRProvider>
-            </ChatProvider>MockingUser
+            </ChatProvider>
           </MockingUser>
         </body>
       </GoogleAnalyticsProvider>
