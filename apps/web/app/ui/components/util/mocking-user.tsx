66eb73c09b6c8441072e4d49d84d211702c78c12
SeonghunYang <didtjdgns852@gmail.com>
2024-09-21 09:54:40 +0000

    [MC-111] [web] feat: 구글 에드센스 등록을 위해 유저를 모킹하는 환경 구축

---
+++ apps/web/app/ui/components/util/mocking-user.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { API_PATH } from '@/app/store/querys/api-path';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+
+export default function MockingUser({ children }: React.PropsWithChildren) {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    async function enableMocking() {
+      if (typeof window !== 'undefined') {
+        const response = await fetch(`${API_PATH.auth}/signIn`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: 'admin@test.com',
+            password: '123456',
+          }),
+        });
+
+        console.log(response);
+        const result = await response.json();
+        // console.log(result);
+        Cookies.set('accessToken', result.accessToken, {
+          secure: true,
+          path: '/',
+        });
+
+        setInit(true);
+      }
+    }
+
+    enableMocking();
+  }, []);
+
+  return <>{init ? children : null}</>;
+}
