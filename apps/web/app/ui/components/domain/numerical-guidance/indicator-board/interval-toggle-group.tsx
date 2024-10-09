cc0029926dafe1e2771c59a49093c331a9f1fd42
yesolz <yesol@ajou.ac.kr>
2024-10-09 17:46:37 +0900

    [MC-26] [web] refactor: split interval logic into BaseIntervalToggleGroup for reuse in DetailChartIntervalGroup and IntervalToggleGroup

--- apps/web/app/ui/components/domain/numerical-guidance/indicator-board/interval-toggle-group.tsx
+++ apps/web/app/ui/components/domain/numerical-guidance/indicator-board/interval-toggle-group.tsx
@@ -1,59 +1,21 @@
-import ToggleGroup from '../../../view/molecule/toggle-group';
-import { intervals, SplitScreen, type Interval } from '@/app/store/stores/numerical-guidance/indicator-board.store';
 import { useIndicatorBoard } from '@/app/business/hooks/numerical-guidance/indicator-board/use-indicator-board.hook';
 import { useSplitIndicatorBoard } from '@/app/business/hooks/numerical-guidance/indicator-board/use-split-indicator-board.hook';
-import { cn } from '@/app/utils/style';
+import { BaseIntervalToggleGroup } from './base-interval-toggle-group';
 
-function isInterval(value: string): value is Interval {
-  return intervals.includes(value as Interval);
-}
-
-type IntervalToggleGroup = {
+type IntervalToggleGroupProps = {
   indicatorBoardMetadataId?: string;
 };
 
-export default function IntervalToggleGroup({ indicatorBoardMetadataId }: IntervalToggleGroup) {
+export default function IntervalToggleGroup({ indicatorBoardMetadataId }: IntervalToggleGroupProps) {
   const { interval, setInterval } = useIndicatorBoard(indicatorBoardMetadataId);
   const { splitScreen } = useSplitIndicatorBoard();
 
-  const handleIntervalChange = (value: string) => {
-    if (isInterval(value)) {
-      setInterval(value);
-    }
-  };
-
   return (
-    <ToggleGroup
-      disabled={indicatorBoardMetadataId ? false : true}
-      value={interval}
-      onValueChange={handleIntervalChange}
-      type="single"
-    >
-      <ToggleGroup.Item  value="day">
-        <Item>Day</Item>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="week">
-        <Item>Week</Item>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="month">
-        <Item>Month</Item>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="year">
-        <Item>Year</Item>
-      </ToggleGroup.Item>
-    </ToggleGroup>
+    <BaseIntervalToggleGroup
+      interval={interval}
+      setInterval={setInterval}
+      disabled={!indicatorBoardMetadataId}
+      splitScreen={splitScreen}
+    />
   );
-
-  function Item({ children }: React.PropsWithChildren) {
-    return (
-      <div
-        className={cn({
-          'w-20': splitScreen !== 'square',
-          'w-10 text-xs': splitScreen === 'square',
-        })}
-      >
-        {children}
-      </div>
-    );
-  }
 }
