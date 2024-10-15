b3930be710f24cc5686a82af63c6dec9155fa027
SeonghunYang <didtjdgns852@gmail.com>
2024-10-15 05:11:49 +0000

    [MC-111] [web] feat: layout 컴포넌트 일부를 page 컴포넌트로 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,17 +5,36 @@ import Form from './ui/components/view/molecule/form';
 import { authenticate } from './business/services/auth/sign-in.service';
 import SplitScreenToggleGroup from './ui/pages/workspace/split-screen/split-screen-toggle-group';
 import Workspace from './ui/pages/workspace/workspace';
+import MockingUser from './ui/components/util/mocking-user';
+import ChatAiNavigator from './ui/pages/workspace/chat-ai-navigator';
+import FloatingComponentContainer from './ui/pages/workspace/floating-component-container';
+import SideNav from './ui/pages/workspace/side-bar/sidenav';
+import { SWRProvider } from './ui/components/util/swr-provider';
+import ChatProvider from './business/hooks/linguistic-guidance/provider/chat-provider';
 
 export default function Page() {
   return (
-    <div className="relative h-full w-full">
-      <div className="absolute left-1/2 top-2 z-10 -translate-x-1/2 rounded-lg ">
-        <SplitScreenToggleGroup />
-      </div>
-      <div className="flex h-full items-center justify-center">
-        <Workspace />
-      </div>
-    </div>
+    <MockingUser>
+      <ChatProvider>
+        <SWRProvider>
+          <div className="flex h-screen md:flex-row md:overflow-hidden">
+            <SideNav />
+            <div className="grow bg-fingoo-gray-1.5">
+              <div className="relative h-full w-full">
+                <div className="absolute left-1/2 top-2 z-10 -translate-x-1/2 rounded-lg ">
+                  <SplitScreenToggleGroup />
+                </div>
+                <div className="flex h-full items-center justify-center">
+                  <Workspace />
+                </div>
+              </div>
+            </div>
+            <ChatAiNavigator />
+            <FloatingComponentContainer />
+          </div>
+        </SWRProvider>
+      </ChatProvider>
+    </MockingUser>
   );
 }
 
