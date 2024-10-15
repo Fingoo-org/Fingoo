599dec2e0e952636ddc36e172fdad77693ff344e
SeonghunYang <didtjdgns852@gmail.com>
2024-10-15 08:13:45 +0000

    [MC-111] [web] feat: worspace에 블로그 이동 callout 구현

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,6 +11,7 @@ import FloatingComponentContainer from './ui/pages/workspace/floating-component-
 import SideNav from './ui/pages/workspace/side-bar/sidenav';
 import { SWRProvider } from './ui/components/util/swr-provider';
 import ChatProvider from './business/hooks/linguistic-guidance/provider/chat-provider';
+import Callout from './ui/components/view/molecule/callout';
 
 export default function Page() {
   return (
@@ -26,6 +27,15 @@ export default function Page() {
                 </div>
                 <div className="flex h-full items-center justify-center">
                   <Workspace />
+                </div>
+                <div className="absolute bottom-2 left-1/2 z-10 -translate-x-1/2 rounded-lg ">
+                  <Callout>
+                    FINGOO 블로그로 이동하려면{'   '}
+                    <Link className="underline hover:text-gray-100" href="/blog">
+                      여기
+                    </Link>
+                    를 클릭하세요.
+                  </Callout>
                 </div>
               </div>
             </div>
