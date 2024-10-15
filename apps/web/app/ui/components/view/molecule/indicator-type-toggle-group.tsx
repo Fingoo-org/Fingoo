7f0adccd0f179dcd0cb7f838c1367ec177b32d4d
yesolz <yesol@ajou.ac.kr>
2024-10-16 00:15:37 +0900

    [MC-8] [web] feat: add fullWidth prop to IndicatorTypeToggleGroup

--- apps/web/app/ui/components/view/molecule/indicator-type-toggle-group.tsx
+++ apps/web/app/ui/components/view/molecule/indicator-type-toggle-group.tsx
@@ -1,17 +1,24 @@
 import { IndicatorType, indicatorTypes } from '@/app/store/stores/numerical-guidance/indicator-list.store';
 import ToggleGroup from './toggle-group';
+import { cn } from '@/app/utils/style';
 
 type IndicatorTypeToggleGroupProps = {
   value: string;
   onValueChange: (value: IndicatorType) => void;
   size: 'narrow' | 'wide';
+  fullWidth?: boolean;
 };
 
 function isIndicatorType(value: string): value is IndicatorType {
   return indicatorTypes.includes(value as IndicatorType);
 }
 
-export default function IndicatorTypeToggleGroup({ value, size, onValueChange }: IndicatorTypeToggleGroupProps) {
+export default function IndicatorTypeToggleGroup({
+  value,
+  size,
+  onValueChange,
+  fullWidth = false,
+}: IndicatorTypeToggleGroupProps) {
   const handleIndicatorTypeChange = (indicatorType: IndicatorType) => {
     if (isIndicatorType(indicatorType)) {
       onValueChange(indicatorType);
@@ -19,13 +26,31 @@ export default function IndicatorTypeToggleGroup({ value, size, onValueChange }:
   };
 
   return (
-    <ToggleGroup value={value} onValueChange={handleIndicatorTypeChange} type="single" size={size}>
-      <ToggleGroup.Item value="stocks">주식</ToggleGroup.Item>
-      <ToggleGroup.Item value="funds">펀드</ToggleGroup.Item>
-      <ToggleGroup.Item value="forex_pairs">외환</ToggleGroup.Item>
-      <ToggleGroup.Item value="cryptocurrencies">크립토</ToggleGroup.Item>
-      <ToggleGroup.Item value="indices">지수</ToggleGroup.Item>
-      <ToggleGroup.Item value="bonds">채권</ToggleGroup.Item>
+    <ToggleGroup
+      value={value}
+      onValueChange={handleIndicatorTypeChange}
+      type="single"
+      size={size}
+      className={cn(fullWidth && 'w-full')}
+    >
+      <ToggleGroup.Item value="stocks" className={cn(fullWidth && 'flex-1')}>
+        주식
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="funds" className={cn(fullWidth && 'flex-1')}>
+        펀드
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="forex_pairs" className={cn(fullWidth && 'flex-1')}>
+        외환
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="cryptocurrencies" className={cn(fullWidth && 'flex-1')}>
+        크립토
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="indices" className={cn(fullWidth && 'flex-1')}>
+        지수
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="bonds" className={cn(fullWidth && 'flex-1')}>
+        채권
+      </ToggleGroup.Item>
     </ToggleGroup>
   );
 }
