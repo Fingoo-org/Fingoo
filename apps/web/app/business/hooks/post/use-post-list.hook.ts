af1448050fefc1cdfdb51bcef9c7515ff2ec0543
HANTAEDONG <htd0913@gmail.com>
2024-10-16 14:51:20 +0900

    [MC-13] [web]feat: use-post-list에하트 추가/삭제 기능 추가

--- apps/web/app/business/hooks/post/use-post-list.hook.ts
+++ apps/web/app/business/hooks/post/use-post-list.hook.ts
@@ -4,7 +4,9 @@ import { useWorkspaceStore } from '@/app/store/stores/numerical-guidance/workspa
 import { PostList, convertPostList } from '../../services/post/view-model/post-list/post-list-view-model.service';
 
 export const usePostList = () => {
+  // 무한스크롤 데이터 불러오기
   const { data: postList, setSize, isValidating } = useFetchPostList();
+  // selectedPost를 workspace.ts에서 정의
   const selectedPostId = useWorkspaceStore((state) => state.selectedPostId);
   const { trigger: addHeartTrigger } = useAddHeartToPost(selectedPostId);
   const { trigger: deleteHeartTrigger } = useDeleteHeartFromPost();
@@ -15,15 +17,20 @@ export const usePostList = () => {
     return convertPostList(allPosts);
   }, [postList]);
 
+  const selectedPost = useMemo(() => {
+    if (!selectedPostId) return undefined;
+    return convertedPostList?.findPostById(selectedPostId);
+  }, [selectedPostId, convertedPostList]);
+
   const optimisticRevalidateOption = { revalidate: false }; // optimistic update용 옵션
 
   // 하트 추가 기능 (optimistic update 적용)
   const addHeartToPost = async (postId: string) => {
+    if (!postId) return;
     addHeartTrigger(
-      { postId }, // { postId: postId } 객체로 전달
+      { postId },
       {
         optimisticData: (): PostList | undefined => {
-          // optimistic update로 UI 먼저 업데이트
           const newPostList = convertedPostList?.addHeartToPostById(postId);
           return newPostList;
         },
@@ -34,21 +41,21 @@ export const usePostList = () => {
 
   // 하트 제거 기능 (optimistic update 적용)
   const removeHeartFromPost = async (postId: string) => {
-    deleteHeartTrigger(
-      postId, // { postId: postId } 객체로 전달
-      {
-        optimisticData: (): PostList | undefined => {
-          // optimistic update로 UI 먼저 업데이트
-          const newPostList = convertedPostList?.removeHeartFromPostById(postId);
-          return newPostList;
-        },
-        ...optimisticRevalidateOption,
+    if (!postId) return;
+
+    deleteHeartTrigger(postId, {
+      optimisticData: (): PostList | undefined => {
+        const newPostList = convertedPostList?.removeHeartFromPostById(postId);
+        return newPostList;
       },
-    );
+      ...optimisticRevalidateOption,
+    });
   };
 
   return {
-    postList: convertedPostList,
+    selectedPostId,
+    selectedPost,
+    postList: convertedPostList, // 무한 스크롤
     isPending: isValidating,
     addHeartToPost, // 하트 추가 기능
     removeHeartFromPost, // 하트 제거 기능
