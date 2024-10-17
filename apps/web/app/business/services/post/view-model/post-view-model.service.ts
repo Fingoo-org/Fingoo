6c740f2b7657b7fc4f7b43f25f1b6ebba68e9808
HANTAEDONG <htd0913@gmail.com>
2024-10-17 17:25:13 +0900

    [MC-13] [web] fix: post-view-model.service.tsx 안쓰이므로 삭제

--- apps/web/app/business/services/post/view-model/post-view-model.service.ts
+++
@@ -1,6 +0,0 @@
-import { PostResponse } from '@/app/store/querys/post/post-list.query';
-import { Post } from './post-list/post-view-model.service';
-
-export function convertPostViewModel(postList: PostResponse[]): Post[] {
-  return postList.map((post) => new Post(post)); // PostResponse 배열을 Post 배열로 변환
-}
