3858620cf552bd5920ec1a93d33a0767eb4405da
HANTAEDONG <htd0913@gmail.com>
2024-10-18 15:48:42 +0900

    [MC-13] [web]test: 좋아요 추가/제거 기능에 따라 post.action.mock.ts와 post-handler.mock.ts 작성

--- apps/web/app/mocks/db/post.action.mock.ts
+++ apps/web/app/mocks/db/post.action.mock.ts
@@ -1,12 +1,13 @@
-import { PostListResponse } from '@/app/store/querys/post/post-list.query';
+import { PostListResponse, UpdatePostHeartRequestBody } from '@/app/store/querys/post/post-list.query';
 import { mockDatabaseStore } from '.';
 
 export type MockPostAction = {
-  getPosts: () => PostListResponse;
+  getPostList: () => PostListResponse;
+  patchHeart: (postId: string, data: UpdatePostHeartRequestBody) => void;
 };
 
 export const mockPostAction: MockPostAction = {
-  getPosts: () => {
+  getPostList: () => {
     return {
       data: mockDatabaseStore.post,
       meta: {
@@ -15,5 +16,17 @@ export const mockPostAction: MockPostAction = {
         cursor: 1,
       },
     };
+  },
+  patchHeart: (postId) => {
+    const index = mockDatabaseStore.post.findIndex((post) => post.postId === postId);
+    if (index !== -1) {
+      const currentPost = mockDatabaseStore.post[index];
+      const updatedPost = {
+        ...currentPost,
+        hasUserLiked: !currentPost.hasUserLiked,
+        likeCount: currentPost.hasUserLiked ? currentPost.likeCount - 1 : currentPost.likeCount + 1,
+      };
+      mockDatabaseStore.post[index] = updatedPost;
+    }
   },
 };
--- apps/web/app/mocks/handlers/post-handler.mock.ts
+++ apps/web/app/mocks/handlers/post-handler.mock.ts
@@ -2,10 +2,23 @@ import { HttpResponse, http } from 'msw';
 import { delayForDevelopment } from '.';
 import { API_PATH } from '../../store/querys/api-path';
 import { mockDB } from '../db';
+import { PostsValueMockData } from '../mock-data/post-value.mock';
+import { PostResponse, UpdatePostHeartRequestBody } from '@/app/store/querys/post/post-list.query';
+
+type postParam = {
+  postId: string;
+};
 
 export const postHandlers = [
   http.get(`${API_PATH.postList}`, async () => {
     await delayForDevelopment();
-    return HttpResponse.json(mockDB.getPosts());
+    return HttpResponse.json(mockDB.getPostList());
+  }),
+  http.patch<postParam, UpdatePostHeartRequestBody>(`${API_PATH.pathchHeart}/:postId`, async ({ request, params }) => {
+    const postId = params.postId as string;
+    const data = await request.json();
+    await delayForDevelopment();
+    mockDB.patchHeart(postId, data);
+    return HttpResponse.json({ status: 200 });
   }),
 ];
