ffaa882114ce34aedcf57d717b04e3d6a329a868
HANTAEDONG <htd0913@gmail.com>
2024-10-18 14:43:14 +0900

    [MC-13] [web]fix: postList의 좋아요 추가/제거 기능을 patch로 바꿈에 따라 use-post-list와 post-list-view-model.service 변경

--- apps/web/app/business/hooks/post/use-post-list.hook.ts
+++ apps/web/app/business/hooks/post/use-post-list.hook.ts
@@ -1,15 +1,14 @@
 import { useMemo } from 'react';
-import { useAddHeartToPost, useDeleteHeartFromPost, useFetchPostList } from '@/app/store/querys/post/post-list.query';
+import { useFetchPostList, useUpdatePostHeart } from '@/app/store/querys/post/post-list.query';
 import { useWorkspaceStore } from '@/app/store/stores/numerical-guidance/workspace.store';
 import { PostList, convertPostList } from '../../services/post/view-model/post-list/post-list-view-model.service';
 
 export const usePostList = () => {
-  // 무한스크롤 데이터 불러오기
   const { data: postList, setSize, isValidating } = useFetchPostList();
-  // selectedPost를 workspace.ts에서 정의
   const selectedPostId = useWorkspaceStore((state) => state.selectedPostId);
-  const { trigger: addHeartTrigger } = useAddHeartToPost(selectedPostId);
-  const { trigger: deleteHeartTrigger } = useDeleteHeartFromPost();
+  // const { trigger: addHeartTrigger } = useAddHeartToPost(selectedPostId);
+  // const { trigger: deleteHeartTrigger } = useDeleteHeartFromPost();
+  const { trigger: updateHeartTrigger } = useUpdatePostHeart(selectedPostId);
 
   const convertedPostList = useMemo(() => {
     if (!postList) return undefined;
@@ -24,41 +23,56 @@ export const usePostList = () => {
 
   const optimisticRevalidateOption = { revalidate: false }; // optimistic update용 옵션
 
-  // 하트 추가 기능 (optimistic update 적용)
-  const addHeartToPost = async (postId: string) => {
-    if (!postId) return;
-    addHeartTrigger(
-      { postId },
+  const updateHeartFromPost = async (hasUserLiked: boolean) => {
+    if (!selectedPostId) return;
+
+    updateHeartTrigger(
+      { hasUserLiked }, // 서버에 보낼 데이터
       {
         optimisticData: (): PostList | undefined => {
-          const newPostList = convertedPostList?.addHeartToPostById(postId);
+          const newPostList = convertedPostList?.updateHeartStatusByPostId(selectedPostId, hasUserLiked);
           return newPostList;
         },
-        ...optimisticRevalidateOption,
+        ...optimisticRevalidateOption, // Optimistic Update 적용 시 revalidate 하지 않음
       },
     );
   };
 
-  // 하트 제거 기능 (optimistic update 적용)
-  const removeHeartFromPost = async (postId: string) => {
-    if (!postId) return;
+  // // 하트 추가 기능 (optimistic update 적용)
+  // const addHeartToPost = async (postId: string) => {
+  //   if (!postId) return;
+  //   addHeartTrigger(
+  //     { postId },
+  //     {
+  //       optimisticData: (): PostList | undefined => {
+  //         const newPostList = convertedPostList?.addHeartToPostById(postId);
+  //         return newPostList;
+  //       },
+  //       ...optimisticRevalidateOption,
+  //     },
+  //   );
+  // };
 
-    deleteHeartTrigger(postId, {
-      optimisticData: (): PostList | undefined => {
-        const newPostList = convertedPostList?.removeHeartFromPostById(postId);
-        return newPostList;
-      },
-      ...optimisticRevalidateOption,
-    });
-  };
+  // // 하트 제거 기능 (optimistic update 적용)
+  // const removeHeartFromPost = async (postId: string) => {
+  //   if (!postId) return;
+
+  //   deleteHeartTrigger(postId, {
+  //     optimisticData: (): PostList | undefined => {
+  //       const newPostList = convertedPostList?.removeHeartFromPostById(postId);
+  //       return newPostList;
+  //     },
+  //     ...optimisticRevalidateOption,
+  //   });
+  // };
 
   return {
     selectedPostId,
     selectedPost,
     postList: convertedPostList, // 무한 스크롤
     isPending: isValidating,
-    addHeartToPost, // 하트 추가 기능
-    removeHeartFromPost, // 하트 제거 기능
+    // addHeartToPost, // 하트 추가 기능
+    // removeHeartFromPost, // 하트 제거 기능
     loadMorePosts: () => setSize((size) => size + 1), // 추가 포스트 로드
   };
 };
--- apps/web/app/business/services/post/view-model/post-list/post-list-view-model.service.ts
+++ apps/web/app/business/services/post/view-model/post-list/post-list-view-model.service.ts
@@ -17,31 +17,25 @@ export class PostList extends Array<Post> {
     return this.map((post) => post.formattedPost);
   }
 
-  // iterate 메서드: 특정 콜백을 사용해 Post 인스턴스를 업데이트하고 PostList로 반환
   iterate(callback: (post: Post) => Post): PostList {
     return convertPostList(this.map(callback));
   }
 
-  // 특정 postId로 Post 찾기
   findPostById(postId: string) {
     return this.find((post) => post.postId === postId);
   }
 
-  // 특정 postId로 Post 삭제하기
   deletePostById(postId: string) {
     return convertPostList(this.filter((post) => post.postId !== postId));
   }
 
-  // 특정 postId에 하트 추가 (유저가 하트를 누를 때)
   toggleHeartByPostId(postId: string | undefined) {
     return this.iterate((post) => {
       if (post.postId === postId) {
         if (post.hasUserLiked) {
-          // 이미 하트를 눌렀을 경우 하트를 해제
           post.likeCount -= 1;
           post.hasUserLiked = false;
         } else {
-          // 하트를 누르지 않은 경우 하트를 추가
           post.likeCount += 1;
           post.hasUserLiked = true;
         }
@@ -50,23 +44,20 @@ export class PostList extends Array<Post> {
     });
   }
 
-  // 특정 postId에 하트 추가 (누른 경우 빨간색 하트)
-  addHeartToPostById(postId: string | undefined) {
+  updateHeartStatusByPostId(postId: string | undefined, hasUserLiked: boolean) {
     return this.iterate((post) => {
       if (post.postId === postId) {
-        post.likeCount += 1;
-        post.hasUserLiked = true;
-      }
-      return post;
-    });
-  }
-
-  // 특정 postId에 하트 제거 (해제한 경우 투명한 하트)
-  removeHeartFromPostById(postId: string | undefined) {
-    return this.iterate((post) => {
-      if (post.postId === postId && post.likeCount > 0) {
-        post.likeCount -= 1;
-        post.hasUserLiked = false;
+        if (hasUserLiked) {
+          if (!post.hasUserLiked) {
+            post.likeCount += 1;
+            post.hasUserLiked = true;
+          }
+        } else {
+          if (post.hasUserLiked) {
+            post.likeCount -= 1;
+            post.hasUserLiked = false;
+          }
+        }
       }
       return post;
     });
